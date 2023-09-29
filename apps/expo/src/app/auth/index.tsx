8d6fdbe40145385ebacc83602a8361828925eb45
einsight <ghaithkale@gmail.com>
2023-09-28 22:42:05 -0400

    updates

--- apps/expo/src/app/auth/index.tsx
+++ apps/expo/src/app/auth/index.tsx
@@ -1,39 +1,58 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
-import { Text, View, Image, YStack, Button, SizableText } from "tamagui";
 import Tinder from "@assets/tinder.png";
+import { Button, Image, SizableText, Text, View, YStack } from "tamagui";
 
 const Index = () => {
   return (
     <View flex={1} backgroundColor={"$background"}>
-      <View flex={1} alignItems="center" justifyContent="center" marginBottom="$4">
-        <Image
-          source={Tinder}
-          resizeMode="contain"
-        />
+      <View
+        flex={1}
+        alignItems="center"
+        justifyContent="center"
+        marginBottom="$4"
+      >
+        <Image source={Tinder} resizeMode="contain" />
       </View>
-      <YStack marginBottom="$6" marginHorizontal="$4"padding="$2" space={"$4"}>
+      <YStack marginHorizontal="$4" padding="$2" space={"$4"}>
         <Text padding={"$2"} textAlign="center" fontSize={12}>
-          By tapping &apos;Sign in&apos;, you agree with our <Text fontWeight={"500"} textDecorationLine="underline">Terms.</Text> Learn how we process your data in our <Text fontWeight={"500"} textDecorationLine="underline">Privacy Policy</Text> and <Text fontWeight={"500"} textDecorationLine="underline">Cookies Policy</Text>.
+          By tapping &apos;Sign in&apos;, you agree with our{" "}
+          <Text fontWeight={"500"} textDecorationLine="underline">
+            Terms.
+          </Text>{" "}
+          Learn how we process your data in our{" "}
+          <Text fontWeight={"500"} textDecorationLine="underline">
+            Privacy Policy
+          </Text>{" "}
+          and{" "}
+          <Text fontWeight={"500"} textDecorationLine="underline">
+            Cookies Policy
+          </Text>
+          .
         </Text>
-        <Button backgroundColor={"white"} padding={'$2'} borderRadius={16}>
+        <Button backgroundColor={"white"} padding={"$2"} borderRadius={16}>
           <Text color="black" fontWeight={"$2"}>
-          CREATE AN ACCOUNT
+            CREATE AN ACCOUNT
           </Text>
         </Button>
 
-        <Button backgroundColor={"$backgroundTransparent"} padding={'$2'} borderRadius={16} borderColor={"white"} borderWidth={1}>
+        <Button
+          backgroundColor={"$backgroundTransparent"}
+          padding={"$2"}
+          borderRadius={16}
+          borderColor={"white"}
+          borderWidth={1}
+        >
           <SizableText color="white" fontWeight={"$2"}>
-          SIGN IN
+            SIGN IN
           </SizableText>
         </Button>
 
-          <Text fontWeight={"400"} alignSelf="center">
-            Trouble signing in?
-          </Text>
+        <Text fontWeight={"400"} alignSelf="center" marginBottom={"$4"}>
+          Trouble signing in?
+        </Text>
       </YStack>
     </View>
-
   );
 };
 
