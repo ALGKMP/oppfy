d4125784587c27ac9716c42a17c4469e4ceba712
tony <antonylbrussolo@gmail.com>
2023-09-28 22:30:53 -0400

    auth

--- apps/expo/src/app/auth/index.tsx
+++ apps/expo/src/app/auth/index.tsx
@@ -13,22 +13,22 @@ const Index = () => {
         />
       </View>
       <YStack marginBottom="$6" marginHorizontal="$4"padding="$2" space={"$4"}>
-        <Text padding={"$2"} textAlign="center">
-          By tapping &apos;Sign in&apos;, you agree with our <Text fontWeight={"$16"} textDecorationLine="underline">Terms.</Text> Learn how we process your data in our <Text fontWeight={"$16"} textDecorationLine="underline">Privacy Policy</Text> and <Text fontWeight={"$16"} textDecorationLine="underline">Cookies Policy</Text>.
+        <Text padding={"$2"} textAlign="center" fontSize={12}>
+          By tapping &apos;Sign in&apos;, you agree with our <Text fontWeight={"500"} textDecorationLine="underline">Terms.</Text> Learn how we process your data in our <Text fontWeight={"500"} textDecorationLine="underline">Privacy Policy</Text> and <Text fontWeight={"500"} textDecorationLine="underline">Cookies Policy</Text>.
         </Text>
         <Button backgroundColor={"white"} padding={'$2'} borderRadius={16}>
           <Text color="black" fontWeight={"$2"}>
-          Create Account
+          CREATE AN ACCOUNT
           </Text>
         </Button>
 
-        <Button backgroundColor={"black"} padding={'$2'} borderRadius={16}>
+        <Button backgroundColor={"$backgroundTransparent"} padding={'$2'} borderRadius={16} borderColor={"white"} borderWidth={1}>
           <SizableText color="white" fontWeight={"$2"}>
-          Create Account
+          SIGN IN
           </SizableText>
         </Button>
 
-          <Text fontWeight={"500"} alignSelf="center">
+          <Text fontWeight={"400"} alignSelf="center">
             Trouble signing in?
           </Text>
       </YStack>
