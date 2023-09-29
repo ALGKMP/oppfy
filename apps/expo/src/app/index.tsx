1cccf053e6120fd28bd72bcb3ffea45e0e5682a7
einsight <ghaithkale@gmail.com>
2023-09-28 20:44:53 -0400

    updates

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -11,7 +11,7 @@ const Index = () => {
         <Redirect href="/home" />
       </SignedIn>
       <SignedOut>
-        <Redirect href="/home" />
+        <Redirect href="/auth" />
       </SignedOut>
     </FirebaseLoaded>
   );
