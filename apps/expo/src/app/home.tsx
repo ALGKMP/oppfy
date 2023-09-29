d6b6235bcb6ceaa5f5ca3586105665691690ffd5
einsight <ghaithkale@gmail.com>
2023-09-28 20:21:43 -0400

    updates

---
+++ apps/expo/src/app/home.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Button } from 'tamagui'
+
+const Home = () => {
+  return (
+    <View>
+      <Button>Home</Button>
+    </View>
+  )
+}
+
+export default Home
