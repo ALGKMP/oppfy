07e912c01ff46caedb31c8caaebb21156b4f9b05
tony <antonylbrussolo@gmail.com>
2023-09-30 02:20:37 -0400

    commit

---
+++ apps/expo/src/utils/firebase.ts
@@ -0,0 +1,6 @@
+import { FirebaseError } from "@firebase/util";
+
+export const isFireBaseError = (error: unknown): error is FirebaseError  => {
+    return error instanceof FirebaseError;
+}
+
--- packages/api/src/router/auth.ts
+++ packages/api/src/router/auth.ts
@@ -1,11 +1,28 @@
+import auth from "@react-native-firebase/auth";
+import { z } from "zod";
+
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const authRouter = createTRPCRouter({
   getSession: publicProcedure.query(({ ctx }) => {
     return ctx.session;
   }),
-  getSecretMessage: protectedProcedure.query(() => {
-    // testing type validation of overridden next-auth Session in @acme/auth package
-    return "you can see this secret message!";
-  }),
+  storeAccountFirebase: publicProcedure
+    .input(
+      z.object({
+        email: z.string().email(),
+        password: z.string().min(6),
+        firebaseUid: z.string(),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      const { email, password, firebaseUid } = input;
+      ctx.prisma.user.create({
+        data: {
+          firebaseUid,
+          email,
+          password,
+        },
+      });
+    }),
 });
