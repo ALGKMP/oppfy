ee0d2da463858ab18ae59911466d5d1c6a3181ff
Einsight <ghaithkhalil2004@gmail.com>
2023-12-12 15:26:31 -0500

    Add error handling and response headers to TRPC handler

--- apps/nextjs/src/pages/api/[trpc].ts
+++ apps/nextjs/src/pages/api/[trpc].ts
@@ -12,6 +12,16 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   return createOpenApiNextHandler({
     router: appRouter,
     createContext: createTRPCContext,
+    onError() {
+      console.error("Error in TRPC handler");
+    },
+    responseMeta() {
+      return {
+        headers: {
+          "x-powered-by": "trpc",
+        },
+      };
+    },
   })(req, res);
 };
 
