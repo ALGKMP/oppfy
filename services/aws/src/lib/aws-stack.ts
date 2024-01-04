d2be37d45351b9ab472009a834ccf4a51c15535f
einsight <ghaithkale@gmail.com>
2024-01-03 20:17:50 -0500

    bastion host for access to vpc endpoints

--- services/aws/src/lib/aws-stack.ts
+++ services/aws/src/lib/aws-stack.ts
@@ -114,6 +114,44 @@ export class AwsStack extends cdk.Stack {
       new s3n.LambdaDestination(myLambda),
     );
 
+    const bastionSecurityGroup = new ec2.SecurityGroup(
+      this,
+      "BastionSecurityGroup",
+      {
+        vpc,
+        description: "Allow SSH access to Bastion Host",
+        allowAllOutbound: true,
+      },
+    );
+
+    // Allow inbound SSH connections on the default port (22)
+    bastionSecurityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      "Allow SSH access",
+    );
+
+    const bastionHostLinux = new ec2.BastionHostLinux(this, "BastionHost", {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE2,
+        ec2.InstanceSize.MICRO,
+      ),
+      securityGroup: bastionSecurityGroup,
+      subnetSelection: { subnetType: ec2.SubnetType.PUBLIC },
+    });
+
+    // Display commands for connect bastion host using ec2 instance connect
+    const createSshKeyCommand = "ssh-keygen -t rsa -f my_rsa_key";
+    const pushSshKeyCommand = `aws ec2-instance-connect send-ssh-public-key --region ${cdk.Aws.REGION} --instance-id ${bastionHostLinux.instanceId} --availability-zone ${bastionHostLinux.instanceAvailabilityZone} --instance-os-user ec2-user --ssh-public-key file://my_rsa_key.pub --profile default`;
+    const sshCommand = `ssh -o "IdentitiesOnly=yes" -i my_rsa_key ec2-user@${bastionHostLinux.instancePublicDnsName}`;
+
+    new cdk.CfnOutput(this, "CreateSshKeyCommand", {
+      value: createSshKeyCommand,
+    });
+    new cdk.CfnOutput(this, "PushSshKeyCommand", { value: pushSshKeyCommand });
+    new cdk.CfnOutput(this, "SshCommand", { value: sshCommand });
+
     // Define a security group for the RDS instance within the VPC
     const openSearchSecurityGroup = new ec2.SecurityGroup(
       this,
@@ -122,6 +160,12 @@ export class AwsStack extends cdk.Stack {
         vpc,
         description: "Security group for Open Search Service",
       },
+    );
+
+    openSearchSecurityGroup.addIngressRule(
+      ec2.Peer.securityGroupId(bastionSecurityGroup.securityGroupId),
+      ec2.Port.tcp(443),
+      "Allow access from Bastion Host",
     );
 
     const openSearchDomain = new opensearch.Domain(this, "MyOpenSearchDomain", {
@@ -218,6 +262,7 @@ export class AwsStack extends cdk.Stack {
       endpointType: "source",
       engineName: "postgres",
       username: dbCredentialsSecret
+        // todo: unsafeUnwrap() needs to be replaced with something safe
         .secretValueFromJson("username")
         .unsafeUnwrap(),
       password: dbCredentialsSecret
@@ -313,135 +358,3 @@ export class AwsStack extends cdk.Stack {
     });
   }
 }
-
-// // OpenSearch Service setup
-// const openSearchDomain = new opensearch.CfnDomain(
-//   this,
-//   "MyOpenSearchDomain",
-//   {
-//     domainName: "my-opensearch-domain",
-//     engineVersion: "OpenSearch_1.0", // Use the desired version
-//     clusterConfig: {
-//       instanceType: "t3.small.search", // Choose an instance type eligible for free tier if available
-//       instanceCount: 1,
-//     },
-//     ebsOptions: {
-//       ebsEnabled: true,
-//       volumeSize: 10, // Set according to your needs within free tier limits
-//     },
-//     accessPolicies: {
-//       Version: "2012-10-17",
-//       Statement: [
-//         {
-//           Effect: "Allow",
-//           Principal: {
-//             AWS: "*",
-//           },
-//           Action: "es:*",
-//           Resource:
-//             "arn:aws:es:region:account:domain/my-opensearch-domain/*",
-//         },
-//       ],
-//     },
-//     nodeToNodeEncryptionOptions: {
-//       enabled: true,
-//     },
-//     encryptionAtRestOptions: {
-//       enabled: true,
-//     },
-//   },
-// );
-
-// // IAM Role for DMS - This role is automatically used by DMS
-// const dmsRole = new iam.Role(this, "DmsServiceRole", {
-//   assumedBy: new iam.ServicePrincipal("dms.amazonaws.com"),
-//   managedPolicies: [
-//     iam.ManagedPolicy.fromAwsManagedPolicyName(
-//       "service-role/AmazonDMSVPCManagementRole",
-//     ),
-//   ],
-// });
-
-// // DMS Replication Instance
-// const dmsReplicationInstance = new dms.CfnReplicationInstance(
-//   this,
-//   "MyDmsReplicationInstance",
-//   {
-//     replicationInstanceClass: "dms.t2.micro",
-//     vpcSecurityGroupIds: [dbSecurityGroup.securityGroupId],
-//     replicationSubnetGroupIdentifier: new dms.CfnReplicationSubnetGroup(
-//       this,
-//       "MyDmsSubnetGroup",
-//       {
-//         replicationSubnetGroupDescription: "Subnet group for DMS",
-//         subnetIds: vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC })
-//           .subnetIds,
-//       },
-//     ).replicationSubnetGroupIdentifier,
-//     publiclyAccessible: true,
-//   },
-// );
-
-// // DMS Source Endpoint
-// const dmsSourceEndpoint = new dms.CfnEndpoint(this, "MyDmsSourceEndpoint", {
-//   endpointType: "source",
-//   engineName: "postgres",
-//   // Use secret ARN directly instead of exposing values
-//   username: dbCredentialsSecret.secretArn,
-//   password: dbCredentialsSecret.secretArn,
-//   serverName: "<RDS_INSTANCE_ENDPOINT>", // Replace with actual endpoint
-//   port: 5432,
-//   databaseName: "mydatabase",
-//   sslMode: "require",
-// });
-
-// // DMS Target Endpoint
-// const dmsTargetEndpoint = new dms.CfnEndpoint(this, "MyDmsTargetEndpoint", {
-//   endpointType: "target",
-//   engineName: "opensearch",
-//   serverName: openSearchDomain.attrDomainEndpoint,
-//   sslMode: "none", // Change if SSL is configured for OpenSearch
-// });
-
-// const dmsReplicationTask = new dms.CfnReplicationTask(
-//   this,
-//   "MyDmsReplicationTask",
-//   {
-//     migrationType: "full-load-and-cdc",
-//     sourceEndpointArn: dmsSourceEndpoint.ref,
-//     targetEndpointArn: dmsTargetEndpoint.ref,
-//     replicationInstanceArn: dmsReplicationInstance.ref,
-//     tableMappings: JSON.stringify({
-//       rules: [
-//         {
-//           "rule-type": "selection",
-//           "rule-id": "1",
-//           "rule-name": "1",
-//           "object-locator": {
-//             "schema-name": "public",
-//             "table-name": "User",
-//           },
-//           "rule-action": "include",
-//           filters: [],
-//         },
-//         {
-//           "rule-type": "transformation",
-//           "rule-id": "2",
-//           "rule-name": "2",
-//           "rule-target": "column",
-//           "object-locator": {
-//             "schema-name": "public",
-//             "table-name": "User",
-//           },
-//           "rule-action": "map-record-to-record",
-//           "transformation-parameters": {
-//             "record-type": "json",
-//           },
-//         },
-//         // Add more rules for other tables like Post, Comment, etc. if needed
-//       ],
-//     }),
-//   },
-// );
-//   }
-// }
