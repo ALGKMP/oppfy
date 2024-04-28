1c7445188efaf9dabb30dd1fba6470e7fc1adb62
Einsight <ghaithkhalil2004@gmail.com>
2024-04-27 22:52:16 -0400

    removed tailwind bundling

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -3,17 +3,7 @@ module.exports = function (api) {
   api.cache.forever();
 
   return {
-    presets: [
-      [
-        "babel-preset-expo", 
-      { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
-    plugins: [
-      // require.resolve("babel-preset-expo"),
-      // require.resolve("react-native-reanimated/plugin"),
-      // "babel-preset-expo",
-      "react-native-reanimated/plugin",
-    ],
+    presets: [["babel-preset-expo"]],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
