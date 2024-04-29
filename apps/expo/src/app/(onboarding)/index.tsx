bdd0a30fe44ddb210c832c4e02a3ccd102e7b666
Einsight <ghaithkhalil2004@gmail.com>
2024-04-28 21:12:56 -0400

    frontend rewrite

--- apps/expo/src/app/(onboarding)/index.tsx
+++ apps/expo/src/app/(onboarding)/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { KeyboardAvoidingView, Platform } from "react-native";
 import { useRouter } from "expo-router";
 import { Button, Text, View, YStack } from "tamagui";
 
@@ -19,41 +18,22 @@ const Start = () => {
   };
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : undefined}
-      style={{ flex: 1 }}
+    <View
+      flex={1}
+      padding="$6"
+      backgroundColor="$background"
+      justifyContent="space-between"
     >
-      <View
-        flex={1}
-        padding="$6"
-        backgroundColor="black"
-        justifyContent="space-between"
-      >
-        <YStack flex={1} alignItems="center" justifyContent="center">
-          <Text fontSize={64} fontWeight="700">
-            OPPFY.
-          </Text>
-          <Text fontSize={24} fontWeight="700">
-            Capture Real Memories.
-          </Text>
-        </YStack>
-
-        <View>
-          <Button
-            onPress={onSubmit}
-            borderWidth={0}
-            pressStyle={{
-              backgroundColor: "$gray12",
-            }}
-            backgroundColor="white"
-          >
-            <Text color="black" fontSize={16} fontWeight="600">
-              Next
-            </Text>
-          </Button>
-        </View>
-      </View>
-    </KeyboardAvoidingView>
+      <YStack flex={1} alignItems="center" justifyContent="center">
+        <Text fontFamily="$modak" fontSize={64} margin={-24}>
+          OPPFY
+        </Text>
+        <Text fontSize={24} fontWeight="700">
+          Capture Real Memories.
+        </Text>
+      </YStack>
+      <Button onPress={onSubmit}>Welcome</Button>
+    </View>
   );
 };
 
