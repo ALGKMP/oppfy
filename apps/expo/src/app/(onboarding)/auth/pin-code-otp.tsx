a74778ec45ec38dc12afa8cdf7fab3543ce4f45f
Einsight <ghaithkhalil2004@gmail.com>
2024-04-30 00:25:04 -0400

    ui rewrite

--- apps/expo/src/app/(onboarding)/auth/pin-code-otp.tsx
+++ apps/expo/src/app/(onboarding)/auth/pin-code-otp.tsx
@@ -8,246 +8,129 @@ import React, {
 import type { TextInput } from "react-native";
 import { KeyboardAvoidingView, Platform } from "react-native";
 import { useRouter } from "expo-router";
-import auth from "@react-native-firebase/auth";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 // import type { FirebaseAuthTypes } from "@react-native-firebase/auth";
-import { Button, Spinner, Text, View, YStack } from "tamagui";
+import { Button, Input, Spinner, Text, View, XStack, YStack } from "tamagui";
 import * as z from "zod";
 
 import { PinCodeInput } from "~/components/Inputs";
+import { KeyboardSafeView } from "~/components/SafeViews";
 import { useSession } from "~/contexts/SessionsContext";
 import useCooldown from "~/hooks/useCooldown";
 import useParams from "~/hooks/useParams";
 import { api } from "~/utils/api";
+
+enum Error {
+  INCORRECT_CODE = "Incorrect code. Try again.",
+  ERROR_SENDING_CODE = "Error sending code. Try again later.",
+  UNKNOWN_ERROR = "An unknown error occurred. Please try again later.",
+}
 
 export interface SignUpFlowParams {
   [key: string]: string;
   phoneNumber: string;
 }
 
-const schemaValidation = z.object({
+const phoneNumberOTPValidation = z.object({
   phoneNumberOTP: z.string().length(6),
 });
-
-const RESEND_CODE_COOLDOWN_DURATION_SEC = 60;
 
 const PhoneNumberOTP = () => {
   const router = useRouter();
   const signUpFlowParams = useParams<SignUpFlowParams>();
 
-  const pinCodeInputRef = useRef<TextInput | null>(null);
+  const { verifyPhoneNumberOTP } = useSession();
 
   const [phoneNumberOTP, setPhoneNumberOTP] = useState("");
-
-  const [isSendingCode, setIsSendingCode] = useState(true);
-  const [isCheckingCode, setIsCheckingCode] = useState(false);
-
-  const [codeChecked, setCodeChecked] = useState(false);
-
-  const [error, setError] = useState<string | null>(null);
-
-  const { cooldown, resetCooldown } = useCooldown(
-    RESEND_CODE_COOLDOWN_DURATION_SEC,
-    { autoStart: true },
-  );
+  const [error, setError] = useState<Error | null>(null);
 
   const createUser = api.auth.createUser.useMutation();
-  const requiresAdditionalDetails = api.user.userComplete.useMutation();
+  const userOnboardingCompleted =
+    api.user.userOnboardingCompleted.useMutation();
 
-  const { verifyPhoneNumberOTP, signInWithPhoneNumber } = useSession();
-
-  const buttonIsDisabled = useMemo(
-    () => cooldown !== 0 || isSendingCode || isCheckingCode,
-    [cooldown, isCheckingCode, isSendingCode],
-  );
-
-  const completeSignIn = useCallback(async () => {
-    setIsCheckingCode(true);
-
-    try {
-      const userCredential = await verifyPhoneNumberOTP(phoneNumberOTP);
-      const isNewUser = userCredential?.additionalUserInfo?.isNewUser;
-
-      if (!userCredential?.user?.uid) {
-        throw new Error("Error verifying phone number.");
-      }
-
-      if (isNewUser) {
-        await createUser.mutateAsync({
-          userId: userCredential.user.uid,
-        });
-      }
-
-      await auth().currentUser?.reload();
-
-      await requiresAdditionalDetails.mutateAsync({
-        userId: userCredential.user.uid,
-      });
-
-      requiresAdditionalDetails.data
-        ? router.replace("/user-info/welcome")
-        : router.replace("/(app)/(bottom-tabs)/profile");
-    } catch (err) {
-      console.log(err)
-      setError("Incorrect code. Try again.");
-    }
-
-    setIsCheckingCode(false);
-  }, [
-    createUser,
-    requiresAdditionalDetails,
+  const isValidPhoneNumberOTP = phoneNumberOTPValidation.safeParse({
     phoneNumberOTP,
-    router,
-    verifyPhoneNumberOTP,
-  ]);
+  }).success;
 
-  const resendCode = useCallback(async () => {
-    setError(null);
-    setIsSendingCode(true);
-
-    try {
-      await signInWithPhoneNumber(signUpFlowParams.phoneNumber);
-    } catch (err) {
-      setError("Error sending code. Try again later.");
-    }
-
-    resetCooldown();
-    setIsSendingCode(false);
-  }, [resetCooldown, signInWithPhoneNumber, signUpFlowParams.phoneNumber]);
-
-  useEffect(() => {
-    const sendCode = async () => {
-      try {
-        await signInWithPhoneNumber(signUpFlowParams.phoneNumber);
-      } catch (err) {
-        setError("Error sending code. Try again later.");
-      }
-
-      setIsSendingCode(false);
-    };
-
-    void sendCode();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    const checkCode = async () => {
-      const isValidOTP = schemaValidation.safeParse({ phoneNumberOTP }).success;
-
-      isValidOTP && !isCheckingCode && !codeChecked && (await completeSignIn());
-      setCodeChecked(isValidOTP);
-    };
-
-    void checkCode();
-  }, [phoneNumberOTP, isCheckingCode, codeChecked, completeSignIn]);
-
-  const renderButtonContent = () => {
-    if (isCheckingCode || isSendingCode) {
-      return <Spinner size="small" color="$background" />;
-    }
-
-    if (cooldown !== 0) {
-      return (
-        <Text fontWeight="600" fontSize={16} color="lightgray">
-          Resend in {cooldown}s
-        </Text>
-      );
-    }
-
-    return (
-      <Text fontWeight="600" fontSize={16} color="black">
-        Send new code
-      </Text>
-    );
+  const handleNewUser = async (userId: string) => {
+    await createUser.mutateAsync({
+      userId,
+    });
+    router.replace("/user-info/welcome");
   };
 
-  const buttonDisabled = buttonIsDisabled || isCheckingCode || isSendingCode;
-  const onPress = cooldown === 0 && !buttonDisabled ? resendCode : undefined;
+  const handleExistingUser = async (userId: string) => {
+    await userOnboardingCompleted.mutateAsync({
+      userId,
+    });
+
+    userOnboardingCompleted.data
+      ? router.replace("/user-info/welcome")
+      : router.replace("/(app)/(bottom-tabs)/profile");
+  };
+
+  const onSubmit = async () => {
+    let userCredential: FirebaseAuthTypes.UserCredential | null = null;
+
+    try {
+      userCredential = await verifyPhoneNumberOTP(phoneNumberOTP);
+    } catch (err) {
+      setError(Error.INCORRECT_CODE);
+      return;
+    }
+
+    if (!userCredential) {
+      setError(Error.UNKNOWN_ERROR);
+      return;
+    }
+
+    const isNewUser = userCredential.additionalUserInfo?.isNewUser;
+    const userId = userCredential.user.uid;
+
+    if (!userId) {
+      setError(Error.UNKNOWN_ERROR);
+      return;
+    }
+
+    isNewUser ? await handleNewUser(userId) : await handleExistingUser(userId);
+  };
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : undefined}
-      style={{ flex: 1 }}
-    >
-      <View
-        flex={1}
-        backgroundColor="black"
-        padding="$6"
-        justifyContent="space-between"
-      >
-        <YStack flex={1} space="$8" alignItems="center">
-          <Text
-            alignSelf="center"
-            textAlign="center"
-            fontSize={22}
-            fontWeight="900"
-          >
-            Enter your 6 digit code
+    <KeyboardSafeView>
+      <View flex={1} padding="$4" backgroundColor="$background">
+        <YStack flex={1} gap="$4">
+          <Text fontSize="$8" fontWeight="bold">
+            Please enter your code.
           </Text>
 
-          <YStack space="$3">
-            <PinCodeInput
-              ref={pinCodeInputRef}
-              length={6}
-              onChange={(value) => setPhoneNumberOTP(value)}
-              onLayout={() => pinCodeInputRef.current?.focus()}
-              containerStyle={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "center",
-                height: 50, // fixed height for the input container
-              }}
-              pinInputStyle={{
-                width: 36,
-                height: "100%",
-                marginHorizontal: 4,
-                borderColor: "gray",
-                justifyContent: "center",
-                alignItems: "center",
-                borderTopWidth: 0,
-                borderLeftWidth: 0,
-                borderRightWidth: 0,
-                borderWidth: 2,
-              }}
-              activePinInputStyle={{
-                borderColor: "white",
-              }}
-              textStyle={{
-                fontSize: 36,
-                color: "white",
-                fontWeight: "900",
-              }}
+          <XStack gap="$2">
+            <Input
+              flex={1}
+              value={phoneNumberOTP}
+              onChangeText={setPhoneNumberOTP}
+              placeholder="6 Digit Code"
+              keyboardType="phone-pad"
             />
-          </YStack>
+          </XStack>
 
-          <Text
-            textAlign="center"
-            fontSize={14}
-            fontWeight="700"
-            color={error ? "$red11" : "$gray11"}
-          >
-            {error
-              ? error
-              : isSendingCode
-                ? "Sending code..."
-                : `Verification code sent to ${signUpFlowParams.phoneNumber}`}
-          </Text>
+          {error ? (
+            <Text color="$red9">{error}</Text>
+          ) : (
+            <Text color="$gray9">
+              Verification code sent to {signUpFlowParams.phoneNumber}
+            </Text>
+          )}
         </YStack>
 
-        <View alignSelf="stretch" marginTop="auto">
-          <Button
-            onPress={onPress}
-            borderWidth={0}
-            pressStyle={{
-              backgroundColor: "$gray12",
-            }}
-            backgroundColor={buttonDisabled ? "gray" : "white"}
-            disabled={buttonDisabled}
-          >
-            {renderButtonContent()}
-          </Button>
-        </View>
+        <Button
+          onPress={onSubmit}
+          disabled={!isValidPhoneNumberOTP}
+          disabledStyle={{ opacity: 0.5 }}
+        >
+          Welcome
+        </Button>
       </View>
-    </KeyboardAvoidingView>
+    </KeyboardSafeView>
   );
 };
 
