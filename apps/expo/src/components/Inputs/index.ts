f96d220d414d80707ab3335ea9654f9c6961ba29
Einsight <ghaithkhalil2004@gmail.com>
2024-04-30 00:25:18 -0400

    moved to screen

--- apps/expo/src/components/Inputs/index.ts
+++ apps/expo/src/components/Inputs/index.ts
@@ -1,4 +1,3 @@
 export { default as UnderlineInput } from "./UnderlineInput";
-export { default as PhoneNumberInput } from "./PhoneNumberInput";
 export { default as PinCodeInput } from "./PinCodeInput";
 export { default as BirthdateInput } from "./BirthdateInput";
