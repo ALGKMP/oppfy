674792ab971aec2f2b922c242afd989b23815464
Einsight <ghaithkhalil2004@gmail.com>
2024-04-30 15:43:57 -0400

    cleanup

--- packages/validators/src/shared/shared.user.schema.ts
+++ packages/validators/src/shared/shared.user.schema.ts
@@ -2,23 +2,6 @@ import type { CountryCode } from "libphonenumber-js";
 import { isValidPhoneNumber } from "libphonenumber-js";
 import { z } from "zod";
 
-// export const sharedUserSchema = {
-//   userId: z.string().uuid(),
-//   profileId: z.string().uuid(),
-//   fullName: z.string().min(1),
-//   username: z.string().min(1),
-//   dateOfBirth: z.date(),
-
-//   phoneNumber: z
-//     .object({
-//       phoneNumber: z.string(),
-//       countryCode: z.string(),
-//     })
-//     .refine((data) =>
-//       isValidPhoneNumber(data.phoneNumber, data.countryCode as CountryCode),
-//     ),
-// };
-
 export const userId = z.string().uuid();
 export const profileId = z.string().uuid();
 export const fullName = z.string().min(1);
