d4c0e4ab89b04e23f136458be5316fb3bccf8bbe
Einsight <ghaithkhalil2004@gmail.com>
2024-04-30 00:33:35 -0400

    removed

--- apps/expo/src/components/Inputs/index.ts
+++ apps/expo/src/components/Inputs/index.ts
@@ -1,3 +1,2 @@
 export { default as UnderlineInput } from "./UnderlineInput";
-export { default as PinCodeInput } from "./PinCodeInput";
 export { default as BirthdateInput } from "./BirthdateInput";
