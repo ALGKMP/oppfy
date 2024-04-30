8b342dcfbdce5b2229f9a1c4b64e0c0f3230922c
Einsight <ghaithkhalil2004@gmail.com>
2024-04-30 02:24:02 -0400

    full rewrite

--- apps/expo/src/app/(onboarding)/user-info/full-name.tsx
+++ apps/expo/src/app/(onboarding)/user-info/full-name.tsx
@@ -1,117 +1,16 @@
-// import React, { useMemo, useRef, useState } from "react";
-// import type { TextInput } from "react-native";
-// import { KeyboardAvoidingView, Platform } from "react-native";
-// import { useRouter } from "expo-router";
-// import { Button, Input, Text, View, YStack } from "tamagui";
-// import * as z from "zod";
-
-// import { api } from "~/utils/api";
-
-// const schemaValidation = z.object({
-//   firstName: z.string().min(2),
-// });
-
-// const FirstName = () => {
-//   const router = useRouter();
-
-//   const [name, setName] = useState("");
-//   const firstNameInputRef = useRef<TextInput | null>(null);
-
-//   const updateName = api.user.updateName.useMutation();
-
-//   const firstNameIsValid = useMemo(
-//     () => schemaValidation.safeParse({ firstName: name }).success,
-//     [name],
-//   );
-
-//   const onPress = async () => {
-//     await updateName.mutateAsync({
-//       name: name,
-//     });
-
-//     router.push("/user-info/date-of-birth");
-//   };
-
-//   return (
-//     <KeyboardAvoidingView
-//       behavior={Platform.OS === "ios" ? "padding" : undefined}
-//       style={{ flex: 1 }}
-//     >
-//       <View
-//         flex={1}
-//         backgroundColor="black"
-//         padding="$6"
-//         justifyContent="space-between"
-//       >
-//         <YStack flex={1} space="$8" alignItems="center">
-//           <Text
-//             alignSelf="center"
-//             textAlign="center"
-//             fontSize={22}
-//             fontWeight="900"
-//           >
-//             What&apos;s your first name?
-//           </Text>
-
-//           <YStack space="$3">
-//             <Input
-//               ref={firstNameInputRef}
-//               value={name}
-//               onChangeText={setName}
-//               onLayout={() => firstNameInputRef.current?.focus()}
-//               textAlign="center"
-//               backgroundColor="transparent"
-//               height={50}
-//               fontSize={36}
-//               fontWeight="900"
-//               fontFamily="$mono"
-//               borderWidth={0}
-//             />
-//           </YStack>
-//         </YStack>
-
-//         <Button
-//           onPress={onPress}
-//           borderWidth={0}
-//           pressStyle={{
-//             backgroundColor: "$gray12",
-//           }}
-//           backgroundColor={firstNameIsValid ? "white" : "gray"}
-//           disabled={!firstNameIsValid}
-//         >
-//           <Text
-//             color={firstNameIsValid ? "black" : "lightgray"}
-//             fontWeight="600"
-//             fontSize={16}
-//           >
-//             Next
-//           </Text>
-//         </Button>
-//       </View>
-//     </KeyboardAvoidingView>
-//   );
-// };
-
-// export default FirstName;
-
 import React, { useState } from "react";
 import { useRouter } from "expo-router";
-import type { FirebaseAuthTypes } from "@react-native-firebase/auth";
-import auth from "@react-native-firebase/auth";
 import { Button, Input, Text, View, XStack, YStack } from "tamagui";
 import * as z from "zod";
 
 import { KeyboardSafeView } from "~/components/SafeViews";
-import { useSession } from "~/contexts/SessionsContext";
-import useParams from "~/hooks/useParams";
 import { api } from "~/utils/api";
 
 const fullNameValidation = z
-    .string()
-    .min(3)
-    .max(50)
-    .regex(/^[a-zA-Z]+([ '-][a-zA-Z]+)*$/),
-
+  .string()
+  .min(3)
+  .max(50)
+  .regex(/^[a-zA-Z]+([ '-][a-zA-Z]+)*$/);
 
 const FullName = () => {
   const router = useRouter();
@@ -124,7 +23,7 @@ const FullName = () => {
 
   const onSubmit = async () => {
     await updateName.mutateAsync({
-      name: fullName
+      fullName,
     });
 
     router.push("/user-info/date-of-birth");
@@ -159,8 +58,5 @@ const FullName = () => {
     </KeyboardSafeView>
   );
 };
-
-// ! This is for testing purposes only, do not use in production
-auth().settings.appVerificationDisabledForTesting = true;
 
 export default FullName;
