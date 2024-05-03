4414d6662dd41b879fd6a8289ef2f431e4e55e36
Einsight <ghaithkhalil2004@gmail.com>
2024-05-02 20:11:43 -0400

    ui rewrite

--- apps/expo/src/app/(onboarding)/user-info/date-of-birth.tsx
+++ apps/expo/src/app/(onboarding)/user-info/date-of-birth.tsx
@@ -1,117 +1,128 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
-import type { TextInput } from "react-native";
-import { KeyboardAvoidingView, Platform, SafeAreaView } from "react-native";
-import { useRouter } from "expo-router";
-import { Button, Text, View, YStack } from "tamagui";
-import * as z from "zod";
+// import React, { useEffect, useMemo, useRef, useState } from "react";
+// import type { TextInput } from "react-native";
+// import { KeyboardAvoidingView, Platform, SafeAreaView } from "react-native";
+// import { useRouter } from "expo-router";
+// import { Button, Text, View, YStack } from "tamagui";
+// import * as z from "zod";
 
-import { Date } from "@acme/utils";
+// import { Date } from "@acme/utils";
 
-import { BirthdateInput } from "~/components/Inputs";
-import { api } from "~/utils/api";
+// import { BirthdateInput } from "~/components/Inputs";
+// import { api } from "~/utils/api";
 
-const schemaValidation = z.object({
-  dateOfBirth: z.string().refine(
-    (date) =>
-      date.length === 8 && // needed to short-circuit next check
-      new Date.AgeChecker(Date.convertToDateObject(date, "MMDDYYYY"))
-        .isAtLeast(13)
-        .isAtMost(100)
-        .checkValid(),
-  ),
-});
+// const schemaValidation = z.object({
+//   dateOfBirth: z.string().refine(
+//     (date) =>
+//       date.length === 8 && // needed to short-circuit next check
+//       new Date.AgeChecker(Date.convertToDateObject(date, "MMDDYYYY"))
+//         .isAtLeast(13)
+//         .isAtMost(100)
+//         .checkValid(),
+//   ),
+// });
+
+// const DateOfBirth = () => {
+//   const router = useRouter();
+
+//   const [dateOfBirth, setDateOfBirth] = useState("");
+//   const dateOfBirthInputRef = useRef<TextInput | null>(null);
+
+//   const updateDateOfBirth = api.user.updateDateOfBirth.useMutation();
+
+//   const dataOfBirthIsValid = useMemo(
+//     () => schemaValidation.safeParse({ dateOfBirth }).success,
+//     [dateOfBirth],
+//   );
+
+//   const onPress = async () => {
+//     await updateDateOfBirth.mutateAsync({
+//       dateOfBirth: Date.convertToDateObject(dateOfBirth, "MMDDYYYY"),
+//     });
+
+//     router.push("/user-info/username");
+//   };
+
+//   return (
+//     <KeyboardAvoidingView
+//       behavior={Platform.OS === "ios" ? "padding" : undefined}
+//       style={{ flex: 1 }}
+//     >
+//       <View
+//         flex={1}
+//         padding="$6"
+//         backgroundColor="black"
+//         justifyContent="space-between"
+//       >
+//         <YStack flex={1} space="$8" alignItems="center">
+//           <Text
+//             fontSize={22}
+//             fontWeight="900"
+//             alignSelf="center"
+//             textAlign="center"
+//           >
+//             When's your birthday?
+//           </Text>
+
+//           <YStack space="$3">
+//             <BirthdateInput
+//               ref={dateOfBirthInputRef}
+//               value={dateOfBirth}
+//               onChange={(value) => setDateOfBirth(value)}
+//               onLayout={() => dateOfBirthInputRef.current?.focus()}
+//               containerStyle={{
+//                 position: "relative",
+//                 alignItems: "center",
+//                 height: 50,
+//               }}
+//               inputStyle={{}}
+//               charStyle={{
+//                 fontFamily: "$chivoMono",
+//                 textAlign: "center",
+//                 fontSize: 36,
+//                 fontWeight: "900",
+//               }}
+//               typedCharStyle={{}}
+//               untypedCharStyle={{
+//                 color: "$gray6",
+//               }}
+//               slashCharStyle={{
+//                 color: "$gray6",
+//               }}
+//             />
+//           </YStack>
+//         </YStack>
+
+//         <Button
+//           onPress={onPress}
+//           borderWidth={0}
+//           pressStyle={{
+//             backgroundColor: "$gray12",
+//           }}
+//           backgroundColor={dataOfBirthIsValid ? "white" : "gray"}
+//           disabled={!dataOfBirthIsValid}
+//         >
+//           <Text
+//             color={dataOfBirthIsValid ? "black" : "lightgray"}
+//             fontWeight="600"
+//             fontSize={16}
+//           >
+//             Next
+//           </Text>
+//         </Button>
+//       </View>
+//     </KeyboardAvoidingView>
+//   );
+// };
+
+// export default DateOfBirth;
+
+import React, { useState } from "react";
+import DatePicker from "react-native-date-picker";
 
 const DateOfBirth = () => {
-  const router = useRouter();
+  const [date, setDate] = useState(new Date());
 
-  const [dateOfBirth, setDateOfBirth] = useState("");
-  const dateOfBirthInputRef = useRef<TextInput | null>(null);
-
-  const updateDateOfBirth = api.user.updateDateOfBirth.useMutation();
-
-  const dataOfBirthIsValid = useMemo(
-    () => schemaValidation.safeParse({ dateOfBirth }).success,
-    [dateOfBirth],
-  );
-
-  const onPress = async () => {
-    await updateDateOfBirth.mutateAsync({
-      dateOfBirth: Date.convertToDateObject(dateOfBirth, "MMDDYYYY"),
-    });
-
-    router.push("/user-info/username");
-  };
-
-  return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : undefined}
-      style={{ flex: 1 }}
-    >
-      <View
-        flex={1}
-        padding="$6"
-        backgroundColor="black"
-        justifyContent="space-between"
-      >
-        <YStack flex={1} space="$8" alignItems="center">
-          <Text
-            fontSize={22}
-            fontWeight="900"
-            alignSelf="center"
-            textAlign="center"
-          >
-            When's your birthday?
-          </Text>
-
-          <YStack space="$3">
-            <BirthdateInput
-              ref={dateOfBirthInputRef}
-              value={dateOfBirth}
-              onChange={(value) => setDateOfBirth(value)}
-              onLayout={() => dateOfBirthInputRef.current?.focus()}
-              containerStyle={{
-                position: "relative",
-                alignItems: "center",
-                height: 50,
-              }}
-              inputStyle={{}}
-              charStyle={{
-                fontFamily: "$chivoMono",
-                textAlign: "center",
-                fontSize: 36,
-                fontWeight: "900",
-              }}
-              typedCharStyle={{}}
-              untypedCharStyle={{
-                color: "$gray6",
-              }}
-              slashCharStyle={{
-                color: "$gray6",
-              }}
-            />
-          </YStack>
-        </YStack>
-
-        <Button
-          onPress={onPress}
-          borderWidth={0}
-          pressStyle={{
-            backgroundColor: "$gray12",
-          }}
-          backgroundColor={dataOfBirthIsValid ? "white" : "gray"}
-          disabled={!dataOfBirthIsValid}
-        >
-          <Text
-            color={dataOfBirthIsValid ? "black" : "lightgray"}
-            fontWeight="600"
-            fontSize={16}
-          >
-            Next
-          </Text>
-        </Button>
-      </View>
-    </KeyboardAvoidingView>
-  );
+  return <DatePicker date={date} onDateChange={setDate} />;
 };
 
 export default DateOfBirth;
