9f422ae6d1161cc61129c0cdb45b67fa54076514
Einsight <ghaithkhalil2004@gmail.com>
2024-05-09 15:27:29 -0400

    causing loading spinner on each rerender

--- apps/expo/src/app/(app)/_layout.tsx
+++ apps/expo/src/app/(app)/_layout.tsx
@@ -1,21 +1,16 @@
 import { Redirect } from "expo-router";
 import { View } from "tamagui";
 
-import { LoadingIndicatorOverlay } from "~/components/Overlays";
 import { usePermissions } from "~/contexts/PermissionsContext";
 import { useSession } from "~/contexts/SessionContext";
 import { Stack } from "~/layouts";
 
 const AppLayout = () => {
-  const { isLoading: permissionsIsLoading, permissions } = usePermissions();
-  const { isLoading: sessionIsLoading, isSignedIn } = useSession();
+  const { isLoading: _permissionsIsLoading, permissions } = usePermissions();
+  const { isLoading: _sessionIsLoading, isSignedIn } = useSession();
 
   const requiredPermissions =
     permissions.camera && permissions.contacts && permissions.notifications;
-
-  if (sessionIsLoading || permissionsIsLoading) {
-    return <LoadingIndicatorOverlay />;
-  }
 
   if (!isSignedIn) {
     return <Redirect href="/(onboarding)" />;
