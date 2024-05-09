c611d75941286306a22f7ef514f85dc557a212d1
Einsight <ghaithkhalil2004@gmail.com>
2024-05-09 15:41:34 -0400

    added local params

--- apps/expo/src/app/(app)/(profile-actions)/edit-profile.tsx
+++ apps/expo/src/app/(app)/(profile-actions)/edit-profile.tsx
@@ -1,20 +1,13 @@
 import React from "react";
-import { SearchParams, useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
 import { Text } from "tamagui";
 
 import { ScreenBaseView } from "~/components/Views";
-import useParams from "~/hooks/useParams";
-
-export interface EditProfileParams {
-  name: string;
-  bio?: string;
-}
 
 const EditProfile = () => {
-  // const signUpFlowParams = useParams<EditProfileParams>();
-  const thing = useLocalSearchParams<{
+  const { name, bio } = useLocalSearchParams<{
     name: string;
-    bio?: string;
+    bio: string;
   }>();
 
   return (
