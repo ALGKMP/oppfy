dd23488035efd360ac51fa5faff358897e7a3f51
Einsight <ghaithkhalil2004@gmail.com>
2024-05-09 15:27:48 -0400

    init

---
+++ apps/expo/src/app/(app)/(profile-actions)/_layout.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useRouter } from "expo-router";
+import type {
+  HeaderBackButtonProps,
+  NativeStackHeaderProps,
+} from "@react-navigation/native-stack/src/types";
+import { ChevronLeft } from "@tamagui/lucide-icons";
+import { Text, useTheme } from "tamagui";
+
+import { Header as BaseHeader } from "~/components/Headers";
+import { Stack } from "~/layouts";
+
+const ProfileActionsLayout = () => {
+  const theme = useTheme();
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: theme.background.val,
+      }}
+    >
+      <Stack
+        screenOptions={{
+          headerLeft: (props) => <HeaderLeft {...props} />,
+          header: (props) => <Header {...props} />,
+        }}
+      >
+        <Stack.Screen
+          name="edit-profile"
+          options={{
+            title: "Edit Profile",
+          }}
+        />
+
+        <Stack.Screen
+          name="share-profile"
+          options={{ title: "Share Profile" }}
+        />
+      </Stack>
+    </SafeAreaView>
+  );
+};
+
+type HeaderLeftProps = HeaderBackButtonProps;
+
+type HeaderProps = NativeStackHeaderProps;
+
+const HeaderLeft = ({ canGoBack }: HeaderLeftProps) => {
+  const router = useRouter();
+
+  return (
+    <TouchableOpacity
+      hitSlop={10}
+      onPress={() => {
+        canGoBack ? void router.back() : null;
+      }}
+    >
+      <ChevronLeft />
+    </TouchableOpacity>
+  );
+};
+
+const Header = ({ navigation, options }: HeaderProps) => (
+  <BaseHeader
+    HeaderLeft={
+      options.headerLeft
+        ? options.headerLeft({
+            canGoBack: navigation.canGoBack(),
+            tintColor: options.headerTintColor,
+          })
+        : undefined
+    }
+    HeaderTitle={
+      typeof options.headerTitle === "function" ? (
+        options.headerTitle({
+          children: options.title ?? "",
+          tintColor: options.headerTintColor,
+        })
+      ) : options.title ? (
+        <Text fontSize="$5" fontWeight="bold">
+          {options.title}
+        </Text>
+      ) : null
+    }
+    HeaderRight={
+      options.headerRight
+        ? options.headerRight({
+            canGoBack: navigation.canGoBack(),
+            tintColor: options.headerTintColor,
+          })
+        : undefined
+    }
+  />
+);
+
+export default ProfileActionsLayout;
