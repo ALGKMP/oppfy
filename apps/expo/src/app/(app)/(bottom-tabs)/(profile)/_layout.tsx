3f460efaeb13d8d078e584963fc68d8a0c02cff8
Einsight <ghaithkhalil2004@gmail.com>
2024-05-09 00:31:21 -0400

    started work on static header

--- apps/expo/src/app/(app)/(bottom-tabs)/(profile)/_layout.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(profile)/_layout.tsx
@@ -1,19 +1,24 @@
 import { useEffect } from "react";
-import { Camera, Grid3x3 } from "@tamagui/lucide-icons";
-import { View } from "tamagui";
+import { Pressable } from "react-native";
+import { useRouter } from "expo-router";
+import { Camera, Grid3x3, MoreHorizontal } from "@tamagui/lucide-icons";
+import { Avatar, View, YStack } from "tamagui";
 
+import { Header } from "~/components/Headers";
 import { TopTabBar } from "~/components/TabBars";
 import { TopTabs } from "~/layouts";
 import { api } from "~/utils/api";
 
 const ProfileLayout = () => {
-  const basicProfile = api.profile.getBasicProfile.useQuery({
+  const router = useRouter();
+
+  const { data: profileData } = api.profile.getBasicProfile.useQuery({
     userId: "OZK0Mq45uIY75FaZdI2OdUkg5Cx1",
   });
 
   useEffect(() => {
-    console.log(basicProfile.data);
-  }, [basicProfile]);
+    console.log(profileData);
+  }, [profileData]);
 
   return (
     <View
@@ -22,7 +27,42 @@ const ProfileLayout = () => {
         backgroundColor: "$background",
       }}
     >
-      <TopTabs tabBar={(props) => <TopTabBar {...props} />}>
+      <TopTabs
+        tabBar={(props) => (
+          <YStack>
+            <>
+              <Header
+                title={profileData?.username}
+                HeaderRight={
+                  <Pressable onPress={() => router.push("/(app)/(settings)")}>
+                    {({ pressed }) => (
+                      <MoreHorizontal
+                        size="$1"
+                        style={{ opacity: pressed ? 0.5 : 1 }}
+                      />
+                    )}
+                  </Pressable>
+                }
+              />
+              <TopTabBar {...props} />
+            </>
+
+            <YStack
+              padding="$4"
+              alignItems="center"
+              backgroundColor="$background"
+            >
+              <Avatar circular size="$10">
+                <Avatar.Image
+                  accessibilityLabel="Cam"
+                  src={profileData?.profilePictureUrl}
+                />
+                <Avatar.Fallback backgroundColor="$blue10" />
+              </Avatar>
+            </YStack>
+          </YStack>
+        )}
+      >
         <TopTabs.Screen
           name="media-of-you"
           options={{
@@ -32,6 +72,7 @@ const ProfileLayout = () => {
         <TopTabs.Screen
           name="media-of-friends-you-posted"
           options={{
+            title: "Test",
             tabBarLabel: () => <Camera />,
           }}
         />
