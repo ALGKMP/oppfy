1e11f38534f45b863ea79ba41335c27895525fba
Einsight <ghaithkhalil2004@gmail.com>
2024-05-08 20:54:21 -0400

    fully functional

--- apps/expo/src/app/(app)/(settings)/blocked-users.tsx
+++ apps/expo/src/app/(app)/(settings)/blocked-users.tsx
@@ -1,7 +1,8 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useHeaderHeight } from "@react-navigation/elements";
 import { FlashList } from "@shopify/flash-list";
 import { UserRoundX } from "@tamagui/lucide-icons";
+import { Skeleton } from "moti/skeleton";
 import {
   Avatar,
   Button,
@@ -15,11 +16,18 @@ import {
 import { AlertDialog } from "~/components/Dialogs";
 import { EmptyPlaceholder } from "~/components/UIPlaceholders";
 import { ScreenBaseView } from "~/components/Views";
+import type { RouterOutputs } from "~/utils/api";
 import { api } from "~/utils/api";
+
+type UserProfile = RouterOutputs["user"]["getBlockedUsers"]["items"][0];
 
 const BlockedUsers = () => {
   const utils = api.useUtils();
   const headerHeight = useHeaderHeight();
+
+  const placeholderData = useMemo(() => {
+    return Array.from({ length: 10 }, () => null);
+  }, []);
 
   const unblockUser = api.user.unblockUser.useMutation({
     onMutate: async (newData) => {
@@ -52,14 +60,13 @@ const BlockedUsers = () => {
     },
     onSettled: async () => {
       // Sync with server once mutation has settled
-      // await utils.user.getBlockedUsers.invalidate();
-      console.log("INVALIDATING THIS SHIT");
       await utils.user.getBlockedUsers.invalidate();
     },
   });
 
   const {
     data: blockedUsersData,
+    isLoading,
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
@@ -86,6 +93,10 @@ const BlockedUsers = () => {
     );
   }, [blockedUsersData]);
 
+  const blockedUsersItems = useMemo(() => {
+    return blockedUsersData?.pages.flatMap((page) => page.items);
+  }, [blockedUsersData]);
+
   const handleOnEndReached = async () => {
     if (!isFetchingNextPage && hasNextPage) {
       await fetchNextPage();
@@ -94,72 +105,40 @@ const BlockedUsers = () => {
 
   return (
     <ScreenBaseView>
-      {blockedUsersData?.pages[0]?.items.length ? (
+      {isLoading || itemCount ? (
         <YStack flex={1} gap="$2">
           <FlashList
-            data={blockedUsersData.pages.flatMap((page) => page.items)}
+            data={isLoading ? placeholderData : blockedUsersItems}
+            estimatedItemSize={75}
+            onEndReached={handleOnEndReached}
+            showsVerticalScrollIndicator={false}
             ListHeaderComponent={
               <SizableText size="$2" theme="alt1" marginBottom="$2">
                 BLOCKED USERS
               </SizableText>
             }
-            estimatedItemSize={75}
-            onEndReached={handleOnEndReached}
             renderItem={({ item, index }) => {
               const isFirstInGroup = index === 0;
               const isLastInGroup = index === itemCount - 1;
 
               return (
-                <ListItem
-                  size="$4.5"
-                  hoverTheme={false}
-                  pressTheme={false}
-                  padding={12}
-                  borderColor="$gray4"
-                  borderWidth={1}
-                  borderBottomWidth={0}
-                  {...(isFirstInGroup && {
-                    borderTopLeftRadius: 10,
-                    borderTopRightRadius: 10,
-                  })}
-                  {...(isLastInGroup && {
-                    borderBottomWidth: 1,
-                    borderBottomLeftRadius: 10,
-                    borderBottomRightRadius: 10,
-                  })}
-                >
-                  <XStack flex={1} alignItems="center">
-                    <XStack flex={1} alignItems="center" gap="$2">
-                      <Avatar circular size="$5">
-                        {item.profilePictureUrl && (
-                          <Avatar.Image
-                            accessibilityLabel="Cam"
-                            {...(item.profilePictureUrl && {
-                              src: item.profilePictureUrl,
-                            })}
-                          />
-                        )}
-                      </Avatar>
-
-                      <YStack>
-                        <SizableText size="$5">{item.name}</SizableText>
-                        <SizableText size="$3" theme="alt1">
-                          {item.username}
-                        </SizableText>
-                      </YStack>
-                    </XStack>
-                    <AlertDialog
-                      title={`Unblock ${item.name}`}
-                      description={`Are you sure you want to unblock ${item.name}?`}
-                      trigger={
-                        <Button size="$3" icon={<UserRoundX size="$1" />}>
-                          Unblock
-                        </Button>
-                      }
-                      onAccept={() => handleUnblock(item.userId)}
+                <View>
+                  {item === null ? (
+                    <BlockedUserListItem
+                      loading
+                      isFirstInGroup={isFirstInGroup}
+                      isLastInGroup={isLastInGroup}
                     />
-                  </XStack>
-                </ListItem>
+                  ) : (
+                    <BlockedUserListItem
+                      item={item}
+                      loading={false}
+                      onUnblock={handleUnblock}
+                      isFirstInGroup={isFirstInGroup}
+                      isLastInGroup={isLastInGroup}
+                    />
+                  )}
+                </View>
               );
             }}
           />
@@ -174,6 +153,104 @@ const BlockedUsers = () => {
         </View>
       )}
     </ScreenBaseView>
+  );
+};
+
+interface BaseProps {
+  isFirstInGroup: boolean;
+  isLastInGroup: boolean;
+}
+
+interface LoadingProps extends BaseProps {
+  loading: true;
+}
+
+interface LoadedProps extends BaseProps {
+  loading: false;
+  item: UserProfile;
+  onUnblock: (blockedUserId: string) => void;
+}
+
+type BlockedUserListItemProps = LoadingProps | LoadedProps;
+
+const BlockedUserListItem = (props: BlockedUserListItemProps) => {
+  const [actionDialogVisible, setActionDialogVisible] = useState(false);
+
+  return (
+    <Skeleton.Group show={props.loading}>
+      <ListItem
+        size="$4.5"
+        hoverTheme={false}
+        pressTheme={false}
+        padding={12}
+        borderColor="$gray4"
+        borderWidth={1}
+        borderBottomWidth={0}
+        {...(props.isFirstInGroup && {
+          borderTopLeftRadius: 10,
+          borderTopRightRadius: 10,
+        })}
+        {...(props.isLastInGroup && {
+          borderBottomWidth: 1,
+          borderBottomLeftRadius: 10,
+          borderBottomRightRadius: 10,
+        })}
+      >
+        <XStack flex={1} alignItems="center">
+          <XStack flex={1} alignItems="center" gap="$2">
+            <Skeleton radius={100}>
+              <Avatar circular size="$5">
+                <Avatar.Image
+                  src={
+                    props.loading
+                      ? undefined
+                      : props.item.profilePictureUrl ?? undefined
+                  }
+                />
+              </Avatar>
+            </Skeleton>
+
+            <YStack>
+              <Skeleton width={100}>
+                <SizableText size="$3" theme="alt1">
+                  {props.loading ? "" : props.item.name}
+                </SizableText>
+              </Skeleton>
+              <Skeleton width={100}>
+                <SizableText size="$3" theme="alt1">
+                  {props.loading ? "" : props.item.username}
+                </SizableText>
+              </Skeleton>
+            </YStack>
+          </XStack>
+          <AlertDialog
+            title={`Unblock ${props.loading ? "" : props.item.name}`}
+            description={`Are you sure you want to unblock ${props.loading ? "" : props.item.name}?`}
+            trigger={
+              <Skeleton>
+                <Button
+                  size="$3"
+                  icon={<UserRoundX size="$1" />}
+                  {...(!props.loading && {
+                    onPress: () => setActionDialogVisible(true),
+                  })}
+                >
+                  Unblock
+                </Button>
+              </Skeleton>
+            }
+            {...(!props.loading && {
+              onAccept: () => {
+                props.onUnblock(props.item.userId);
+                setActionDialogVisible(false);
+              },
+            })}
+            onCancel={() => setActionDialogVisible(false)}
+            isVisible={actionDialogVisible}
+          />
+        </XStack>
+      </ListItem>
+    </Skeleton.Group>
   );
 };
 
