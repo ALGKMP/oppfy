c3b75c377118fcf7d0fbadfa020824a89b4bf321
Antony <antonylbrussolo@gmail.com>
2024-05-12 18:36:53 -0400

    crud mux operations

--- packages/api/src/repositories/mux.ts
+++ packages/api/src/repositories/mux.ts
@@ -3,12 +3,27 @@ import { mux } from "@acme/db";
 import { handleMuxErrors } from "../errors";
 
 export class MuxRepository {
-  
+
   @handleMuxErrors
   async createDirectUpload() {
     return await mux.video.uploads.create({
       new_asset_settings: { playback_policy: ["public"], encoding_tier: "baseline"},
       cors_origin: '*',
     });
+  }
+
+  @handleMuxErrors
+  async getAsset(assetId: string) {
+    return await mux.video.assets.retrieve(assetId);
+  }
+
+  @handleMuxErrors
+  async deleteAsset(assetId: string) {
+    return await mux.video.assets.delete(assetId);
+  }
+
+  @handleMuxErrors
+  async updateAsset(assetId: string, settings: object) {
+    return await mux.video.assets.update(assetId, settings);
   }
 }
