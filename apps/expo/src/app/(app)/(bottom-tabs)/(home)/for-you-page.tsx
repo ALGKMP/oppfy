2b6dafa9834948012ee67107dc44aaa52ff661fe
Einsight <ghaithkhalil2004@gmail.com>
2024-05-12 00:07:07 -0400

    refactor

---
+++ apps/expo/src/app/(app)/(bottom-tabs)/(home)/for-you-page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "tamagui";
+
+const ForYouPage = () => {
+  return (
+    <View flex={1} backgroundColor="black" paddingHorizontal="$4">
+      <Text>Reccomended</Text>
+    </View>
+  );
+};
+
+export default ForYouPage;
