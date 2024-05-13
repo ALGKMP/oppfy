f52d3f34b6d85d1947150b8c74a80378cf5e66d4
Einsight <ghaithkhalil2004@gmail.com>
2024-05-13 01:22:53 -0400

    reworked to be used as both a trigger and controlled component

--- apps/expo/src/components/Sheets/ActionSheet.tsx
+++ apps/expo/src/components/Sheets/ActionSheet.tsx
@@ -1,5 +1,11 @@
-import { useEffect, useState } from "react";
-import { Animated, Easing, Modal } from "react-native";
+import React, { ReactElement, useCallback, useEffect, useState } from "react";
+import {
+  Animated,
+  ButtonProps,
+  Easing,
+  Modal,
+  TouchableOpacityProps,
+} from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import type { ParagraphProps, SizableTextProps } from "tamagui";
 import {
@@ -26,18 +32,21 @@ export interface ActionSheetProps {
 
   buttonOptions: ButtonOption[];
 
+  trigger?: ReactElement<ButtonProps>;
   isVisible?: boolean;
   onClose?: () => void;
 }
 
 const ActionSheet = ({
-  isVisible,
-  onClose,
   title,
   titleProps,
   subtitle,
   subtitleProps,
   buttonOptions,
+
+  trigger,
+  isVisible,
+  onClose,
 }: ActionSheetProps) => {
   const insets = useSafeAreaInsets();
 
@@ -45,114 +54,131 @@ const ActionSheet = ({
   const [slideAnimation] = useState(new Animated.Value(300));
   const [backgroundOpacity] = useState(new Animated.Value(0));
 
+  const closeModal = useCallback(() => {
+    setShowModal(true);
+    Animated.parallel([
+      Animated.timing(backgroundOpacity, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+      Animated.timing(slideAnimation, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, [backgroundOpacity, slideAnimation]);
+
+  const openModal = useCallback(() => {
+    Animated.parallel([
+      Animated.timing(backgroundOpacity, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+        easing: Easing.in(Easing.ease),
+      }),
+      Animated.timing(slideAnimation, {
+        toValue: 300,
+        duration: 300,
+        useNativeDriver: true,
+        easing: Easing.in(Easing.ease),
+      }),
+    ]).start(() => {
+      setShowModal(false);
+    });
+  }, [backgroundOpacity, slideAnimation]);
+
   useEffect(() => {
-    const closeModal = () => {
-      setShowModal(true);
-      Animated.parallel([
-        Animated.timing(backgroundOpacity, {
-          toValue: 1,
-          duration: 300,
-          useNativeDriver: true,
-        }),
-        Animated.timing(slideAnimation, {
-          toValue: 0,
-          duration: 300,
-          useNativeDriver: true,
-        }),
-      ]).start();
-    };
-
-    const openModal = () => {
-      Animated.parallel([
-        Animated.timing(backgroundOpacity, {
-          toValue: 0,
-          duration: 300,
-          useNativeDriver: true,
-          easing: Easing.in(Easing.ease),
-        }),
-        Animated.timing(slideAnimation, {
-          toValue: 300,
-          duration: 300,
-          useNativeDriver: true,
-          easing: Easing.in(Easing.ease),
-        }),
-      ]).start(() => {
-        setShowModal(false);
-      });
-    };
-
     isVisible ? closeModal() : openModal();
-  }, [isVisible, backgroundOpacity, slideAnimation]);
+  }, [isVisible, backgroundOpacity, slideAnimation, closeModal, openModal]);
+
+  const TriggerElement = trigger
+    ? React.cloneElement(trigger, {
+        onPress: () => openModal,
+      })
+    : null;
 
   return (
-    <Modal
-      animationType="none"
-      transparent={true}
-      visible={showModal}
-      onRequestClose={onClose}
-    >
-      <Animated.View
-        style={{
-          flex: 1,
-          justifyContent: "flex-end",
-          backgroundColor: "rgba(0, 0, 0, 0.5)",
-          opacity: backgroundOpacity,
-        }}
+    <>
+      {TriggerElement}
+      <Modal
+        animationType="none"
+        transparent={true}
+        visible={showModal}
+        onRequestClose={onClose}
       >
         <Animated.View
           style={{
-            transform: [{ translateY: slideAnimation }],
+            flex: 1,
+            justifyContent: "flex-end",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            opacity: backgroundOpacity,
           }}
         >
-          <YStack gap="$3" paddingHorizontal="$4" paddingBottom={insets.bottom}>
-            <YStack>
-              <YStack
-                padding="$4"
-                alignItems="center"
-                backgroundColor="$color4"
-                borderTopLeftRadius={9}
-                borderTopRightRadius={9}
-              >
-                <SizableText size="$5" fontWeight="bold" {...titleProps}>
-                  {title}
-                </SizableText>
+          <Animated.View
+            style={{
+              transform: [{ translateY: slideAnimation }],
+            }}
+          >
+            <YStack
+              gap="$3"
+              paddingHorizontal="$4"
+              paddingBottom={insets.bottom}
+            >
+              <YStack>
+                <YStack
+                  padding="$4"
+                  alignItems="center"
+                  backgroundColor="$color4"
+                  borderTopLeftRadius={9}
+                  borderTopRightRadius={9}
+                >
+                  <SizableText size="$5" fontWeight="bold" {...titleProps}>
+                    {title}
+                  </SizableText>
 
-                {subtitle && (
-                  <Paragraph textAlign="center" theme="alt2" {...subtitleProps}>
-                    {subtitle}
-                  </Paragraph>
-                )}
+                  {subtitle && (
+                    <Paragraph
+                      textAlign="center"
+                      theme="alt2"
+                      {...subtitleProps}
+                    >
+                      {subtitle}
+                    </Paragraph>
+                  )}
+                </YStack>
+
+                {buttonOptions.map((option, index) => (
+                  <View key={index}>
+                    <Separator theme="alt1" />
+                    <Button
+                      size="$5"
+                      borderTopLeftRadius={0}
+                      borderTopRightRadius={0}
+                      borderBottomLeftRadius={
+                        index === buttonOptions.length - 1 ? 9 : 0
+                      }
+                      borderBottomRightRadius={
+                        index === buttonOptions.length - 1 ? 9 : 0
+                      }
+                      onPress={option.onPress}
+                    >
+                      <SizableText size="$5" {...option.textProps}>
+                        {option.text}
+                      </SizableText>
+                    </Button>
+                  </View>
+                ))}
               </YStack>
-
-              {buttonOptions.map((option, index) => (
-                <View key={index}>
-                  <Separator theme="alt1" />
-                  <Button
-                    size="$5"
-                    borderTopLeftRadius={0}
-                    borderTopRightRadius={0}
-                    borderBottomLeftRadius={
-                      index === buttonOptions.length - 1 ? 9 : 0
-                    }
-                    borderBottomRightRadius={
-                      index === buttonOptions.length - 1 ? 9 : 0
-                    }
-                    onPress={option.onPress}
-                  >
-                    <SizableText size="$5" {...option.textProps}>
-                      {option.text}
-                    </SizableText>
-                  </Button>
-                </View>
-              ))}
+              <Button size="$5" color="$blue9" onPress={onClose}>
+                Cancel
+              </Button>
             </YStack>
-            <Button size="$5" color="$blue9" onPress={onClose}>
-              Cancel
-            </Button>
-          </YStack>
+          </Animated.View>
         </Animated.View>
-      </Animated.View>
-    </Modal>
+      </Modal>
+    </>
   );
 };
 
