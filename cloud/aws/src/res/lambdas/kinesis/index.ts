587c3b9ab9bc07200dbf57f23788be49f2282880
Einsight <ghaithkhalil2004@gmail.com>
2024-05-15 18:18:33 -0400

    init kinesis lambda

---
+++ cloud/aws/src/res/lambdas/kinesis/index.ts
@@ -0,0 +1,24 @@
+import type {
+  Context,
+  KinesisStreamEvent,
+  KinesisStreamRecord,
+} from "aws-lambda";
+
+import { db, eq, schema } from "@acme/db";
+
+export const handler = async (event: KinesisStreamEvent, _context: Context) => {
+  // for (const record of event.Records) {
+  //   const payload = Buffer.from(record.kinesis.data, "base64").toString(
+  //     "utf-8",
+  //   );
+  //   console.log("Received record:", payload);
+  // }
+
+  // get the first user
+  const userQuery = db.query.user.findFirst();
+  console.log("Query", userQuery);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Records processed successfully" }),
+  };
+};
