b21712e9c6ea428db00278849b972b6397fa9653
Einsight <ghaithkhalil2004@gmail.com>
2024-05-15 18:49:29 -0400

    removed s3

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -30,13 +30,7 @@ export const db = drizzle(connection, {
   mode: "default",
 });
 
-// export const s3 = new S3Client({
-//   region: "us-east-1",
-//   credentials: {
-//     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-//   },
-// });
+
 
 // export const openSearch = new OpenSearchClient({
 //   ...AwsSigv4Signer({
