84e1067e344a814b52b3c66404f16ddcdffd9ac0
Einsight <ghaithkhalil2004@gmail.com>
2024-05-16 16:45:44 -0400

    renamed for clarity

--- packages/db/src/schema/schema.ts
+++ packages/db/src/schema/schema.ts
@@ -67,7 +67,7 @@ export const profile = mySqlTable("Profile", {
   fullName: varchar("fullName", { length: 255 }),
   dateOfBirth: date("dateOfBirth"),
   bio: text("bio"),
-  profilePictureKey: varchar("url", { length: 255 })
+  profilePictureKey: varchar("profilePictureKey", { length: 255 })
     .default("profile-pictures/default.jpg")
     .notNull(),
   createdAt: timestamp("createdAt")
