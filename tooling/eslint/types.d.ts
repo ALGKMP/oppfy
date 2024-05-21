f13199b4b9c16f4a7601a82d4d23728045ecc0db
Einsight <ghaithkhalil2004@gmail.com>
2024-05-21 18:39:30 -0400

    fixed eslint

--- tooling/eslint/types.d.ts
+++ tooling/eslint/types.d.ts
@@ -47,6 +47,15 @@ declare module "eslint-plugin-react-hooks" {
   export const rules: Record<string, Rule.RuleModule>;
 }
 
+declare module "eslint-plugin-react-compiler" {
+  import type { Linter, Rule } from "eslint";
+
+  export const configs: {
+    recommended: { rules: Linter.RulesRecord };
+  };
+  export const rules: Record<string, Rule.RuleModule>;
+}
+
 declare module "@next/eslint-plugin-next" {
   import type { Linter, Rule } from "eslint";
 
