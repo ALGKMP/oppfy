f84bcaace94273249a9d24fff855c224ec5c7911
Einsight <ghaithkhalil2004@gmail.com>
2024-05-26 00:44:19 -0400

    added type assertion for RequireContext

--- apps/expo/index.tsx
+++ apps/expo/index.tsx
@@ -1,12 +1,13 @@
 import { registerRootComponent } from "expo";
 import { ExpoRoot } from "expo-router";
+import type { RequireContext } from "expo-router/build/types";
 import * as SplashScreen from "expo-splash-screen";
 
 void SplashScreen.preventAutoHideAsync();
 
 // Must be exported or Fast Refresh won't update the context
 export const App = () => {
-  const ctx = require.context("./src/app");
+  const ctx = require.context("./src/app") as RequireContext;
   return <ExpoRoot context={ctx} />;
 };
 
