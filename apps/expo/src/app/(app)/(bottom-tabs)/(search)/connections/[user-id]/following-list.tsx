a8fcb68c0ef44f8f5fd36358fe371ae37d7f6534
Einsight <ghaithkhalil2004@gmail.com>
2024-05-26 15:15:57 -0400

    using hook

--- apps/expo/src/app/(app)/(bottom-tabs)/(search)/connections/[user-id]/following-list.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(search)/connections/[user-id]/following-list.tsx
@@ -9,6 +9,7 @@ import { VirtualizedListItem } from "~/components/ListItems";
 import type { ButtonProps } from "~/components/ListItems/VirtualizedListItem";
 import { EmptyPlaceholder } from "~/components/UIPlaceholders";
 import { BaseScreenView } from "~/components/Views";
+import { useFollowMutations } from "~/hooks/connections";
 import type { RouterOutputs } from "~/utils/api";
 import { api } from "~/utils/api";
 import { PLACEHOLDER_DATA } from "~/utils/placeholder-data";
@@ -20,139 +21,11 @@ const Following = () => {
   const router = useRouter();
   const headerHeight = useHeaderHeight();
 
-  const utils = api.useUtils();
-
-  const follow = api.follow.followUser.useMutation({
-    onMutate: async (newData) => {
-      // Cancel outgoing fetches (so they don't overwrite our optimistic update)
-      await utils.follow.paginateFollowingOthers.cancel();
-
-      // Get the data from the queryCache
-      const prevData = utils.follow.paginateFollowingOthers.getInfiniteData({
-        userId,
-      });
-
-      if (prevData === undefined) return;
-
-      // Optimistically update the data
-      utils.follow.paginateFollowingOthers.setInfiniteData(
-        { userId },
-        {
-          ...prevData,
-          pages: prevData.pages.map((page) => ({
-            ...page,
-            items: page.items.map((item) =>
-              item.userId === newData.userId
-                ? { ...item, isFollowing: true }
-                : item,
-            ),
-          })),
-        },
-      );
-
-      return { prevData };
-    },
-    onError: (_err, _newData, ctx) => {
-      if (ctx === undefined) return;
-      utils.follow.paginateFollowingOthers.setInfiniteData(
-        { userId },
-        ctx.prevData,
-      );
-    },
-    onSettled: async () => {
-      // Sync with server once mutation has settled
-      await utils.follow.paginateFollowingOthers.invalidate();
-    },
-  });
-
-  const unfollow = api.follow.unfollowUser.useMutation({
-    onMutate: async (newData) => {
-      // Cancel outgoing fetches (so they don't overwrite our optimistic update)
-      await utils.follow.paginateFollowingOthers.cancel();
-
-      // Get the data from the queryCache
-      const prevData = utils.follow.paginateFollowingOthers.getInfiniteData({
-        userId,
-      });
-      if (prevData === undefined) return;
-
-      // Optimistically update the data
-      utils.follow.paginateFollowingOthers.setInfiniteData(
-        { userId },
-        {
-          ...prevData,
-          pages: prevData.pages.map((page) => ({
-            ...page,
-            items: page.items.map((item) =>
-              item.userId === newData.userId
-                ? { ...item, isFollowing: false }
-                : item,
-            ),
-          })),
-        },
-      );
-
-      return { prevData };
-    },
-    onError: (_err, _newData, ctx) => {
-      if (ctx === undefined) return;
-      utils.follow.paginateFollowingOthers.setInfiniteData(
-        { userId },
-        ctx.prevData,
-      );
-    },
-    onSettled: async () => {
-      // Sync with server once mutation has settled
-      await utils.follow.paginateFollowingOthers.invalidate();
-    },
-  });
-
-  const cancelFollowRequest = api.follow.cancelFollowRequest.useMutation({
-    onMutate: async (newData) => {
-      // Cancel outgoing fetches (so they don't overwrite our optimistic update)
-      await utils.follow.paginateFollowingOthers.cancel();
-
-      // Get the data from the queryCache
-      const prevData = utils.follow.paginateFollowingOthers.getInfiniteData({
-        userId,
-      });
-      if (prevData === undefined) return;
-
-      // Optimistically update the data
-      utils.follow.paginateFollowingOthers.setInfiniteData(
-        { userId },
-        {
-          ...prevData,
-          pages: prevData.pages.map((page) => ({
-            ...page,
-            items: page.items.map((item) =>
-              item.userId === newData.userId
-                ? { ...item, isFollowing: false }
-                : item,
-            ),
-          })),
-        },
-      );
-
-      return { prevData };
-    },
-    onError: (_err, newData, ctx) => {
-      if (ctx === undefined) return;
-      utils.follow.paginateFollowingOthers.setInfiniteData(
-        { userId: newData.userId },
-        ctx.prevData,
-      );
-    },
-    onSettled: async () => {
-      // Sync with server once mutation has settled
-      await utils.follow.paginateFollowingOthers.invalidate();
-    },
-  });
+  const { follow, unfollow, cancelFollowRequest } = useFollowMutations(userId);
 
   const {
     data: followingData,
     isLoading,
-    isRefetching,
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
@@ -235,7 +108,7 @@ const Following = () => {
     </View>
   );
 
-  if (isLoading || isRefetching) {
+  if (isLoading) {
     return (
       <BaseScreenView paddingBottom={0}>
         <FlashList
