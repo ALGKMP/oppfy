c2489372f58c9c0b02a166df5b156dd3ad24c4aa
Einsight <ghaithkhalil2004@gmail.com>
2024-05-26 00:52:34 -0400

    added typesafety for require.context()

--- apps/expo/index.tsx
+++ apps/expo/index.tsx
@@ -1,13 +1,12 @@
 import { registerRootComponent } from "expo";
 import { ExpoRoot } from "expo-router";
-import type { RequireContext } from "expo-router/build/types";
 import * as SplashScreen from "expo-splash-screen";
 
 void SplashScreen.preventAutoHideAsync();
 
 // Must be exported or Fast Refresh won't update the context
 export const App = () => {
-  const ctx = require.context("./src/app") as RequireContext;
+  const ctx = require.context("./src/app");
   return <ExpoRoot context={ctx} />;
 };
 
