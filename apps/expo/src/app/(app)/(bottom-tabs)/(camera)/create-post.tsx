052522c8fa939a389a920de4c41e47db0157e283
Einsight <ghaithkhalil2004@gmail.com>
2024-05-31 02:24:56 -0400

    generate thumbnails from vid

--- apps/expo/src/app/(app)/(bottom-tabs)/(camera)/create-post.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(camera)/create-post.tsx
@@ -1,6 +1,10 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import { Image } from "expo-image";
 import { useLocalSearchParams } from "expo-router";
+import * as VideoThumbnails from "expo-video-thumbnails";
+
+import { BaseScreenView } from "~/components/Views";
 
 const CreatePost = () => {
   const { uri, type } = useLocalSearchParams<{
@@ -8,11 +12,42 @@ const CreatePost = () => {
     type: "photo" | "video";
   }>();
 
+  const [thumbnail, setThumbnail] = useState<string | null>(null);
+
+  useEffect(() => {
+    const generateThumbnail = async () => {
+      const { uri: thumbnailUri } =
+        await VideoThumbnails.getThumbnailAsync(uri);
+      setThumbnail(thumbnailUri);
+    };
+
+    if (type === "video") {
+      void generateThumbnail();
+    }
+  }, [type, uri]);
+
   return (
-    <View>
-      <Text>CreatePost</Text>
-    </View>
+    <BaseScreenView>
+      {type === "photo" ? (
+        <Image source={{ uri }} style={styles.media} />
+      ) : (
+        <Image source={{ uri: thumbnail ?? uri }} style={styles.media} />
+      )}
+    </BaseScreenView>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginVertical: 20,
+  },
+  media: {
+    width: "100%",
+    height: 200,
+    borderRadius: 10,
+  },
+});
 
 export default CreatePost;
