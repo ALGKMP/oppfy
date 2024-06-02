9ace901da3f9a705d9a2b61fc6190d385f519114
Antony <antonylbrussolo@gmail.com>
2024-06-02 16:24:31 -0400

    shared element example

--- apps/expo/src/app/(app)/(bottom-tabs)/(profile)/self-profile/media-of-you.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(profile)/self-profile/media-of-you.tsx
@@ -16,8 +16,6 @@ import { Image } from "expo-image";
 import { FlashList } from "@shopify/flash-list";
 import { Text, YStack } from "tamagui";
 
-const { width: screenWidth } = Dimensions.get("window");
-
 type DataItem = {
   key: string;
   image: string;
@@ -84,154 +82,166 @@ const data: DataItem[] = [
 const MediaOfYou: React.FC = () => {
   const [showSingleColumn, setShowSingleColumn] = useState(false);
   const [selectedItem, setSelectedItem] = useState<DataItem | null>(null);
-  const position = useRef(new Animated.Value(0)).current;
 
-  const startAncestor = useRef<SharedElementNode | null>(null);
-  const startNode = useRef<SharedElementNode | null>(null);
-  const endAncestor = useRef<SharedElementNode | null>(null);
-  const endNode = useRef<SharedElementNode | null>(null);
+  const [progress] = useState(new Animated.Value(0));
+  const [isScene2Visible, setIsScene2Visible] = useState(false);
+  const [isInProgress, setIsInProgress] = useState(false);
+  const [scene1Ancestor, setScene1Ancestor] =
+    useState<null | SharedElementNode>(null);
+  const [scene1Node, setScene1Node] = useState<null | SharedElementNode>(null);
+  const [scene2Ancestor, setScene2Ancestor] =
+    useState<null | SharedElementNode>(null);
+  const [scene2Node, setScene2Node] = useState<null | SharedElementNode>(null);
 
-  useEffect(() => {
-    if (selectedItem) {
-      Animated.timing(position, {
-        toValue: 1,
-        duration: 300,
-        useNativeDriver: true,
-      }).start();
-    } else {
-      Animated.timing(position, {
-        toValue: 0,
-        duration: 300,
-        useNativeDriver: true,
-      }).start();
-    }
-  }, [selectedItem, position]);
+  const { width } = Dimensions.get("window");
 
-  const renderItem = ({ item }: { item: DataItem }) => (
-    // <View
-    //   ref={(ref) => {
-    //     if (ref) startAncestor.current = nodeFromRef(ref);
-    //   }}
-    // >
-    //   <TouchableOpacity
-    //     onPress={() => {
-    //       setSelectedItem(item);
-    //       setShowSingleColumn(true);
-    //     }}
-    //   >
-    //     <SharedElement id={item.key} onNode={(node) => (startNode.current = node)}>
-    // <Image source={{ uri: item.image }} style={[styles.image, { height: 200 }]} contentFit="cover" />
-    //     </SharedElement>
-    //   </TouchableOpacity>
-    // </View>
+  const onPressNavigate = () => {
+    setIsScene2Visible(true);
+    setIsInProgress(true);
+    Animated.timing(progress, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start(() => setIsInProgress(false));
+  };
 
-    <View
-      ref={(ref) => {
-        if (ref) startAncestor.current = nodeFromRef(ref);
-      }}
-    >
-      <Image
-        source={{ uri: item.image }}
-        style={[styles.image, { height: 200 }]}
-        contentFit="cover"
-      />
-    </View>
-  );
+  const onPressBack = () => {
+    setIsInProgress(true);
+    Animated.timing(progress, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start(() => {
+      setIsScene2Visible(false);
+      setIsInProgress(false);
+    });
+  };
 
-  const renderSingleColumnItem = ({ item }: { item: DataItem }) => (
-    <View
-      ref={(ref) => {
-        if (ref) endAncestor.current = nodeFromRef(ref);
-      }}
-    >
-      <TouchableOpacity
-        onPress={() => {
-          setSelectedItem(null);
-          setShowSingleColumn(false);
-        }}
-      >
-        <SharedElement
-          id={item.key}
-          onNode={(node) => (endNode.current = node)}
-        >
-          <Image
-            source={{ uri: item.image }}
-            style={[
-              styles.image,
-              { height: undefined, aspectRatio: 1, alignSelf: "center" },
-            ]}
-            contentFit="contain"
-          />
-          <YStack
-            position="absolute"
-            bottom={0}
-            width="100%"
-            backgroundColor="rgba(0, 0, 0, 0.5)"
-            padding="$2"
-            borderBottomLeftRadius="$2"
-            borderBottomRightRadius="$2"
-          >
-            <Text color="#fff" textAlign="center">
-              {item.caption}
-            </Text>
-          </YStack>
-        </SharedElement>
-      </TouchableOpacity>
-    </View>
-  );
+  const onSetScene1Ref = (ref: View | null) => {
+    // const node = nodeFromRef(ref);
+    // if (node !== scene1Ancestor) {
+    //   setScene1Ancestor(node);
+    // }
+  };
+
+  const onSetScene2Ref = (ref: View | null) => {
+    // const node = nodeFromRef(ref);
+    // if (node !== scene2Ancestor) {
+    //   setScene2Ancestor(node);
+    // }
+  };
 
   return (
-    <YStack flex={1} margin="$1">
-      {selectedItem && (
-        <View style={StyleSheet.absoluteFill}>
+    <>
+      <TouchableOpacity
+        style={styles.container}
+        activeOpacity={0.5}
+        onPress={isScene2Visible ? onPressBack : onPressNavigate}
+      >
+        {/* Scene 1 */}
+        <Animated.View
+          style={{
+            ...StyleSheet.absoluteFillObject,
+            transform: [{ translateX: Animated.multiply(-200, progress) }],
+          }}
+        >
+          <View style={styles.scene} ref={onSetScene1Ref}>
+            <SharedElement onNode={(node) => setScene1Node(node)}>
+              <Image
+                style={styles.image1}
+                source={{
+                  uri: "https://media.discordapp.net/attachments/1246536535382167663/1246588862705438750/IMG_2127.jpg?ex=665cefd9&is=665b9e59&hm=03d95787f0509b92983eb1d626f134508cac0a4ffc18fc7610ffad13548173b5&=&format=webp&width=507&height=676",
+                }}
+              />
+            </SharedElement>
+          </View>
+        </Animated.View>
+
+        {/* Scene 2 */}
+        {isScene2Visible ? (
+          <Animated.View
+            style={{
+              ...StyleSheet.absoluteFillObject,
+              transform: [
+                {
+                  translateX: Animated.multiply(
+                    -width,
+                    Animated.add(progress, -1),
+                  ),
+                },
+              ],
+            }}
+          >
+            <View style={styles.scene2} ref={onSetScene2Ref}>
+              <SharedElement onNode={(node) => setScene2Node(node)}>
+                <Image
+                  style={styles.image2}
+                  source={{
+                    uri: "https://media.discordapp.net/attachments/1246536535382167663/1246588862705438750/IMG_2127.jpg?ex=665cefd9&is=665b9e59&hm=03d95787f0509b92983eb1d626f134508cac0a4ffc18fc7610ffad13548173b5&=&format=webp&width=507&height=676",
+                  }}
+                />
+              </SharedElement>
+            </View>
+          </Animated.View>
+        ) : null}
+      </TouchableOpacity>
+
+      {/* Transition overlay */}
+      {isInProgress ? (
+        <View style={styles.sharedElementOverlay} pointerEvents="none">
           <SharedElementTransition
             start={{
-              node: startNode.current,
-              ancestor: startAncestor.current,
+              node: scene1Node,
+              ancestor: scene1Ancestor,
             }}
             end={{
-              node: endNode.current,
-              ancestor: endAncestor.current,
+              node: scene2Node,
+              ancestor: scene2Ancestor,
             }}
-            position={position}
+            position={progress}
             animation="move"
             resize="auto"
             align="auto"
           />
         </View>
-      )}
-      {!showSingleColumn && (
-        <Animated.View style={styles.content}>
-          <FlashList
-            data={data}
-            numColumns={2}
-            renderItem={renderItem}
-            estimatedItemSize={screenWidth / 2}
-          />
-        </Animated.View>
-      )}
-
-      {showSingleColumn && (
-        <Animated.View style={styles.content}>
-          <FlashList
-            data={data}
-            numColumns={1}
-            renderItem={renderSingleColumnItem}
-            estimatedItemSize={screenWidth}
-          />
-        </Animated.View>
-      )}
-    </YStack>
+      ) : null}
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  content: {
+  container: {
     flex: 1,
+    backgroundColor: "#ecf0f1",
   },
-  image: {
-    width: "100%",
-    borderRadius: 10,
+  scene: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  scene2: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "#00d8ff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image1: {
+    resizeMode: "cover",
+    width: 160,
+    height: 160,
+    // Images & border-radius have quirks in Expo SDK 35/36
+    // Uncomment the next line when SDK 37 has been released
+    //borderRadius: 80
+  },
+  image2: {
+    resizeMode: "cover",
+    width: 300,
+    height: 300,
+    borderRadius: 0,
+  },
+  sharedElementOverlay: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
 
