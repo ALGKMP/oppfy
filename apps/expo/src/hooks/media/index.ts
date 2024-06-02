80ece67511454a6c8498d5be361e66124dd60e42
Einsight <ghaithkhalil2004@gmail.com>
2024-06-02 19:17:41 -0400

    abstracted media types

--- apps/expo/src/hooks/media/index.ts
+++ apps/expo/src/hooks/media/index.ts
@@ -1,2 +1,4 @@
+export type { UploadMediaInput } from "./useUploadMedia";
+
 export { default as useUploadMedia } from "./useUploadMedia";
 export { default as useUploadProfilePicture } from "./useUploadProfilePicture";
