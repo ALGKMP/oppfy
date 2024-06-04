a4a94b6ee4ffef68288ea39d1903bf9cd3ae5b57
Einsight <ghaithkhalil2004@gmail.com>
2024-06-04 01:19:48 -0400

    starting point for rendering data

--- apps/expo/src/app/(app)/(bottom-tabs)/(inbox)/requests.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(inbox)/requests.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { Text, View } from "tamagui";
+import { FlashList } from "@shopify/flash-list";
+import { Separator, Text, View, YStack } from "tamagui";
 
+import { VirtualizedListItem } from "~/components/ListItems";
+import { BaseScreenView } from "~/components/Views";
+import { ListHeader } from "~/features/connections/components";
 import { api } from "~/utils/api";
+import { PLACEHOLDER_DATA } from "~/utils/placeholder-data";
 
 const Requests = () => {
-  const { data: followRequestsData } =
+  const { data: followRequestsData, isLoading: isLoadingFollowRequests } =
     api.request.paginateFollowRequests.useInfiniteQuery(
       {
         pageSize: 5,
@@ -14,7 +19,7 @@ const Requests = () => {
       },
     );
 
-  const { data: friendRequestsData } =
+  const { data: friendRequestsData, isLoading: isLoadingFriendRequests } =
     api.request.paginateFriendRequests.useInfiniteQuery(
       {
         pageSize: 5,
@@ -24,10 +29,86 @@ const Requests = () => {
       },
     );
 
+  const isLoading = isLoadingFollowRequests || isLoadingFriendRequests;
+
+  if (true) {
+    return (
+      <BaseScreenView paddingBottom={0}>
+        <YStack flex={1} gap="$4">
+          <View
+            flex={1}
+            borderRadius="$6"
+            paddingHorizontal="$4"
+            backgroundColor="$gray2"
+          >
+            <FlashList
+              data={PLACEHOLDER_DATA}
+              ItemSeparatorComponent={Separator}
+              estimatedItemSize={75}
+              showsVerticalScrollIndicator={false}
+              ListHeaderComponent={
+                <View marginTop="$3">
+                  <ListHeader title="FRIEND REQUESTS" />
+                </View>
+              }
+              renderItem={() => (
+                <VirtualizedListItem
+                  loading
+                  showSkeletons={{
+                    imageUrl: true,
+                    title: true,
+                    subtitle: true,
+                    button: true,
+                    button2: true,
+                  }}
+                />
+              )}
+            />
+          </View>
+
+          <View
+            flex={1}
+            borderRadius="$6"
+            paddingHorizontal="$4"
+            backgroundColor="$gray2"
+          >
+            <FlashList
+              data={PLACEHOLDER_DATA}
+              ItemSeparatorComponent={Separator}
+              estimatedItemSize={75}
+              showsVerticalScrollIndicator={false}
+              ListHeaderComponent={
+                <View marginTop="$3">
+                  <ListHeader title="FRIEND REQUESTS" />
+                </View>
+              }
+              renderItem={() => (
+                <VirtualizedListItem
+                  loading
+                  showSkeletons={{
+                    imageUrl: true,
+                    title: true,
+                    subtitle: true,
+                    button: true,
+                    button2: true,
+                  }}
+                />
+              )}
+            />
+          </View>
+        </YStack>
+      </BaseScreenView>
+    );
+  }
+
   return (
-    <View flex={1} backgroundColor="black" paddingHorizontal="$4">
-      <Text>Requests</Text>
-    </View>
+    <BaseScreenView>
+      <VirtualizedListItem
+        loading={false}
+        title="Christina"
+        subtitle="wants to be your friend"
+      />
+    </BaseScreenView>
   );
 };
 
