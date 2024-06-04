5acfe02687a8addba0df6f1867a4ab3ab4d89cab
Antony <antonylbrussolo@gmail.com>
2024-06-03 20:44:57 -0400

    feat: Add trpcRequestOutputSchema to output in validators

--- packages/validators/src/trpc/output/index.ts
+++ packages/validators/src/trpc/output/index.ts
@@ -3,11 +3,13 @@ import trpcProfileOutputSchema from "../output/profile/profile";
 import trpcFriendOutputSchema from "./network/friend";
 import trpcPostOutputSchema from "./post/post";
 import trpcBlockOutputSchema from "./network/block";
+import trpcRequestOutputSchema from "./network/request";
 
 export const output = {
   profile: trpcProfileOutputSchema,
   follow: trpcFollowOutputSchema,
   friend: trpcFriendOutputSchema,
   post: trpcPostOutputSchema,
-  block: trpcBlockOutputSchema
+  block: trpcBlockOutputSchema,
+  request: trpcRequestOutputSchema
 };
