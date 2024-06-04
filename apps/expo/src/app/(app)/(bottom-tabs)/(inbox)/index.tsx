7ffd50643de46373cbe286c727a2ea08570a692a
Einsight <ghaithkhalil2004@gmail.com>
2024-06-03 21:08:17 -0400

    init request functionality

--- apps/expo/src/app/(app)/(bottom-tabs)/(inbox)/index.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(inbox)/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { Text, View } from "tamagui";
+import { TouchableOpacity } from "react-native";
+import { Paragraph, SizableText, Text, View, YStack } from "tamagui";
+
+import { BaseScreenView } from "~/components/Views";
+import { api } from "~/utils/api";
 
 const Inbox = () => {
+  const { data: requestsCount } = api.request.countRequests.useQuery();
+
   return (
-    <View flex={1} backgroundColor="black" paddingHorizontal="$4">
-      <Text>Inbox</Text>
-    </View>
+    <BaseScreenView>
+      <TouchableOpacity>
+        <View padding="$4" borderRadius="$6" backgroundColor="$gray2">
+          <YStack>
+            <SizableText size="$6" fontWeight="bold">
+              Follow and Friend Requests
+            </SizableText>
+            <Paragraph theme="alt1">Approve or ignore these requests</Paragraph>
+          </YStack>
+        </View>
+      </TouchableOpacity>
+    </BaseScreenView>
   );
 };
 
