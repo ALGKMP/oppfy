7368d83af0b82d3fa448d1fe0207775efb6629cd
Einsight <ghaithkhalil2004@gmail.com>
2024-06-04 00:31:08 -0400

    setup infinite queries

--- apps/expo/src/app/(app)/(bottom-tabs)/(inbox)/requests.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(inbox)/requests.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 import { Text, View } from "tamagui";
 
+import { api } from "~/utils/api";
+
 const Requests = () => {
+  const { data: followRequestsData } =
+    api.request.paginateFollowRequests.useInfiniteQuery(
+      {
+        pageSize: 5,
+      },
+      {
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+      },
+    );
+
+  const { data: friendRequestsData } =
+    api.request.paginateFriendRequests.useInfiniteQuery(
+      {
+        pageSize: 5,
+      },
+      {
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+      },
+    );
+
   return (
     <View flex={1} backgroundColor="black" paddingHorizontal="$4">
       <Text>Requests</Text>
