89522dae775afb98b8dcc2b004100a7f05dd13e3
Einsight <ghaithkhalil2004@gmail.com>
2024-06-03 21:08:02 -0400

    init

---
+++ apps/expo/src/app/(app)/(bottom-tabs)/(inbox)/requests.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "tamagui";
+
+const Requests = () => {
+  return (
+    <View flex={1} backgroundColor="black" paddingHorizontal="$4">
+      <Text>Requests</Text>
+    </View>
+  );
+};
+
+export default Requests;
