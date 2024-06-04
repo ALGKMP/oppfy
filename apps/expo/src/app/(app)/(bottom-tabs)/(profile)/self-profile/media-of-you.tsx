7ad1991b8e61f9e4f98ef864dd9479ec34bcb3ae
Antony <antonylbrussolo@gmail.com>
2024-06-04 19:59:35 -0400

    updates

--- apps/expo/src/app/(app)/(bottom-tabs)/(profile)/self-profile/media-of-you.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(profile)/self-profile/media-of-you.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { Dimensions, StyleSheet, TouchableOpacity } from "react-native";
+import { SharedValue, withSpring } from "react-native-reanimated";
 import { Image } from "expo-image";
 import { FontAwesome } from "@expo/vector-icons";
 import { FlashList } from "@shopify/flash-list";
 import { Heart, MessageCircle } from "@tamagui/lucide-icons";
-import { Stack, Text, View, XStack, YStack } from "tamagui";
+import { Avatar, Separator, Stack, Text, View, XStack, YStack } from "tamagui";
+
+// import { api } from "@oppfy/utils";
 
 const { width: screenWidth } = Dimensions.get("window");
 
@@ -27,7 +30,8 @@ type DataItem = {
 const data = [
   {
     author: "JohnDoe",
-    authorProfilePicture: "https://example.com/author1.jpg",
+    authorProfilePicture:
+      "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
     recipient: "JaneSmith",
     recipientProfilePicture: "https://example.com/recipient1.jpg",
     isFollowing: true,
@@ -35,100 +39,100 @@ const data = [
     key: "1",
     comments: 100,
     likes: 50,
-    image: "https://media.discordapp.net/attachments/1246536535382167663/1246669290761879633/image.png?ex=665de380&is=665c9200&hm=f8ad544db1e2d90cf69f85f2927e20ed81594757d6cc89679ff52e881a44b95f&=&format=webp&quality=lossless&width=496&height=638",
+    image: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
     caption: "Enjoying the sunset!",
   },
-  {
-    author: "JaneSmith",
-    authorProfilePicture: "https://example.com/author2.jpg",
-    recipient: "JohnDoe",
-    recipientProfilePicture: "https://example.com/recipient2.jpg",
-    isFollowing: false,
-    hasLiked: true,
-    key: "2",
-    comments: 100,
-    likes: 50,
-    image: "https://media.discordapp.net/attachments/1246536535382167663/1246588862705438750/IMG_2127.jpg?ex=665cefd9&is=665b9e59&hm=03d95787f0509b92983eb1d626f134508cac0a4ffc18fc7610ffad13548173b5&=&format=webp&width=507&height=676",
-    caption: "Delicious breakfast.",
-  },
-  {
-    author: "AliceWang",
-    authorProfilePicture: "https://example.com/author3.jpg",
-    recipient: "BobJones",
-    recipientProfilePicture: "https://example.com/recipient3.jpg",
-    isFollowing: true,
-    hasLiked: true,
-    key: "3",
-    comments: 100,
-    likes: 50,
-    image: "https://media.discordapp.net/attachments/1246536535382167663/1246588862705438750/IMG_2127.jpg?ex=665cefd9&is=665b9e59&hm=03d95787f0509b92983eb1d626f134508cac0a4ffc18fc7610ffad13548173b5&=&format=webp&width=507&height=676",
-    caption: "Hiking adventures.",
-  },
-  {
-    author: "BobJones",
-    authorProfilePicture: "https://example.com/author4.jpg",
-    recipient: "AliceWang",
-    recipientProfilePicture: "https://example.com/recipient4.jpg",
-    isFollowing: false,
-    hasLiked: false,
-    key: "4",
-    comments: 100,
-    likes: 50,
-    image: "https://media.discordapp.net/attachments/1246536535382167663/1246588862705438750/IMG_2127.jpg?ex=665cefd9&is=665b9e59&hm=03d95787f0509b92983eb1d626f134508cac0a4ffc18fc7610ffad13548173b5&=&format=webp&width=507&height=676",
-    caption: "Family time.",
-  },
-  {
-    author: "CharlieBrown",
-    authorProfilePicture: "https://example.com/author5.jpg",
-    recipient: "LucyVanPelt",
-    recipientProfilePicture: "https://example.com/recipient5.jpg",
-    isFollowing: true,
-    hasLiked: true,
-    key: "5",
-    comments: 100,
-    likes: 50,
-    image: "https://media.discordapp.net/attachments/1246536535382167663/1246588862705438750/IMG_2127.jpg?ex=665cefd9&is=665b9e59&hm=03d95787f0509b92983eb1d626f134508cac0a4ffc18fc7610ffad13548173b5&=&format=webp&width=507&height=676",
-    caption: "City lights.",
-  },
-  {
-    author: "LucyVanPelt",
-    authorProfilePicture: "https://example.com/author6.jpg",
-    recipient: "CharlieBrown",
-    recipientProfilePicture: "https://example.com/recipient6.jpg",
-    isFollowing: false,
-    hasLiked: false,
-    key: "6",
-    comments: 100,
-    likes: 50,
-    image: "https://media.discordapp.net/attachments/1246536535382167663/1246588862705438750/IMG_2127.jpg?ex=665cefd9&is=665b9e59&hm=03d95787f0509b92983eb1d626f134508cac0a4ffc18fc7610ffad13548173b5&=&format=webp&width=507&height=676",
-    caption: "At the beach.",
-  },
-  {
-    author: "EmilyClark",
-    authorProfilePicture: "https://example.com/author7.jpg",
-    recipient: "DavidSmith",
-    recipientProfilePicture: "https://example.com/recipient7.jpg",
-    isFollowing: true,
-    hasLiked: false,
-    key: "7",
-    comments: 100,
-    likes: 50,
-    image: "https://media.discordapp.net/attachments/1246536535382167663/1246588862705438750/IMG_2127.jpg?ex=665cefd9&is=665b9e59&hm=03d95787f0509b92983eb1d626f134508cac0a4ffc18fc7610ffad13548173b5&=&format=webp&width=507&height=676",
-    caption: "Mountain views.",
-  },
-  {
-    author: "DavidSmith",
-    authorProfilePicture: "https://example.com/author8.jpg",
-    recipient: "EmilyClark",
-    recipientProfilePicture: "https://example.com/recipient8.jpg",
-    isFollowing: false,
-    hasLiked: true,
-    key: "8",
-    comments: 100,
-    likes: 50,
-    image: "https://media.discordapp.net/attachments/1246536535382167663/1246588862705438750/IMG_2127.jpg?ex=665cefd9&is=665b9e59&hm=03d95787f0509b92983eb1d626f134508cac0a4ffc18fc7610ffad13548173b5&=&format=webp&width=507&height=676",
-    caption: "Birthday celebrations.",
-  },
+  // {
+  //   author: "JaneSmith",
+  //   authorProfilePicture: "https://example.com/author2.jpg",
+  //   recipient: "JohnDoe",
+  //   recipientProfilePicture: "https://example.com/recipient2.jpg",
+  //   isFollowing: false,
+  //   hasLiked: true,
+  //   key: "2",
+  //   comments: 100,
+  //   likes: 50,
+  //   image: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
+  //   caption: "Delicious breakfast.",
+  // },
+  // {
+  //   author: "AliceWang",
+  //   authorProfilePicture: "https://example.com/author3.jpg",
+  //   recipient: "BobJones",
+  //   recipientProfilePicture: "https://example.com/recipient3.jpg",
+  //   isFollowing: true,
+  //   hasLiked: true,
+  //   key: "3",
+  //   comments: 100,
+  //   likes: 50,
+  //   image: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0",
+  //   caption: "Hiking adventures.",
+  // },
+  // {
+  //   author: "BobJones",
+  //   authorProfilePicture: "https://example.com/author4.jpg",
+  //   recipient: "AliceWang",
+  //   recipientProfilePicture: "https://example.com/recipient4.jpg",
+  //   isFollowing: false,
+  //   hasLiked: false,
+  //   key: "4",
+  //   comments: 100,
+  //   likes: 50,
+  //   image: "https://images.unsplash.com/photo-1501594907352-04cda38ebc29",
+  //   caption: "Family time.",
+  // },
+  // {
+  //   author: "CharlieBrown",
+  //   authorProfilePicture: "https://example.com/author5.jpg",
+  //   recipient: "LucyVanPelt",
+  //   recipientProfilePicture: "https://example.com/recipient5.jpg",
+  //   isFollowing: true,
+  //   hasLiked: true,
+  //   key: "5",
+  //   comments: 100,
+  //   likes: 50,
+  //   image: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
+  //   caption: "City lights.",
+  // },
+  // {
+  //   author: "LucyVanPelt",
+  //   authorProfilePicture: "https://example.com/author6.jpg",
+  //   recipient: "CharlieBrown",
+  //   recipientProfilePicture: "https://example.com/recipient6.jpg",
+  //   isFollowing: false,
+  //   hasLiked: false,
+  //   key: "6",
+  //   comments: 100,
+  //   likes: 50,
+  //   image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+  //   caption: "At the beach.",
+  // },
+  // {
+  //   author: "EmilyClark",
+  //   authorProfilePicture: "https://example.com/author7.jpg",
+  //   recipient: "DavidSmith",
+  //   recipientProfilePicture: "https://example.com/recipient7.jpg",
+  //   isFollowing: true,
+  //   hasLiked: false,
+  //   key: "7",
+  //   comments: 100,
+  //   likes: 50,
+  //   image: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
+  //   caption: "Mountain views.",
+  // },
+  // {
+  //   author: "DavidSmith",
+  //   authorProfilePicture: "https://example.com/author8.jpg",
+  //   recipient: "EmilyClark",
+  //   recipientProfilePicture: "https://example.com/recipient8.jpg",
+  //   isFollowing: false,
+  //   hasLiked: true,
+  //   key: "8",
+  //   comments: 100,
+  //   likes: 50,
+  //   image: "https://images.unsplash.com/photo-1552664730-d307ca884978",
+  //   caption: "Birthday celebrations.",
+  // },
   {
     author: "GraceLee",
     authorProfilePicture: "https://example.com/author9.jpg",
@@ -139,7 +143,7 @@ const data = [
     key: "9",
     comments: 100,
     likes: 50,
-    image: "https://media.discordapp.net/attachments/1246536535382167663/1246588862705438750/IMG_2127.jpg?ex=665cefd9&is=665b9e59&hm=03d95787f0509b92983eb1d626f134508cac0a4ffc18fc7610ffad13548173b5&=&format=webp&width=507&height=676",
+    image: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0",
     caption: "Night out with friends.",
   },
 ];
@@ -150,49 +154,65 @@ const MediaOfYou = () => {
   const handleComment = (key: string) => {};
 
   const renderItem = ({ item }: { item: DataItem }) => (
-    <YStack flex={1}>
-      <View
-        flex={1}
-        alignItems="center"
-        justifyContent="center"
-        borderRadius={10}
-        margin={1}
-      >
-        <Image
-          source={{ uri: item.image }}
-          style={[styles.image]}
-          contentFit="contain"
+    <View
+      flex={1}
+      alignItems="center"
+      justifyContent="center"
+      marginTop={25}
+      paddingBottom={20}
+      maxHeight={550}
+      width={"100%"}
+    >
+      <Avatar circular size="$5"  position="absolute" left={25} top={25}>
+        <Avatar.Image
+          accessibilityLabel="Cam"
+          src="https://images.unsplash.com/photo-1548142813-c348350df52b?&w=150&h=150&dpr=2&q=80"
         />
-        <YStack gap="$2" position="absolute" bottom={20} right={20}>
-          <View>
-            <TouchableOpacity onPress={() => handleLike(item.key)}>
-              <Heart size={24} color="#fff" />
-            </TouchableOpacity>
-          </View>
-          <Text color="#fff">{item.likes}</Text>
-          <TouchableOpacity onPress={() => handleComment(item.key)}>
-            <MessageCircle size={24} color="#fff" />
-          </TouchableOpacity>
-          <Text color="#fff">{item.comments}</Text>
-        </YStack>
-        <Text color="#fff" textAlign="center">
-          {item.caption}
-        </Text>
-        <YStack
-          position="absolute"
-          bottom={0}
-          width="100%"
-          backgroundColor="rgba(0, 0, 0, 0.5)"
-          padding="$2"
-          borderBottomLeftRadius="$2"
-          borderBottomRightRadius="$2"
-        ></YStack>
-      </View>
-    </YStack>
+        <Avatar.Fallback backgroundColor="$blue10" />
+      </Avatar>
+      <Image source={{ uri: item.image }} style={styles.image} />
+      <YStack
+        gap="$2"
+        position="absolute"
+        bottom={20}
+        right={20}
+        paddingBottom={40}
+      >
+        <View flex={1} alignItems="center" marginBottom={8}>
+          <TouchableOpacity onPress={() => handleLike(item.key)} />
+          <Heart size={24} color="white" />
+          <Text marginTop={3} color="#fff">
+            {item.likes}
+          </Text>
+        </View>
+        <View flex={1} alignItems="center" marginBottom={8}>
+          <TouchableOpacity onPress={() => handleComment(item.key)} />
+          <MessageCircle
+            borderBlockWidth={1}
+            borderColor={"black"}
+            size={24}
+            color="white"
+          />
+          <Text marginTop={3} color="#fff">
+            {item.comments}
+          </Text>
+        </View>
+      </YStack>
+      <Text
+        color="#fff"
+        textAlign="center"
+        borderBottomLeftRadius={10}
+        borderBottomRightRadius={10}
+        marginTop={20}
+      >
+        {item.caption}
+      </Text>
+    </View>
   );
 
   return (
     <View flex={1}>
+      <Separator margin={10} borderColor={"white"} height={50} />
       <FlashList
         data={data}
         numColumns={1}
@@ -205,10 +225,10 @@ const MediaOfYou = () => {
 
 const styles = StyleSheet.create({
   image: {
-    alignSelf: "center",
     width: "100%",
-    padding: 10,
-    height: 500,
+    height: "100%",
+    // resizeMode: "cover",
+    alignSelf: "center",
     borderRadius: 10,
   },
 });
