25564e607b8c1bb60815a4f112beb19abc5b5e7f
Einsight <ghaithkhalil2004@gmail.com>
2024-06-06 01:18:13 -0400

    added storePushToken endpoint

--- packages/api/src/routers/user/notifications.ts
+++ packages/api/src/routers/user/notifications.ts
@@ -5,9 +5,22 @@ import { trpcValidators } from "@oppfy/validators";
 import { createTRPCRouter, protectedProcedure } from "../../trpc";
 
 export const notificationsRouter = createTRPCRouter({
+  storePushToken: protectedProcedure
+    .input(trpcValidators.input.notifications.storePushToken)
+    .mutation(async ({ input, ctx }) => {
+      try {
+        await ctx.services.notifications.storePushToken(
+          ctx.session.uid,
+          input.pushToken,
+        );
+      } catch (err) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" });
+      }
+    }),
+
   getNotificationSettings: protectedProcedure.query(async ({ ctx }) => {
     try {
-      return await ctx.services.notifications.getUserNotificationSettings(
+      return await ctx.services.notifications.getNotificationSettings(
         ctx.session.uid,
       );
     } catch (err) {
