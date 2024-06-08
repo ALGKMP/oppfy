db23c73577ab2d7830a15bbc6d1b1faa634bc533
Einsight <ghaithkhalil2004@gmail.com>
2024-06-08 00:58:09 -0400

    handling inserts

--- packages/api/src/repositories/user/notifications.ts
+++ packages/api/src/repositories/user/notifications.ts
@@ -1,10 +1,27 @@
 import { eq } from "drizzle-orm";
 import type { z } from "zod";
 
-import { db, schema } from "@oppfy/db";
+import { db, InferInsertModel, schema } from "@oppfy/db";
 import type { trpcValidators } from "@oppfy/validators";
 
 import { handleDatabaseErrors } from "../../errors";
+
+type Notifications = InferInsertModel<typeof schema.notifications>;
+type EntityTypes = Notifications["entityType"];
+
+interface Entity {
+  id: string;
+  type: EntityTypes;
+}
+
+interface Notification {
+  recipientId: string;
+
+  title: string;
+  body: string;
+
+  entity: Entity;
+}
 
 export type NotificationSettings = z.infer<
   typeof trpcValidators.input.notifications.updateNotificationSettings
@@ -50,5 +67,25 @@ export class NotificationsRepository {
         ...notificationSettings,
       })
       .where(eq(schema.notificationSettings.id, notificationSettingsId));
+  }
+
+  @handleDatabaseErrors
+  async storeNotification({
+    recipientId,
+
+    title,
+    body,
+
+    entity,
+  }: Notification) {
+    await this.db.insert(schema.notifications).values({
+      recipientId,
+
+      title,
+      body,
+
+      entityId: entity.id,
+      entityType: entity.type,
+    });
   }
 }
