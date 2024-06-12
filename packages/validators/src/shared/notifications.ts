af8b125e9a21942192e9d1820e0aa111f46e7704
Einsight <ghaithkhalil2004@gmail.com>
2024-06-11 21:05:23 -0400

    reworked types

--- packages/validators/src/shared/notifications.ts
+++ packages/validators/src/shared/notifications.ts
@@ -1,31 +1,59 @@
 import { z } from "zod";
 
-export const entityData = z.object({
-  entityId: z.string(),
-  entityType: z.enum(["post", "profile", "comment"]),
-});
-
-export const baseNotificationData = z.object({
-  title: z.string(),
-  body: z.string(),
-});
-
-export const entityNotificationData = baseNotificationData.merge(entityData);
-
-export const notificationData = z.union([
-  entityNotificationData,
-  baseNotificationData,
+const entityType = z.enum(["post", "profile", "comment"]);
+const eventType = z.enum([
+  "like",
+  "post",
+  "comment",
+  "follow",
+  "friend",
+  "followRequest",
+  "friendRequest",
 ]);
 
-export const snsBaseNotificationData = baseNotificationData.extend({
-  pushToken: z.string(),
+const entityData = z.object({
+  entityId: z.string(),
+  entityType,
 });
 
-export const snsEntityNotificationData = entityNotificationData.extend({
-  pushToken: z.string(),
+const baseStoreNotificationData = z.object({
+  eventType,
 });
+
+const entityStoreNotificationData = baseStoreNotificationData.merge(entityData);
+
+export const notificationData = z.union([
+  entityStoreNotificationData,
+  baseStoreNotificationData,
+]);
+
+const baseSendNotificationData = z.object({
+  title: z.string(),
+  body: z.string(),
+
+  eventType,
+});
+
+const entitySendNotificationData = baseSendNotificationData.extend({
+  entityId: z.string(),
+  entityType,
+});
+
+export const sendNotificationData = z.union([
+  entitySendNotificationData,
+  baseSendNotificationData,
+]);
+
+const extraData = z.object({
+  pushToken: z.string(),
+  senderId: z.string(),
+  recipientId: z.string(),
+});
+
+const baseSnsNotificationData = baseSendNotificationData.merge(extraData);
+const entitySnsNotificationData = entitySendNotificationData.merge(extraData);
 
 export const snsNotificationData = z.union([
-  snsEntityNotificationData,
-  snsBaseNotificationData,
+  entitySnsNotificationData,
+  baseSnsNotificationData,
 ]);
