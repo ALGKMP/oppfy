43c41260199b584ec51ce83cd25883584ea66e60
Antony <antonylbrussolo@gmail.com>
2024-06-16 19:53:26 -0400

    onBoarding

--- apps/expo/src/app/(app)/_layout.tsx
+++ apps/expo/src/app/(app)/_layout.tsx
@@ -5,17 +5,23 @@ import { usePermissions } from "~/contexts/PermissionsContext";
 import { useSession } from "~/contexts/SessionContext";
 import { usePushNotifications } from "~/hooks/notifications";
 import { Stack } from "~/layouts";
+import { api } from "~/utils/api";
 
 const AppLayout = () => {
   usePushNotifications();
 
   const { isLoading: _sessionIsLoading, isSignedIn } = useSession();
   const { isLoading: _permissionsIsLoading, permissions } = usePermissions();
+  const onBoardingComplete = api.user.checkOnboardingComplete;
 
   const requiredPermissions = permissions.camera && permissions.contacts;
 
   if (!isSignedIn) {
     return <Redirect href="/(onboarding)" />;
+  }
+
+  if (!onBoardingComplete) {
+    return <Redirect href="/(onboarding)/user-info/welcome" />;
   }
 
   if (!requiredPermissions) {
