7b9f123a79c89c9a0e67bc8ad2ac91a6ccf30aca
Antony <antonylbrussolo@gmail.com>
2024-06-17 23:05:08 -0400

    non nullable captions

--- packages/validators/src/shared/media/index.ts
+++ packages/validators/src/shared/media/index.ts
@@ -10,9 +10,19 @@ export const post = z.object({
   recipientId: z.string(),
   recipientUsername: z.string(),
   recipientProfilePicture: z.string(), // Corrected typo
-  caption: z.string().nullable(),
+  caption: z.string().default(""),
   imageUrl: z.string(),
+  height: z.number(),
+  width: z.number(),
   commentsCount: z.number(),
   likesCount: z.number(),
   createdAt: z.date(), // Added missing field
+});
+
+export const postMetadataForS3 = z.object({
+  author: z.string(),
+  recipient: z.string(),
+  caption: z.string().max(2000).default(""),
+  width: z.string(),
+  height: z.string(),
 });
