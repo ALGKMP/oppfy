a40582a40762220b9afeb9f0977c6bf49b75e31e
matt <30198937+6mp@users.noreply.github.com>
2024-06-22 15:59:31 -0400

    rename

--- cloud/aws/src/res/lambdas/contact-recs/index.ts
+++ cloud/aws/src/res/lambdas/contact-recs/index.ts
@@ -141,7 +141,7 @@ export const handler = middy(lambdaHandler).use(
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-export const lambdaHandler = async (
+export const handler = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
   try {
