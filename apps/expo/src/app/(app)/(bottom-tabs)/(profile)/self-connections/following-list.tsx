e4e37d99c0b09e179b63a9ce9ec82b637710e429
Einsight <ghaithkhalil2004@gmail.com>
2024-06-22 22:55:59 -0400

    fixed routing bug

--- apps/expo/src/app/(app)/(bottom-tabs)/(profile)/self-connections/following-list.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(profile)/self-connections/following-list.tsx
@@ -121,7 +121,7 @@ const FollowingList = () => {
                     onPress={() =>
                       // @ts-expect-error: Experimental typed routes dont support layouts yet
                       router.push({
-                        pathname: "/profile/[profile-id]",
+                        pathname: "/(profile)/profile/[profile-id]",
                         params: { profileId: String(item.profileId) },
                       })
                     }
