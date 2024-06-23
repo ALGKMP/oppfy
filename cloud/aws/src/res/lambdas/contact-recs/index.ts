481b8decbeb0042510df51271f1cb10c5a86cc40
matt <30198937+6mp@users.noreply.github.com>
2024-06-23 00:10:30 -0400

    gremlin things

--- cloud/aws/src/res/lambdas/contact-recs/index.ts
+++ cloud/aws/src/res/lambdas/contact-recs/index.ts
@@ -1,98 +1,23 @@
-/* import { SqsEnvelope } from "@aws-lambda-powertools/parser/envelopes";
-import { parser } from "@aws-lambda-powertools/parser/middleware";
-import middy from "@middy/core";
-import { APIGatewayProxyResult, Context } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import gremlin from "gremlin";
-import { z } from "zod";
 
-const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
-const Graph = gremlin.structure.Graph;
-const { t, P } = gremlin.process;
-const { onCreate, onMatch } = gremlin.process.merge;
-const __ = gremlin.process.statics;
+const {
+  driver: { DriverRemoteConnection },
+  structure: { Graph },
+  process: {
+    t,
+    P,
+    merge: { onCreate, onMatch },
+    statics: __,
+  },
+} = gremlin;
 
 const NEPTUNE_ENDPOINT = process.env.NEPTUNE_ENDPOINT;
 const NEPTUNE_PORT = process.env.NEPTUNE_PORT || 8182;
 
-const contactSyncBody = z.object({
-  userId: z.string(),
-  userPhoneNumberHash: z.string(),
-  contacts: z.array(z.string()),
-});
-
-interface Vertex {
-  id: string;
-  label: string;
-  properties: Record<string, any>;
-}
-
-async function updateContacts(
-  g: gremlin.process.GraphTraversalSource,
-  userId: string,
-  userPhoneNumberHash: string,
-  contacts: string[],
-): Promise<boolean> {
-  // Add or update the user vertex
-  let userResult = await g
-    .mergeV(
-      new Map([
-        [t.id, userId],
-        [t.label, "User"],
-      ]),
-    )
-    .option(
-      onCreate,
-      new Map([
-        ["created", Date.now().toString()],
-        ["phoneNumberHash", userPhoneNumberHash],
-      ]),
-    )
-    .option(
-      onMatch,
-      new Map([
-        ["phoneNumberHash", userPhoneNumberHash],
-        ["updatedAt", Date.now().toString()],
-      ]),
-    )
-    .next();
-
-  // Extract user vertex from the result and assert type
-  const user = userResult.value as Vertex;
-
-  // Remove existing contacts edges (both incoming and outgoing)
-  await g.V(user.id).outE("contacts").drop().iterate();
-
-  // Add bidirectional edges to all other users who have a phoneNumber that matches my edge phone number
-  await g
-    .V(user.id)
-    .as("currentUser")
-    .V()
-    .hasLabel("User")
-    .has("phoneNumberHash", P.within(contacts))
-    .where(P.neq("currentUser"))
-    .as("contactUser")
-    .addE("contacts")
-    .from_("currentUser")
-    .property("updatedAt", Date.now().toString())
-    .select("contactUser")
-    .addE("contacts")
-    .to("currentUser")
-    .property("updatedAt", Date.now().toString())
-    .iterate();
-
-  return true;
-}
-// list bc of middy powertools thing
-// 1. Parses data using SqsSchema.
-// 2. Parses records in body key using your schema and return them in a list.
-type ContactSyncBodyType = z.infer<typeof contactSyncBody>[];
-
-const lambdaHandler = async (
-  event: ContactSyncBodyType,
-  _context: Context,
+export const handler = async (
+  event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
-  console.log("Lambda invoked");
-
   let dc: any;
   let g: gremlin.process.GraphTraversalSource;
 
@@ -103,20 +28,14 @@ const lambdaHandler = async (
     dc = new DriverRemoteConnection(`wss://${NEPTUNE_ENDPOINT}/gremlin`, {});
     g = graph.traversal().withRemote(dc);
 
-    const { userId, userPhoneNumberHash, contacts } = event[0];
-    console.log("userId", userId);
-    console.log("userPhoneNumberHash", userPhoneNumberHash);
-    console.log("contacts", contacts);
+    console.log("connected");
 
-    await updateContacts(g, userId, userPhoneNumberHash, contacts);
-
-    console.log("Update successful");
+    // get all vertices
+    const result = await g.V().toList();
 
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        message: "Success",
-      }),
+      body: result.toString(),
     };
   } catch (error) {
     console.error("Error during execution", error);
@@ -131,34 +50,5 @@ const lambdaHandler = async (
       dc.close();
       console.log("Remote connection closed");
     }
-  }
-};
-
-export const handler = middy(lambdaHandler).use(
-  parser({ schema: contactSyncBody, envelope: SqsEnvelope }),
-);
- */
-
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-
-export const handler = async (
-  event: APIGatewayProxyEvent,
-): Promise<APIGatewayProxyResult> => {
-  try {
-    console.log("hi");
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "hi",
-      }),
-    };
-  } catch (err) {
-    console.log(err);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: "some error happened",
-      }),
-    };
   }
 };
