e3b00f0d87b8236a107aec639e7af19f569f5825
Einsight <ghaithkhalil2004@gmail.com>
2024-06-25 01:48:28 -0400

    using new relationship states + full rewrite

--- apps/expo/src/app/(app)/(bottom-tabs)/(profile)/self-connections/following-list.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(profile)/self-connections/following-list.tsx
@@ -1,35 +1,146 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo } from "react";
 import { useRouter } from "expo-router";
-import { useHeaderHeight } from "@react-navigation/elements";
 import { FlashList } from "@shopify/flash-list";
-import { UserRoundMinus, UserRoundPlus } from "@tamagui/lucide-icons";
-import { Separator, SizableText, View } from "tamagui";
+import { Send, UserRoundMinus, UserRoundPlus } from "@tamagui/lucide-icons";
+import { Input, SizableText, View, YStack } from "tamagui";
 
+import CardContainer from "~/components/Containers/CardContainer";
 import { VirtualizedListItem } from "~/components/ListItems";
+import type { ButtonProps } from "~/components/ListItems/VirtualizedListItem";
 import { EmptyPlaceholder } from "~/components/UIPlaceholders";
 import { BaseScreenView } from "~/components/Views";
+import useSearch from "~/hooks/useSearch";
+import type { RouterOutputs } from "~/utils/api";
 import { api } from "~/utils/api";
+import { PLACEHOLDER_DATA } from "~/utils/placeholder-data";
+
+type FollowingItem =
+  RouterOutputs["follow"]["paginateFollowingSelf"]["items"][0];
 
 const FollowingList = () => {
-  const headerHeight = useHeaderHeight();
-
   const router = useRouter();
+  const utils = api.useUtils();
 
-  const unfollow = api.follow.unfollowUser.useMutation({
-    onSuccess: (_data, variables) => {
-      setUnfollowed((prev) => ({
-        ...prev,
-        [variables.userId]: true,
-      }));
+  const followMutation = api.follow.followUser.useMutation({
+    onMutate: async (newData) => {
+      // Cancel outgoing fetches (so they don't overwrite our optimistic update)
+      await utils.follow.paginateFollowingSelf.cancel();
+
+      // Get the data from the queryCache
+      const prevData = utils.follow.paginateFollowingSelf.getInfiniteData({
+        pageSize: 20,
+      });
+      if (prevData === undefined) return;
+
+      // Optimistically update the data
+      utils.follow.paginateFollowingSelf.setInfiniteData(
+        { pageSize: 20 },
+        {
+          ...prevData,
+          pages: prevData.pages.map((page) => ({
+            ...page,
+            items: page.items.map((item) =>
+              item.userId === newData.userId
+                ? {
+                    ...item,
+                    relationshipState:
+                      item.privacy === "private"
+                        ? "followRequestSent"
+                        : "following",
+                  }
+                : item,
+            ),
+          })),
+        },
+      );
+
+      return { prevData };
+    },
+    onError: (_err, _newData, ctx) => {
+      if (ctx === undefined) return;
+      utils.follow.paginateFollowingSelf.setInfiniteData({}, ctx.prevData);
+    },
+    onSettled: async () => {
+      // Sync with server once mutation has settled
+      // await utils.follow.paginateFollowingSelf.invalidate();
     },
   });
 
-  const follow = api.follow.followUser.useMutation({
-    onSuccess: (_data, variables) => {
-      setUnfollowed((prev) => ({
-        ...prev,
-        [variables.userId]: false,
-      }));
+  const unfollowMutation = api.follow.unfollowUser.useMutation({
+    onMutate: async (newData) => {
+      // Cancel outgoing fetches (so they don't overwrite our optimistic update)
+      await utils.follow.paginateFollowingSelf.cancel();
+
+      // Get the data from the queryCache
+      const prevData = utils.follow.paginateFollowingSelf.getInfiniteData({
+        pageSize: 20,
+      });
+      if (prevData === undefined) return;
+
+      // Optimistically update the data
+      utils.follow.paginateFollowingSelf.setInfiniteData(
+        { pageSize: 20 },
+        {
+          ...prevData,
+          pages: prevData.pages.map((page) => ({
+            ...page,
+            items: page.items.map((item) =>
+              item.userId === newData.userId
+                ? { ...item, relationshipState: "notFollowing" }
+                : item,
+            ),
+          })),
+        },
+      );
+
+      return { prevData };
+    },
+    onError: (_err, _newData, ctx) => {
+      if (ctx === undefined) return;
+      utils.follow.paginateFollowingSelf.setInfiniteData({}, ctx.prevData);
+    },
+    onSettled: async () => {
+      // Sync with server once mutation has settled
+      // await utils.follow.paginateFollowingSelf.invalidate();
+    },
+  });
+
+  const cancelFollowRequest = api.request.cancelFollowRequest.useMutation({
+    onMutate: async (newData) => {
+      // Cancel outgoing fetches (so they don't overwrite our optimistic update)
+      await utils.follow.paginateFollowingSelf.cancel();
+
+      // Get the data from the queryCache
+      const prevData = utils.follow.paginateFollowingSelf.getInfiniteData({
+        pageSize: 20,
+      });
+      if (prevData === undefined) return;
+
+      // Optimistically update the data
+      utils.follow.paginateFollowingSelf.setInfiniteData(
+        { pageSize: 20 },
+        {
+          ...prevData,
+          pages: prevData.pages.map((page) => ({
+            ...page,
+            items: page.items.map((item) =>
+              item.userId === newData.senderId
+                ? { ...item, relationshipState: "notFollowing" }
+                : item,
+            ),
+          })),
+        },
+      );
+
+      return { prevData };
+    },
+    onError: (_err, _newData, ctx) => {
+      if (ctx === undefined) return;
+      utils.follow.paginateFollowingSelf.setInfiniteData({}, ctx.prevData);
+    },
+    onSettled: async () => {
+      // Sync with server once mutation has settled
+      // await utils.follow.paginateFollowingSelf.invalidate();
     },
   });
 
@@ -39,6 +150,7 @@ const FollowingList = () => {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
+    refetch,
   } = api.follow.paginateFollowingSelf.useInfiniteQuery(
     {
       pageSize: 20,
@@ -48,22 +160,14 @@ const FollowingList = () => {
     },
   );
 
-  const placeholderData = useMemo(() => {
-    return Array.from({ length: 20 }, () => null);
-  }, []);
-
-  const friendsItems = useMemo(() => {
-    return followingData?.pages.flatMap((page) => page.items);
+  const followingItems = useMemo(() => {
+    return followingData?.pages.flatMap((page) => page.items) ?? [];
   }, [followingData]);
 
-  const itemCount = useMemo(() => {
-    if (followingData === undefined) return 0;
-
-    return followingData.pages.reduce(
-      (total, page) => total + page.items.length,
-      0,
-    );
-  }, [followingData]);
+  const { searchQuery, setSearchQuery, filteredItems } = useSearch({
+    data: followingItems,
+    keys: ["name", "username"],
+  });
 
   const handleOnEndReached = async () => {
     if (!isFetchingNextPage && hasNextPage) {
@@ -71,75 +175,121 @@ const FollowingList = () => {
     }
   };
 
-  const [unfollowed, setUnfollowed] = useState<Record<string, boolean>>({});
+  const handleFollow = async (userId: string) =>
+    await followMutation.mutateAsync({ userId });
 
-  const toggleFollowStatus = (userId: string) => {
-    const isFollowing = !unfollowed[userId];
-    isFollowing ? unfollow.mutate({ userId }) : follow.mutate({ userId });
-  };
+  const handleUnfollow = async (userId: string) =>
+    await unfollowMutation.mutateAsync({ userId });
 
-  return (
-    <BaseScreenView paddingBottom={0}>
-      {isLoading || itemCount ? (
-        <FlashList
-          extraData={unfollowed}
-          data={isLoading ? placeholderData : friendsItems}
-          ItemSeparatorComponent={Separator}
-          estimatedItemSize={75}
-          onEndReached={handleOnEndReached}
-          showsVerticalScrollIndicator={false}
-          ListHeaderComponent={
-            <SizableText size="$2" theme="alt1" marginBottom="$2">
-              FOLLOWERS
-            </SizableText>
-          }
-          renderItem={({ item }) => {
-            return (
-              <View>
-                {item === null ? (
-                  <VirtualizedListItem
-                    loading
-                    showSkeletons={{
-                      imageUrl: true,
-                      title: true,
-                      subtitle: true,
-                      button: true,
-                    }}
-                  />
-                ) : (
-                  <VirtualizedListItem
-                    loading={false}
-                    title={item.username}
-                    subtitle={item.name}
-                    imageUrl={item.profilePictureUrl}
-                    button={{
-                      onPress: () => toggleFollowStatus(item.userId),
-                      ...(unfollowed[item.userId]
-                        ? { text: "Follow", icon: UserRoundPlus }
-                        : { text: "Unfollow", icon: UserRoundMinus }),
-                    }}
-                    onPress={() =>
-                      // @ts-expect-error: Experimental typed routes dont support layouts yet
-                      router.push({
-                        pathname: "/(profile)/profile/[profile-id]",
-                        params: { profileId: String(item.profileId) },
-                      })
-                    }
-                  />
-                )}
-              </View>
-            );
+  const handleCancelFollowRequest = async (senderId: string) =>
+    await cancelFollowRequest.mutateAsync({ senderId });
+
+  const renderLoadingSkeletons = () => (
+    <CardContainer>
+      {PLACEHOLDER_DATA.map((_, index) => (
+        <VirtualizedListItem
+          key={index}
+          loading
+          showSkeletons={{
+            imageUrl: true,
+            title: true,
+            subtitle: true,
+            button: true,
           }}
         />
-      ) : (
-        <View flex={1} justifyContent="center" bottom={headerHeight}>
-          <EmptyPlaceholder
-            title="Following"
-            subtitle="Once you follow someone, you'll see them here."
-            icon={<UserRoundPlus />}
-          />
-        </View>
-      )}
+      ))}
+    </CardContainer>
+  );
+
+  const renderButton = (item: FollowingItem): ButtonProps => {
+    switch (item.relationshipState) {
+      case "followRequestSent":
+        return {
+          text: "Sent",
+          icon: Send,
+          onPress: () => void handleCancelFollowRequest(item.userId),
+        };
+      case "following":
+        return {
+          text: "Unfollow",
+          icon: UserRoundMinus,
+          onPress: () => void handleUnfollow(item.userId),
+        };
+      default:
+        return {
+          text: "Follow",
+          icon: UserRoundPlus,
+          theme: "blue",
+          onPress: () => void handleFollow(item.userId),
+        };
+    }
+  };
+
+  const renderListItem = (item: FollowingItem) => (
+    <VirtualizedListItem
+      loading={false}
+      title={item.username}
+      subtitle={item.name}
+      imageUrl={item.profilePictureUrl}
+      button={renderButton(item)}
+      onPress={() =>
+        router.push({
+          pathname: "/(profile)/profile/[profile-id]",
+          params: { profileId: String(item.profileId) },
+        })
+      }
+    />
+  );
+
+  const renderFollowing = () => (
+    <CardContainer>
+      <FlashList
+        data={filteredItems}
+        onRefresh={refetch}
+        refreshing={isLoading}
+        estimatedItemSize={75}
+        onEndReached={handleOnEndReached}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => renderListItem(item)}
+      />
+    </CardContainer>
+  );
+
+  const renderNoResults = () => (
+    <View flex={1} justifyContent="center">
+      <EmptyPlaceholder
+        title="No results"
+        subtitle="No following found."
+        icon={<UserRoundPlus />}
+      />
+    </View>
+  );
+
+  if (isLoading) {
+    return (
+      <BaseScreenView scrollable>{renderLoadingSkeletons()}</BaseScreenView>
+    );
+  }
+
+  if (followingItems.length === 0) {
+    return <BaseScreenView>{renderNoResults()}</BaseScreenView>;
+  }
+
+  return (
+    <BaseScreenView scrollable>
+      <YStack gap="$4">
+        <Input
+          placeholder="Search following..."
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+        />
+
+        {filteredItems.length > 0 ? (
+          renderFollowing()
+        ) : (
+          <SizableText lineHeight={0}>No Users Found</SizableText>
+        )}
+      </YStack>
     </BaseScreenView>
   );
 };
