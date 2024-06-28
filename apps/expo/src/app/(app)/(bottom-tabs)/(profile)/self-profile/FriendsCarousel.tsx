fcb59f2a01e50db61ef58df94837b1c6a95050f7
Antony <antonylbrussolo@gmail.com>
2024-06-28 15:05:24 -0400

    FriendsCarousel component

---
+++ apps/expo/src/app/(app)/(bottom-tabs)/(profile)/self-profile/FriendsCarousel.tsx
@@ -0,0 +1,175 @@
+import React, { useCallback, useEffect, useRef } from "react";
+import { TouchableOpacity } from "react-native";
+import type { NativeScrollEvent, NativeSyntheticEvent } from "react-native";
+import * as Haptics from "expo-haptics";
+import { useRouter } from "expo-router";
+import { FlashList } from "@shopify/flash-list";
+import { throttle } from "lodash";
+import {
+  Avatar,
+  Button,
+  getToken,
+  ListItemTitle,
+  SizableText,
+  Spacer,
+  Text,
+  View,
+  XStack,
+  YStack,
+} from "tamagui";
+
+import { abbreviatedNumber } from "@oppfy/utils";
+
+import CardContainer from "~/components/Containers/CardContainer";
+import { Skeleton } from "~/components/Skeletons";
+import type { RouterOutputs } from "~/utils/api";
+import { PLACEHOLDER_DATA } from "~/utils/placeholder-data";
+
+type FriendItems = RouterOutputs["friend"]["paginateFriendsSelf"]["items"];
+
+interface FriendsData {
+  friendCount: number;
+  friendItems: FriendItems;
+}
+
+interface LoadingProps {
+  loading: true;
+}
+
+interface FriendsLoadedProps {
+  loading: false;
+  data: FriendsData;
+}
+
+type FriendsProps = LoadingProps | FriendsLoadedProps;
+
+const FriendsCarousel = (props: FriendsProps) => {
+  const router = useRouter();
+
+  const showMore =
+    !props.loading && props.data.friendItems.length < props.data.friendCount;
+
+  const handleFriendClicked = (profileId: number) => {
+    void Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+    router.navigate({
+      pathname: "/(profile)/profile/[profile-id]/",
+      params: { profileId: String(profileId) },
+    });
+  };
+
+  const handleShowMoreFriends = () => {
+    void Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+    router.push("/self-connections/friend-list");
+  };
+
+  const throttledHandleAction = useRef(
+    throttle(handleShowMoreFriends, 300, { leading: true, trailing: false }),
+  ).current;
+
+  const handleScroll = useCallback(
+    (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+      if (!showMore) return;
+
+      const { contentSize, contentOffset, layoutMeasurement } =
+        event.nativeEvent;
+
+      const contentWidth = contentSize.width;
+      const offsetX = contentOffset.x;
+      const layoutWidth = layoutMeasurement.width;
+
+      // Check if within the threshold from the end
+      if (offsetX + layoutWidth - 80 >= contentWidth) {
+        throttledHandleAction();
+      }
+    },
+    [showMore, throttledHandleAction],
+  );
+
+  useEffect(() => throttledHandleAction.cancel(), [throttledHandleAction]);
+
+  const renderLoadingSkeletons = () => (
+    <CardContainer>
+      <XStack gap="$2">
+        {PLACEHOLDER_DATA.map((item, index) => (
+          <Skeleton key={index} circular size={70} />
+        ))}
+      </XStack>
+    </CardContainer>
+  );
+
+  const renderSuggestions = () => (
+    <CardContainer>
+      <YStack gap="$2">
+        <Text fontWeight="600">Find Friends</Text>
+        <Button size="$3.5">@oxy add recommendations here</Button>
+      </YStack>
+    </CardContainer>
+  );
+
+  const renderFriendList = (data: FriendsData) => (
+    <CardContainer paddingHorizontal={0}>
+      <YStack gap="$2">
+        <TouchableOpacity onPress={handleShowMoreFriends}>
+          <ListItemTitle paddingLeft="$3">
+            Friends ({abbreviatedNumber(data.friendCount)})
+          </ListItemTitle>
+        </TouchableOpacity>
+
+        <FlashList
+          data={data.friendItems}
+          horizontal
+          estimatedItemSize={70}
+          showsHorizontalScrollIndicator={false}
+          onScroll={handleScroll}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => handleFriendClicked(item.profileId)}
+            >
+              <YStack gap="$1.5">
+                <Avatar circular size="$6" bordered>
+                  <Avatar.Image src={item.profilePictureUrl} />
+                </Avatar>
+                <Text fontWeight="600" textAlign="center">
+                  {item.username}
+                </Text>
+              </YStack>
+            </TouchableOpacity>
+          )}
+          ListFooterComponent={
+            showMore ? (
+              <View
+                marginRight={-100}
+                justifyContent="center"
+                alignItems="center"
+              >
+                <SizableText color="$blue7" fontWeight="600">
+                  See more
+                </SizableText>
+              </View>
+            ) : null
+          }
+          ItemSeparatorComponent={() => <Spacer size="$2" />}
+          contentContainerStyle={{
+            paddingHorizontal: getToken("$3", "space") as number,
+          }}
+          ListFooterComponentStyle={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        />
+      </YStack>
+    </CardContainer>
+  );
+
+  if (props.loading) {
+    return renderLoadingSkeletons();
+  }
+
+  if (props.data.friendCount === 0) {
+    return renderSuggestions();
+  }
+
+  return renderFriendList(props.data);
+};
+
+export default FriendsCarousel;
