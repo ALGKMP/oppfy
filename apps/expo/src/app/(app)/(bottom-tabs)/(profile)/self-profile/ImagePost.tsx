53663f32f02b3ae326d46aa168a32fa3accc1d14
Antony <antonylbrussolo@gmail.com>
2024-06-29 17:33:58 -0400

    feat: Update ImagePost component to use SVG heart icon with a gradient

--- apps/expo/src/app/(app)/(bottom-tabs)/(profile)/self-profile/ImagePost.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(profile)/self-profile/ImagePost.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import type { StyleProp, ViewStyle } from "react-native";
 import Animated from "react-native-reanimated";
+import Svg, { Defs, LinearGradient, Path, Stop } from "react-native-svg";
 import { Image } from "expo-image";
 import { Heart } from "@tamagui/lucide-icons";
-import type { StyleProp, ViewStyle } from "react-native";
 
 interface ImagePostProps {
   imageUrl: string;
@@ -28,7 +29,19 @@ const ImagePost: React.FC<ImagePostProps> = ({
       contentFit="cover"
     >
       <Animated.View style={[animatedHeartImageStyle]}>
-        <Heart size={100} color="red" fill="red" />
+        {/* <Heart size={100} color="red" fill="red" /> */}
+        <Svg height="100" width="100" viewBox="0 0 24 24">
+          <Defs>
+            <LinearGradient id="grad" x1="0" y1="0" x2="1" y2="1">
+              <Stop offset="0" stopColor="#ff7f7f" stopOpacity="1" />
+              <Stop offset="1" stopColor="#ff0000" stopOpacity="1" />
+            </LinearGradient>
+          </Defs>
+          <Path
+            d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+            fill="url(#grad)"
+          />
+        </Svg>
       </Animated.View>
     </Image>
   );
