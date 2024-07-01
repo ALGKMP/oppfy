42cb56d7d9b0f1b26dae47787aeb40fd746bf9b7
Einsight <ghaithkhalil2004@gmail.com>
2024-07-01 02:09:56 -0400

    init

---
+++ apps/expo/src/features/onboarding/components/OnboardingInput.tsx
@@ -0,0 +1,17 @@
+import { Input, styled } from "tamagui";
+
+const OnboardingInput = styled(Input, {
+  flex: 1,
+  height: 70,
+  borderRadius: "$9",
+  backgroundColor: "$gray3",
+  paddingLeft: "$3",
+  paddingRight: "$3",
+  selectionColor: "$color",
+  borderWidth: 0,
+  color: "$color",
+  fontSize: "$8",
+  shadowColor: "$gray6",
+});
+
+export default OnboardingInput;
