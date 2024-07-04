628e931b3668072219c598011d878adb79020d70
Antony <antonylbrussolo@gmail.com>
2024-07-04 19:49:26 -0400

    PostActionBottomSheet

---
+++ apps/expo/src/components/BottomSheets/PostActionsBottomSheet.tsx
@@ -0,0 +1,78 @@
+import { useCallback, useEffect, useMemo, useRef } from "react";
+import { Dimensions, Modal, TouchableOpacity } from "react-native";
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import BottomSheet, {
+  BottomSheetFlatList,
+  BottomSheetFooter,
+  BottomSheetSectionList,
+} from "@gorhom/bottom-sheet";
+
+interface PostActionBottomSheetProps {
+  postId: number;
+  modalVisible: boolean;
+  setModalVisible: (value: boolean) => void;
+}
+
+const PostActionsBottomSheet = ({
+  postId,
+  modalVisible,
+  setModalVisible,
+}: PostActionBottomSheetProps) => {
+  const insets = useSafeAreaInsets();
+  const { height: screenHeight } = useMemo(() => Dimensions.get("window"), []);
+  const sheetRef = useRef<BottomSheet>(null);
+
+  const openModal = useCallback(() => {
+    sheetRef.current?.expand();
+  }, [sheetRef]);
+
+  const closeModal = useCallback(() => {
+    setModalVisible(false);
+    sheetRef.current?.close();
+  }, [sheetRef, setModalVisible]);
+
+  useEffect(() => {
+    if (modalVisible) {
+      openModal();
+    } else {
+      closeModal();
+    }
+  }, [modalVisible, closeModal, openModal]);
+
+  const animatedPosition = useSharedValue(0);
+  const animatedOverlayStyle = useAnimatedStyle(() => {
+    const heightPercentage = animatedPosition.value / screenHeight;
+    const opacity = interpolate(
+      heightPercentage,
+      [0.2, 0.9],
+      [0.9, 0],
+      "clamp",
+    );
+    return { backgroundColor: `rgba(0, 0, 0, ${opacity})` };
+  });
+
+  return (
+    <Modal visible={modalVisible} transparent={true}>
+      <TouchableOpacity onPress={closeModal} />
+      <Animated.View style={[animatedOverlayStyle, { flex: 1 }]}>
+        <BottomSheet
+          onClose={closeModal}
+          snapPoints={["60%"]}
+          enablePanDownToClose={true}
+          animatedPosition={animatedPosition}
+          ref={sheetRef}
+        >
+          {/* Add a childrens prop */}
+          <></>
+        </BottomSheet>
+      </Animated.View>
+    </Modal>
+  );
+};
+
+export default PostActionsBottomSheet;
