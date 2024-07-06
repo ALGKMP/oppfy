55ed8f288e8304731e4ae5f65abe186aad970657
Antony <antonylbrussolo@gmail.com>
2024-07-05 22:50:41 -0400

    wip: share bottom sheet

---
+++ apps/expo/src/components/BottomSheets/ShareBottomSheet.tsx
@@ -0,0 +1,40 @@
+import React, { useCallback, useEffect, useMemo, useRef } from "react";
+import type BottomSheet from "@gorhom/bottom-sheet";
+import { Text, View } from "tamagui";
+
+import BottomSheetWrapper from "./BottomSheetWrapper";
+
+interface ShareBottomSheetProps {
+  modalVisible: boolean;
+  setModalVisible: (value: boolean) => void;
+}
+
+const ShareBottomSheet = (props: ShareBottomSheetProps) => {
+  const { modalVisible, setModalVisible } = props;
+  const sheetRef = useRef<BottomSheet>(null);
+
+  const closeModal = useCallback(() => {
+    sheetRef.current?.close();
+    setModalVisible(false);
+  }, [sheetRef, setModalVisible]);
+
+  const openModal = useCallback(() => {
+    sheetRef.current?.expand();
+  }, [sheetRef]);
+
+  return (
+    <BottomSheetWrapper
+      sheetRef={sheetRef}
+      modalVisible={modalVisible}
+      onClose={closeModal}
+      onOpen={openModal}
+      snapPoints={["50%", "90%"]}
+    >
+      <View>
+        <Text>Text</Text>
+      </View>
+    </BottomSheetWrapper>
+  );
+};
+
+export default ShareBottomSheet;
