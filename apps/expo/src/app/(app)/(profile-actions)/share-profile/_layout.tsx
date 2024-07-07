51a6a3764c2c4e928193151bdb3a20ca85837238
Einsight <ghaithkhalil2004@gmail.com>
2024-07-07 00:10:22 -0400

    init

--- apps/expo/src/app/(app)/(profile-actions)/share-profile/_layout.tsx
+++ apps/expo/src/app/(app)/(profile-actions)/share-profile/_layout.tsx
@@ -6,33 +6,65 @@ import type {
   HeaderBackButtonProps,
   NativeStackHeaderProps,
 } from "@react-navigation/native-stack/src/types";
-import { ChevronLeft, QrCode } from "@tamagui/lucide-icons";
+import { ChevronLeft, QrCode, X } from "@tamagui/lucide-icons";
 import { Text } from "tamagui";
 
 import { Header as BaseHeader } from "~/components/Headers";
 import { Stack } from "~/layouts";
 
-const ProfileActionsLayout = () => {
+const ShareProfileLayout = () => {
   const router = useRouter();
 
   return (
     <Stack
       screenOptions={{
-        headerLeft: (props) => <HeaderLeft {...props} />,
         header: (props) => <Header {...props} />,
       }}
     >
       <Stack.Screen
-        name="edit-profile"
+        name="index"
         options={{
-          title: "Edit Profile",
+          title: "Share Profile",
+          headerTransparent: true,
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => router.navigate("/profile-actions")}
+            >
+              <X />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {
+                router.navigate("/share-profile/scan-qr");
+              }}
+            >
+              <QrCode />
+            </TouchableOpacity>
+          ),
         }}
       />
+
       <Stack.Screen
-        name="share-profile"
+        name="scan-qr"
         options={{
-          header: () => null,
-          animation: "slide_from_bottom",
+          title: "Scan QR",
+          animation: "fade",
+          headerTransparent: true,
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <ChevronLeft />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {
+                router.navigate("scan-qr");
+              }}
+            >
+              <QrCode />
+            </TouchableOpacity>
+          ),
         }}
       />
     </Stack>
@@ -94,4 +126,4 @@ const Header = ({ navigation, options }: HeaderProps) => (
   />
 );
 
-export default ProfileActionsLayout;
+export default ShareProfileLayout;
---
+++ apps/expo/src/app/(app)/(profile-actions)/share-profile/scan-qr.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ScanQr = () => {
+  return (
+    <View>
+      <Text>ScanQr</Text>
+    </View>
+  );
+};
+
+export default ScanQr;
