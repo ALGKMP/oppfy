791643bfa7ff5b5a5d5bee9d1c39a3c142ce1ca6
Einsight <ghaithkhalil2004@gmail.com>
2024-07-09 01:55:14 -0400

    re-exporting drizzle stuff

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,18 +1,3 @@
-// import { drizzle } from "drizzle-orm/mysql2";
-// import { migrate } from "drizzle-orm/postgres-js/migrator";
-// import * as mysql from "mysql2/promise";
-// import postgres from "postgres";
-
-// import { env } from "@oppfy/env";
-
-// import * as migration from "./src/schema/schema";
-
-// export const schema = { ...migration };
-
-// export { mySqlTable as tableCreator } from "./src/schema/_table";
-
-// export * from "drizzle-orm";
-
 // export const connection = await mysql.createConnection({
 //   port: Number(env.DATABASE_PORT),
 //   host: env.DATABASE_ENDPOINT,
@@ -27,14 +12,31 @@
 //   mode: "default",
 // });
 
+// import { drizzle } from "drizzle-orm/mysql2";
+// import { migrate } from "drizzle-orm/postgres-js/migrator";
+// import * as mysql from "mysql2/promise";
+// import postgres from "postgres";
+
+// import { env } from "@oppfy/env";
+
+// import * as migration from "./src/schema/schema";
+
+// export const schema = { ...migration };
+
+// export { mySqlTable as tableCreator } from "./src/schema/_table";
+
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 import * as schema from "./src/schema";
 
+export * from "drizzle-orm";
+
 const queryClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db");
 
-export const db = drizzle(queryClient, {
+const db = drizzle(queryClient, {
   schema,
   logger: true,
 });
+
+export { db, schema };
