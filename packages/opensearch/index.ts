712caf6a433548e011a3f122a52b90efe6795509
Einsight <ghaithkhalil2004@gmail.com>
2024-07-09 19:07:10 -0400

    refactored

--- packages/opensearch/index.ts
+++ packages/opensearch/index.ts
@@ -4,6 +4,33 @@ import { AwsSigv4Signer } from "@opensearch-project/opensearch/aws";
 
 import { env } from "@oppfy/env";
 
+export enum OpenSearchIndex {
+  PROFILE = "profile",
+}
+
+export interface OpenSearchProfileIndexResult {
+  id: number;
+  username: string;
+  fullName: string;
+  bio: string;
+  profilePictureKey: string;
+}
+
+export interface OpenSearchResponse<T> {
+  hits: {
+    total: {
+      value: number;
+    };
+    hits: {
+      _index: string;
+      _type: string;
+      _id: string;
+      _score: number;
+      _source: T;
+    }[];
+  };
+}
+
 export const openSearch = new OpenSearchClient({
   ...AwsSigv4Signer({
     region: env.AWS_REGION,
