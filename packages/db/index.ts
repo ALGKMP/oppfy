117a44bb05d5b1141fea8a179138f24d24ba26ea
Einsight <ghaithkhalil2004@gmail.com>
2024-07-09 02:57:47 -0400

    cleanup

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,30 +1,3 @@
-// export const connection = await mysql.createConnection({
-//   port: Number(env.DATABASE_PORT),
-//   host: env.DATABASE_ENDPOINT,
-//   user: env.DATABASE_USERNAME,
-//   database: env.DATABASE_NAME,
-//   password: env.DATABASE_PASSWORD,
-// });
-
-// export const db = drizzle(connection, {
-//   logger: true,
-//   schema,
-//   mode: "default",
-// });
-
-// import { drizzle } from "drizzle-orm/mysql2";
-// import { migrate } from "drizzle-orm/postgres-js/migrator";
-// import * as mysql from "mysql2/promise";
-// import postgres from "postgres";
-
-// import { env } from "@oppfy/env";
-
-// import * as migration from "./src/schema/schema";
-
-// export const schema = { ...migration };
-
-// export { mySqlTable as tableCreator } from "./src/schema/_table";
-
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
--- packages/db/migrate.ts
+++ packages/db/migrate.ts
@@ -1,15 +1,3 @@
-// import "dotenv/config";
-
-// import { migrate } from "drizzle-orm/mysql2/migrator";
-
-// import { connection, db } from "./index";
-
-// // This will run migrations on the database, skipping the ones already applied
-// await migrate(db, { migrationsFolder: "./drizzle" });
-
-// // Don't forget to close the connection, otherwise the script will hang
-// await connection.end();
-
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
