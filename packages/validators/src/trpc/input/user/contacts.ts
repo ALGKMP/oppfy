517f8dcae19e365ea44b43dfb0e7daa898993c16
Antony <antonylbrussolo@gmail.com>
2024-07-15 19:08:58 -0400

    feat: add getRecommendationProfilesOther to user contacts input schema

--- packages/validators/src/trpc/input/user/contacts.ts
+++ packages/validators/src/trpc/input/user/contacts.ts
@@ -2,6 +2,10 @@ import { z } from "zod";
 
 const trpcContactsInputSchema = {
   syncContacts: z.array(z.string()),
+
+  getRecommendationProfilesOther: z.object({
+    profileId: z.number(),
+  }),
 };
 
 export default trpcContactsInputSchema;
