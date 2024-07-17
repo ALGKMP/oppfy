af9ba7c1bd98eb9f4c136d1883b9bb1de650db80
Antony <antonylbrussolo@gmail.com>
2024-07-17 00:56:52 -0400

    layout

--- apps/expo/src/app/(app)/(bottom-tabs)/(inbox, profile, search)/profile/_layout.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(inbox, profile, search)/profile/_layout.tsx
@@ -1,31 +1,77 @@
 import { Slot, Stack } from "expo-router";
+import {Text} from "tamagui";
+import type {
+  HeaderBackButtonProps,
+  NativeStackHeaderProps,
+} from "@react-navigation/native-stack/src/types";
+import { useTheme } from "tamagui";
+
+import { Header as BaseHeader } from "~/components/Headers";
+
+type HeaderProps = NativeStackHeaderProps;
 
 const ProfileLayout = () => {
-  return (
-    <Slot />
-    // <Stack
-    //   screenOptions={{
-    //     header: () => null,
-    //   }}
-    // >
-    //   {/* <Stack.Screen
-    //   name="[profile-id]"
-    //   options={{
-    //     headerShown: false,
-    //     // header: () => null,
-    //     // headerRight: () => (
-    //     //   <View>
-    //     //     <Pressable onPress={() => console.log("THING CLICKED")}>
-    //     //       {({ pressed }) => (
-    //     //         <MoreHorizontal style={{ opacity: pressed ? 0.5 : 1 }} />
-    //     //       )}
-    //     //     </Pressable>
-    //     //   </View>
-    //   }}
-    //   /> */}
+  const theme = useTheme();
 
-    // </Stack>
+  return (
+    <Stack
+      screenOptions={{
+        header: (props) => <Header {...props} />, // So this bitch makes it so the header doesn't look like shit rn
+        contentStyle: { backgroundColor: theme.background.val }, // this bitch sets the color for wtv the fuck idek
+      }}
+    >
+      <Stack.Screen
+        name="[profile-id]"
+        options={
+          {
+            // headerShown: false,
+            // header: () => null,
+            // headerRight: () => (
+            //   <View>
+            //     <Pressable onPress={() => console.log("THING CLICKED")}>
+            //       {({ pressed }) => (
+            //         <MoreHorizontal style={{ opacity: pressed ? 0.5 : 1 }} />
+            //       )}
+            //     </Pressable>
+            //   </View>
+          }
+        }
+      />
+    </Stack>
   );
 };
+
+const Header = ({ navigation, options }: HeaderProps) => (
+  <BaseHeader
+    HeaderLeft={
+      options.headerLeft
+        ? options.headerLeft({
+            canGoBack: navigation.canGoBack(),
+            tintColor: options.headerTintColor,
+          })
+        : undefined
+    }
+    HeaderTitle={
+      typeof options.headerTitle === "function" ? (
+        options.headerTitle({
+          children: options.title ?? "",
+          tintColor: options.headerTintColor,
+        })
+      ) : options.title ? (
+        <Text fontSize="$5" fontWeight="bold">
+          {options.title}
+        </Text>
+      ) : null
+    }
+    HeaderRight={
+      options.headerRight
+        ? options.headerRight({
+            canGoBack: navigation.canGoBack(),
+            tintColor: options.headerTintColor,
+          })
+        : undefined
+    }
+  />
+);
 
 export default ProfileLayout;
