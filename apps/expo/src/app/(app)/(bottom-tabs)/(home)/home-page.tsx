7fb5835208d487450b87f6aa22193ab36a57c36b
matt <30198937+6mp@users.noreply.github.com>
2024-07-19 12:47:16 -0400

    recs on top of home page

---
+++ apps/expo/src/app/(app)/(bottom-tabs)/(home)/home-page.tsx
@@ -0,0 +1,135 @@
+import { useCallback, useMemo, useState } from "react";
+import { Dimensions } from "react-native";
+import { FlashList, ViewToken } from "@shopify/flash-list";
+import { Camera } from "@tamagui/lucide-icons";
+import { SizableText, Text, useTheme, View, YStack } from "tamagui";
+
+import RecommendationsCarousel from "~/components/Carousels/RecommendationsCarousel";
+import { api } from "~/utils/api";
+import PostItem from "../../../../components/Media/PostItem";
+
+const { width: screenWidth } = Dimensions.get("window");
+
+const HomeScreen = () => {
+  const [status, setStatus] = useState<"success" | "loading" | "error">(
+    "loading",
+  );
+  const [refreshing, setRefreshing] = useState(false);
+  const [viewableItems, setViewableItems] = useState<number[]>([]);
+
+  const {
+    data: postData,
+    isLoading: isLoadingPostData,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+    refetch,
+  } = api.post.paginatePostsForFeed.useInfiniteQuery(
+    {
+      pageSize: 10,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    },
+  );
+
+  // Recommendations data
+  const selfRecommendationsQuery =
+    api.contacts.getRecommendationProfilesSelf.useQuery();
+  const isLoadingRecommendationsData = selfRecommendationsQuery.isLoading;
+  const recommendationsData = selfRecommendationsQuery.data;
+  const refetchRecommendationsData = selfRecommendationsQuery.refetch;
+
+  const handleOnEndReached = async () => {
+    if (!isFetchingNextPage && hasNextPage) {
+      await fetchNextPage();
+    }
+  };
+
+  const posts = useMemo(
+    () => postData?.pages.flatMap((page) => page.items),
+    [postData],
+  );
+
+  /*   const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await Promise.all([
+    ]);
+    setRefreshing(false);
+  }, [refetchFriendsData, refetchProfileData, refetchRecomendationsData]); */
+
+  const onViewableItemsChanged = useCallback(
+    ({ viewableItems }: { viewableItems: ViewToken[] }) => {
+      const visibleItemIds = viewableItems
+        .filter((token) => token.isViewable)
+        .map((token) => token.item?.postId)
+        .filter((id): id is number => id !== undefined);
+
+      setViewableItems(visibleItemIds);
+    },
+    [],
+  );
+
+  const viewabilityConfig = {
+    itemVisiblePercentThreshold: 40,
+  };
+
+  return (
+    <View flex={1} width="100%" height="100%">
+      {isLoadingRecommendationsData ? (
+        <RecommendationsCarousel loading />
+      ) : (
+        recommendationsData && (
+          <RecommendationsCarousel
+            loading={isLoadingRecommendationsData}
+            reccomendationsData={recommendationsData}
+          ></RecommendationsCarousel>
+        )
+      )}
+
+      {/*       <RecommendationsCarousel
+        loading={isLoadingRecommendationsData}
+        reccomendationsData={recommendationsData}
+      ></RecommendationsCarousel>
+ */}
+      <FlashList
+        nestedScrollEnabled={true}
+        data={posts}
+        refreshing={refreshing}
+        showsVerticalScrollIndicator={false}
+        // onRefresh={onRefresh}
+        numColumns={1}
+        onEndReached={handleOnEndReached}
+        keyExtractor={(item) => {
+          return "home_" + item?.postId.toString() ?? "home_undefined";
+        }}
+        renderItem={({ item }) => {
+          if (item === undefined) {
+            return null;
+          }
+          return (
+            <>
+              {isLoadingPostData ? (
+                <>
+                  <Text>Loading...</Text>
+                </>
+              ) : (
+                <PostItem
+                  post={item}
+                  isSelfPost={false}
+                  isViewable={viewableItems.includes(item.postId)}
+                />
+              )}
+            </>
+          );
+        }}
+        estimatedItemSize={screenWidth}
+        onViewableItemsChanged={onViewableItemsChanged}
+        viewabilityConfig={viewabilityConfig}
+        extraData={viewableItems}
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
