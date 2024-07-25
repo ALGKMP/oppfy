b2c447564964ba5c845928b5f1544a107c75e997
Antony <antonylbrussolo@gmail.com>
2024-07-25 05:19:08 -0400

    revert

--- packages/api/src/routers/user/user.ts
+++ packages/api/src/routers/user/user.ts
@@ -1,60 +1,91 @@
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+
 import { trpcValidators } from "@oppfy/validators";
 
 import {
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
-  withAsyncErrorHandling,
 } from "../../trpc";
 
 export const userRouter = createTRPCRouter({
   createUser: publicProcedure
     .input(trpcValidators.input.user.createUser)
-    .mutation(({ ctx, input }) =>
-      withAsyncErrorHandling(
-        () => ctx.services.user.createUser(input.userId, input.phoneNumber),
-        "Failed to create a new user",
-      )(),
-    ),
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.services.user.createUser(input.userId, input.phoneNumber);
+      } catch (err) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to create a new user",
+          cause: err,
+        });
+      }
+    }),
 
-  deleteUser: protectedProcedure.mutation(({ ctx }) =>
-    withAsyncErrorHandling(
-      () => ctx.services.user.deleteUser(ctx.session.uid),
-      `Failed to delete user with ID ${ctx.session.uid}`,
-    )(),
-  ),
+  deleteUser: protectedProcedure.mutation(async ({ ctx }) => {
+    try {
+      await ctx.services.user.deleteUser(ctx.session.uid);
+    } catch (err) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: `Failed to delete user with ID ${ctx.session.uid}`,
+        cause: err,
+      });
+    }
+  }),
 
-  onboardingComplete: protectedProcedure.query(({ ctx }) =>
-    withAsyncErrorHandling(
-      () => ctx.services.user.checkOnboardingComplete(ctx.session.uid),
-      "Failed to check if user has completed the onboarding process",
-    )(),
-  ),
+  onboardingComplete: protectedProcedure.query(async ({ ctx }) => {
+    try {
+      return await ctx.services.user.checkOnboardingComplete(ctx.session.uid);
+    } catch (err) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Failed to check if user has completed the onboarding process",
+        cause: err,
+      });
+    }
+  }),
 
-  checkOnboardingComplete: protectedProcedure.mutation(({ ctx }) =>
-    withAsyncErrorHandling(
-      () => ctx.services.user.checkOnboardingComplete(ctx.session.uid),
-      "Failed to check if user has completed the onboarding process",
-    )(),
-  ),
+  checkOnboardingComplete: protectedProcedure.mutation(async ({ ctx }) => {
+    try {
+      return await ctx.services.user.checkOnboardingComplete(ctx.session.uid);
+    } catch (err) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Failed to check if user has completed the onboarding process",
+        cause: err,
+      });
+    }
+  }),
 
-  getPrivacySetting: protectedProcedure.query(({ ctx }) =>
-    withAsyncErrorHandling(
-      () => ctx.services.privacy.getPrivacySettings(ctx.session.uid),
-      "Failed to get privacy settings",
-    )(),
-  ),
+  getPrivacySetting: protectedProcedure.query(async ({ ctx }) => {
+    try {
+      return await ctx.services.privacy.getPrivacySettings(ctx.session.uid);
+    } catch (err) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Failed to get privacy settings",
+        cause: err,
+      });
+    }
+  }),
 
   updatePrivacySetting: protectedProcedure
     .input(trpcValidators.input.user.updatePrivacySetting)
-    .mutation(({ input, ctx }) =>
-      withAsyncErrorHandling(
-        () =>
-          ctx.services.privacy.updatePrivacySettings(
-            ctx.session.uid,
-            input.privacy,
-          ),
-        "Failed to update privacy settings",
-      )(),
-    ),
+    .mutation(async ({ input, ctx }) => {
+      try {
+        await ctx.services.privacy.updatePrivacySettings(
+          ctx.session.uid,
+          input.privacy,
+        );
+      } catch (err) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to update privacy settings",
+          cause: err,
+        });
+      }
+    }),
 });
