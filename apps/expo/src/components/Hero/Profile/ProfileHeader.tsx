d8515f2c55263745608ebad71d52cb37babebfd1
Antony <antonylbrussolo@gmail.com>
2024-07-28 13:54:44 -0400

    header self type

--- apps/expo/src/components/Hero/Profile/ProfileHeader.tsx
+++ apps/expo/src/components/Hero/Profile/ProfileHeader.tsx
@@ -7,27 +7,26 @@ import FriendsCarousel from "../../Carousels/FriendsCarousel";
 import ProfileHeaderDetailsOther from "./components/ProfileHeaderDetailsOther";
 import ProfileHeaderDetailsSelf from "./components/ProfileHeaderDetailsSelf";
 
+// Define types at the top of the file
+type FullProfileOther = z.infer<typeof trpcValidators.output.profile.fullProfileOther>;
+type FullProfileSelf = z.infer<typeof trpcValidators.output.profile.fullProfileSelf>;
+type FriendItems = z.infer<typeof trpcValidators.output.friend.friendItems>;
+type FriendItemsOther = z.infer<typeof trpcValidators.output.friend.friendItemsOther>;
+type RecommendedProfiles = z.infer<typeof trpcValidators.output.recommendations.recommededProfiles>;
+
 interface ProfileHeaderProps {
   isSelfProfile: boolean;
   isLoadingProfileData: boolean;
   isLoadingFriendsData: boolean;
   isLoadingRecommendationsData: boolean;
-  profileData:
-    | z.infer<typeof trpcValidators.output.profile.fullProfileOther>
-    | z.infer<typeof trpcValidators.output.profile.fullProfileSelf>
-    | undefined;
-  friendsData:
-    | z.infer<typeof trpcValidators.output.friend.friendItems>
-    | z.infer<typeof trpcValidators.output.friend.friendItemsOther>
-    | undefined;
-  recommendationsData:
-    | z.infer<typeof trpcValidators.output.recommendations.recommededProfiles>
-    | undefined;
+  profileData: FullProfileOther | FullProfileSelf | undefined;
+  friendsData: FriendItems | FriendItemsOther | undefined;
+  recommendationsData: RecommendedProfiles | undefined;
 }
 
 function isOtherProfile(
   profile: ProfileHeaderProps["profileData"],
-): profile is z.infer<typeof trpcValidators.output.profile.fullProfileOther> {
+): profile is FullProfileOther {
   return profile !== undefined && "networkStatus" in profile;
 }
 
@@ -60,7 +59,10 @@ const ProfileHeader = (props: ProfileHeaderProps) => {
     <YStack gap="$5" marginBottom="$5">
       <YStack gap="$5">
         {isSelfProfile ? (
-          <ProfileHeaderDetailsSelf loading={false} data={profileData} />
+          <ProfileHeaderDetailsSelf
+            loading={false}
+            data={profileData as FullProfileSelf}
+          />
         ) : isOtherProfile(profileData) ? (
           <ProfileHeaderDetailsOther loading={false} data={profileData} />
         ) : (
