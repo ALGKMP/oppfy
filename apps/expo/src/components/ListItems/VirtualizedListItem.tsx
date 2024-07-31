4e18119557e4ddc55f165b9d5ec8f40e7bdef872
Einsight <ghaithkhalil2004@gmail.com>
2024-07-31 19:05:41 -0400

    rewrite to allow react elements for text props + cleanup

--- apps/expo/src/components/ListItems/VirtualizedListItem.tsx
+++ apps/expo/src/components/ListItems/VirtualizedListItem.tsx
@@ -1,45 +1,31 @@
+import type { ReactNode } from "react";
 import React, { useMemo } from "react";
 import type { ImageSourcePropType } from "react-native";
-import { TouchableOpacity, View } from "react-native";
+import { TouchableOpacity } from "react-native";
 import debounce from "lodash/debounce";
-import type {
-  ButtonProps as BaseButtonProps,
-  SizeTokens,
-  ViewProps,
-} from "tamagui";
+import type { ButtonProps as BaseButtonProps, ViewProps } from "tamagui";
 import { Avatar, Button, SizableText, Spacer, XStack, YStack } from "tamagui";
 
 import { Skeleton } from "~/components/Skeletons";
 import StatusRenderer from "../StatusRenderer";
 
+type ReactElementOrString = ReactNode | string;
+
 interface LoadingProps {
   loading: true;
-  showSkeletons?: {
-    imageUrl?: boolean;
-    title?: boolean;
-    subtitle?: boolean;
-    subtitle2?: boolean;
-    button?: boolean;
-    button2?: boolean;
-  };
+  showSkeletons?: Partial<Record<keyof Omit<LoadedProps, "loading">, boolean>>;
 }
 
 export interface ButtonProps extends BaseButtonProps {
   text: string;
 }
 
-const isButtonProps = (
-  button: ButtonProps | React.ReactNode,
-): button is ButtonProps => {
-  return button !== null && typeof button === "object" && "text" in button;
-};
-
 interface LoadedProps {
   loading: false;
   imageUrl?: string | ImageSourcePropType;
-  title?: string;
-  subtitle?: string;
-  subtitle2?: string;
+  title?: ReactElementOrString;
+  subtitle?: ReactElementOrString;
+  subtitle2?: ReactElementOrString;
   button?: ButtonProps | React.ReactElement;
   button2?: ButtonProps | React.ReactElement;
 }
@@ -49,123 +35,135 @@ type VirtualizedListItemProps = (LoadingProps | LoadedProps) & {
   containerStyle?: ViewProps;
 };
 
-interface DefaultButtonProps {
-  width?: number | undefined;
-  size?: SizeTokens | number | undefined;
-}
+type DefaultButtonProps = Partial<Pick<BaseButtonProps, "width" | "size">>;
+
+const isButtonProps = (
+  button: ButtonProps | React.ReactNode,
+): button is ButtonProps => {
+  return button !== null && typeof button === "object" && "text" in button;
+};
+
+const renderContent = (
+  content: ReactElementOrString,
+  defaultProps: object,
+): ReactNode => {
+  if (React.isValidElement(content)) {
+    return content;
+  }
+  if (typeof content === "string") {
+    return <SizableText {...defaultProps}>{content}</SizableText>;
+  }
+  return null;
+};
 
 const VirtualizedListItem = (props: VirtualizedListItemProps) => {
   const debouncedOnPress = useMemo(
-    () => (props.onPress ? debounce(props.onPress, 300) : undefined),
+    () => props.onPress && debounce(props.onPress, 300),
     [props.onPress],
   );
 
-  const shouldRenderProfilePicture =
-    (props.loading && !!props.showSkeletons?.imageUrl) ||
-    (!props.loading && !!props.imageUrl);
+  const shouldRender = (key: keyof Omit<LoadedProps, "loading">): boolean =>
+    (props.loading && !!props.showSkeletons?.[key]) ||
+    (!props.loading && !!props[key]);
 
-  const shouldRenderTitle =
-    (props.loading && !!props.showSkeletons?.title) ||
-    (!props.loading && !!props.title);
-
-  const shouldRenderSubtitle =
-    (props.loading && !!props.showSkeletons?.subtitle) ||
-    (!props.loading && !!props.subtitle);
-
-  const shouldRenderSubtitle2 =
-    (props.loading && !!props.showSkeletons?.subtitle2) ||
-    (!props.loading && !!props.subtitle2);
-
-  const shouldRenderButton =
-    (props.loading && !!props.showSkeletons?.button) ||
-    (!props.loading && !!props.button);
-
-  const shouldRenderButton2 =
-    (props.loading && !!props.showSkeletons?.button2) ||
-    (!props.loading && !!props.button2);
-
-  const isTwoButtons = shouldRenderButton && shouldRenderButton2;
-  const defaultButtonProps = {
+  const isTwoButtons = shouldRender("button") && shouldRender("button2");
+  const defaultButtonProps: DefaultButtonProps = {
     size: isTwoButtons ? "$3.5" : "$3.5",
-  } as const satisfies DefaultButtonProps;
+  };
+
+  const renderButton = (button?: ButtonProps | React.ReactElement) => {
+    if (!button) return null;
+    if (isButtonProps(button)) {
+      return (
+        <Button
+          {...defaultButtonProps}
+          {...button}
+          onPress={button.onPress && debounce(button.onPress, 300)}
+        >
+          {button.text}
+        </Button>
+      );
+    }
+    return button;
+  };
 
   const content = (
     <XStack alignItems="center" paddingVertical="$2" {...props.containerStyle}>
       <XStack flex={1} alignItems="center" gap="$3">
-        <View>
-          {shouldRenderProfilePicture && (
-            <StatusRenderer
-              data={
-                !props.loading && props.imageUrl ? props.imageUrl : undefined
-              }
-              loadingComponent={<Skeleton circular size={56} />}
-              successComponent={(imageUrl) => (
-                <Avatar circular size={56}>
-                  <Avatar.Image
-                    source={
-                      typeof imageUrl === "string"
-                        ? { uri: imageUrl }
-                        : imageUrl
-                    }
-                  />
-                </Avatar>
-              )}
-            />
-          )}
-        </View>
+        {shouldRender("imageUrl") && (
+          <StatusRenderer
+            data={!props.loading && props.imageUrl ? props.imageUrl : undefined}
+            loadingComponent={<Skeleton circular size={56} />}
+            successComponent={(imageUrl) => (
+              <Avatar circular size={56}>
+                <Avatar.Image
+                  source={
+                    typeof imageUrl === "string" ? { uri: imageUrl } : imageUrl
+                  }
+                />
+              </Avatar>
+            )}
+          />
+        )}
 
         <YStack flex={1}>
-          {shouldRenderTitle && (
+          {shouldRender("title") && (
             <StatusRenderer
               data={!props.loading && props.title ? props.title : undefined}
               loadingComponent={<Skeleton width={75} height={20} radius={4} />}
-              successComponent={(title) => (
-                <SizableText fontWeight="bold" lineHeight={0}>
-                  {title}
-                </SizableText>
-              )}
-            />
-          )}
-
-          {props.loading && props.showSkeletons?.subtitle && (
-            <Spacer size="$1" />
-          )}
-
-          {shouldRenderSubtitle && (
-            <StatusRenderer
-              data={
-                !props.loading && props.subtitle ? props.subtitle : undefined
+              successComponent={(title) =>
+                renderContent(title, { fontWeight: "bold", lineHeight: 0 })
               }
-              loadingComponent={<Skeleton width={100} height={18} radius={4} />}
-              successComponent={(subtitle) => (
-                <SizableText theme="alt1" size="$3" lineHeight={0}>
-                  {subtitle}
-                </SizableText>
-              )}
             />
           )}
 
-          {props.loading && props.showSkeletons?.subtitle2 && (
-            <Spacer size="$1" />
+          {shouldRender("subtitle") && (
+            <>
+              {props.loading && <Spacer size="$1" />}
+              <StatusRenderer
+                data={
+                  !props.loading && props.subtitle ? props.subtitle : undefined
+                }
+                loadingComponent={
+                  <Skeleton width={100} height={18} radius={4} />
+                }
+                successComponent={(subtitle) =>
+                  renderContent(subtitle, {
+                    theme: "alt1",
+                    size: "$3",
+                    lineHeight: 0,
+                  })
+                }
+              />
+            </>
           )}
 
-          {shouldRenderSubtitle2 && (
-            <StatusRenderer
-              data={
-                !props.loading && props.subtitle2 ? props.subtitle2 : undefined
-              }
-              loadingComponent={<Skeleton width={100} height={16} radius={4} />}
-              successComponent={(subtitle2) => (
-                <SizableText theme="alt1" size="$2" lineHeight={0}>
-                  {subtitle2}
-                </SizableText>
-              )}
-            />
+          {shouldRender("subtitle2") && (
+            <>
+              {props.loading && <Spacer size="$1" />}
+              <StatusRenderer
+                data={
+                  !props.loading && props.subtitle2
+                    ? props.subtitle2
+                    : undefined
+                }
+                loadingComponent={
+                  <Skeleton width={100} height={16} radius={4} />
+                }
+                successComponent={(subtitle2) =>
+                  renderContent(subtitle2, {
+                    theme: "alt1",
+                    size: "$2",
+                    lineHeight: 0,
+                  })
+                }
+              />
+            </>
           )}
         </YStack>
 
         <XStack gap="$2">
-          {shouldRenderButton && (
+          {shouldRender("button") && (
             <StatusRenderer
               data={!props.loading && props.button ? props.button : undefined}
               loadingComponent={
@@ -175,25 +173,11 @@ const VirtualizedListItem = (props: VirtualizedListItemProps) => {
                   radius={10}
                 />
               }
-              successComponent={(button) =>
-                isButtonProps(button) ? (
-                  <Button
-                    {...defaultButtonProps}
-                    {...button}
-                    onPress={
-                      button.onPress ? debounce(button.onPress, 300) : undefined
-                    }
-                  >
-                    {button.text}
-                  </Button>
-                ) : (
-                  button
-                )
-              }
+              successComponent={renderButton}
             />
           )}
 
-          {shouldRenderButton2 && (
+          {shouldRender("button2") && (
             <StatusRenderer
               data={!props.loading && props.button2 ? props.button2 : undefined}
               loadingComponent={
@@ -203,23 +187,7 @@ const VirtualizedListItem = (props: VirtualizedListItemProps) => {
                   radius={10}
                 />
               }
-              successComponent={(button2) =>
-                isButtonProps(button2) ? (
-                  <Button
-                    {...defaultButtonProps}
-                    {...button2}
-                    onPress={
-                      button2.onPress
-                        ? debounce(button2.onPress, 300)
-                        : undefined
-                    }
-                  >
-                    {button2.text}
-                  </Button>
-                ) : (
-                  button2
-                )
-              }
+              successComponent={renderButton}
             />
           )}
         </XStack>
@@ -236,4 +204,4 @@ const VirtualizedListItem = (props: VirtualizedListItemProps) => {
   );
 };
 
-export default VirtualizedListItem;
+export default React.memo(VirtualizedListItem);
