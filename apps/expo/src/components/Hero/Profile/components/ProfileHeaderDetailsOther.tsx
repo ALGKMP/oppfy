2f21ee3ffc41496255dccc47ff97a52e08b84cae
Antony <antonylbrussolo@gmail.com>
2024-08-02 02:48:40 -0400

    bigger Name

--- apps/expo/src/components/Hero/Profile/components/ProfileHeaderDetailsOther.tsx
+++ apps/expo/src/components/Hero/Profile/components/ProfileHeaderDetailsOther.tsx
@@ -637,7 +637,7 @@ const ProfileHeaderDetailsOther = (props: ProfileProps) => {
             loadingComponent={<Skeleton width={80} height={20} />}
             successComponent={(name) => (
               <SizableText
-                size="$5"
+                size="$8"
                 fontWeight="bold"
                 textAlign="left"
                 lineHeight={0}
