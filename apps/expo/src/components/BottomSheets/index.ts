24a305d3d5a9341f3d4e37cfed1edcefe83f7960
tonyy.lb <antonylbrussolo@gmail.com>
2024-08-11 14:01:57 -0400

    fix: exporitng components that don't exist

--- apps/expo/src/components/BottomSheets/index.ts
+++ apps/expo/src/components/BottomSheets/index.ts
@@ -1,4 +1,2 @@
 export { default as CommentsBottomSheet } from "./CommentsBottomSheet";
 export { default as PostActionsBottomSheet } from "./PostActionsBottomSheet";
-export { default as CommentsTestBottomSheet } from "./CommentsTestBottomSheet";
-export { default as CommentsTestBottomSheet2 } from "./CommentsTestBottomSheet2";
