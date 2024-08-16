31add2d2794dee3d0738e439580ed574a8568a72
Einsight <ghaithkhalil2004@gmail.com>
2024-08-16 16:27:25 -0400

    init

---
+++ cloud/aws/src/lib/constructs/queue.ts
@@ -0,0 +1,25 @@
+import * as cdk from 'aws-cdk-lib';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
+
+export class Queue extends Construct {
+  public readonly queue: sqs.Queue;
+  public readonly deadLetterQueue: sqs.Queue;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.deadLetterQueue = new sqs.Queue(this, 'DeadLetterQueue', {
+      retentionPeriod: cdk.Duration.days(1),
+    });
+
+    this.queue = new sqs.Queue(this, 'Queue', {
+      queueName: `${id}Queue`,
+      retentionPeriod: cdk.Duration.hours(5),
+      deadLetterQueue: {
+        maxReceiveCount: 1,
+        queue: this.deadLetterQueue,
+      },
+    });
+  }
+}
