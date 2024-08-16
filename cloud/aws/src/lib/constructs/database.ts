f5408ab45088ce1aa6d3d455a04be4d42cca3cae
Einsight <ghaithkhalil2004@gmail.com>
2024-08-16 16:24:45 -0400

    init

---
+++ cloud/aws/src/lib/constructs/database.ts
@@ -0,0 +1,55 @@
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
+import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
+import { Construct } from "constructs";
+
+export class Database extends Construct {
+  public readonly instance: rds.DatabaseInstance;
+
+  constructor(scope: Construct, id: string, vpc: ec2.Vpc) {
+    super(scope, id);
+
+    const securityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
+      vpc,
+      allowAllOutbound: true,
+      description: "Security group for RDS PostgreSQL instance",
+    });
+
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(5432),
+      "Allow PostgreSQL access from any IPv4 address",
+    );
+
+    const credentials = new secretsmanager.Secret(this, "Credentials", {
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ username: "oppfy_db" }),
+        generateStringKey: "password",
+        passwordLength: 16,
+        excludeCharacters: '"@/\\;:%+`?[]{}()<>|~!#$^&*_=',
+      },
+    });
+
+    this.instance = new rds.DatabaseInstance(this, "Instance", {
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_14,
+      }),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.MICRO,
+      ),
+      vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      publiclyAccessible: true,
+      securityGroups: [securityGroup],
+      credentials: rds.Credentials.fromSecret(credentials),
+      databaseName: "database",
+      allocatedStorage: 20,
+      maxAllocatedStorage: 20,
+      backupRetention: cdk.Duration.days(1),
+      deleteAutomatedBackups: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+  }
+}
