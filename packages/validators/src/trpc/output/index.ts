137c6fdb6517bd614723612a4754c701ee272979
tonyy.lb <antonylbrussolo@gmail.com>
2024-08-19 18:02:49 -0400

    removed output validators

--- packages/validators/src/trpc/output/index.ts
+++ packages/validators/src/trpc/output/index.ts
@@ -4,7 +4,6 @@ import trpcBlockOutputSchema from "./network/block";
 import trpcFriendOutputSchema from "./network/friend";
 import trpcRequestOutputSchema from "./network/request";
 import trpcPostOutputSchema from "./post/post";
-import trpcReccomendationsOutputSchema from "./network/recommendations";
 
 export const output = {
   profile: trpcProfileOutputSchema,
@@ -13,5 +12,4 @@ export const output = {
   post: trpcPostOutputSchema,
   block: trpcBlockOutputSchema,
   request: trpcRequestOutputSchema,
-  recommendations: trpcReccomendationsOutputSchema,
 };
