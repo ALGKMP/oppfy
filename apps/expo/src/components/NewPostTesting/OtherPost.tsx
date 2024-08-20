01120deedb3bc46cb80a6b6d0969331eee0e84cb
Einsight <ghaithkhalil2004@gmail.com>
2024-08-19 20:05:01 -0400

    init

---
+++ apps/expo/src/components/NewPostTesting/OtherPost.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OtherPost = () => {
+  return (
+    <View>
+      <Text>OtherPost</Text>
+    </View>
+  )
+}
+
+export default OtherPost
---
+++ apps/expo/src/components/NewPostTesting/SelfPost.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SelfPost = () => {
+  return (
+    <View>
+      <Text>SelfPost</Text>
+    </View>
+  )
+}
+
+export default SelfPost
