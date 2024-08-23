cbf9710db147ef799b7a7ce58eb6c6fd8ad58e7f
Einsight <ghaithkhalil2004@gmail.com>
2024-08-23 16:10:33 -0400

    border

--- apps/expo/src/components/Avatar.tsx
+++ apps/expo/src/components/Avatar.tsx
@@ -1,21 +1,28 @@
 import type { ImageSourcePropType } from "react-native";
 import { Image } from "expo-image";
 import defaultProfilePicture from "@assets/default-profile-picture.jpg";
+import { useTheme } from "tamagui";
 
 interface AvatarProps {
   source: ImageSourcePropType | string | null;
   size?: number;
 }
 
-const Avatar = (props: AvatarProps) => (
-  <Image
-    source={props.source ?? defaultProfilePicture}
-    style={{
-      width: props.size ?? 46,
-      height: props.size ?? 46,
-      borderRadius: props.size ? props.size / 2 : 23,
-    }}
-  />
-);
+const Avatar = (props: AvatarProps) => {
+  const theme = useTheme();
+
+  return (
+    <Image
+      source={props.source ?? defaultProfilePicture}
+      style={{
+        width: props.size ?? 46,
+        height: props.size ?? 46,
+        borderRadius: props.size ? props.size / 2 : 23,
+        borderWidth: 2,
+        borderColor: theme.primary.val as string,
+      }}
+    />
+  );
+};
 
 export default Avatar;
