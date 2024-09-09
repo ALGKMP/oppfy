7d58664ad95d0a206b66f3468b7cb31880b7af03
tonyy.lb <antonylbrussolo@gmail.com>
2024-09-09 13:46:52 -0400

    plugin

---
+++ apps/expo/snapchat-plugin.js
@@ -0,0 +1,24 @@
+module.exports = function withSnapchat(
+    config,
+    { snapchatClientId},
+  ) {
+    if (!config.ios) {
+      config.ios = {};
+    }
+    if (!config.ios.infoPlist) {
+      config.ios.infoPlist = {};
+    }
+  
+    config.ios.infoPlist["LSApplicationQueriesSchemes"] = [
+      "snapchat",
+      "bitmoji-sdk",
+      "itms-apps",
+    ];
+    config.ios.infoPlist["SCSDKClientId"] = snapchatClientId;
+    config.ios.infoPlist["SCSDKRedirectUrl"] = "oppfyapp://homepage/someshit";
+    config.ios.infoPlist["URL Types / Document Role"] = "Editor";
+    config.ios.infoPlist["URL Types / URL Identifier"] = "app.oppfy";
+    config.ios.infoPlist["URL Types / URL Schemes "] =["oppfyapp"];
+  
+    return config;
+  };
