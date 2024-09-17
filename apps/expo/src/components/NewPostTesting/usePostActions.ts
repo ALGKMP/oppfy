b162d1717f56f0d900e252f4efda704767c71f8c
Einsight <ghaithkhalil2004@gmail.com>
2024-09-17 17:39:03 -0400

    updates

--- apps/expo/src/components/NewPostTesting/usePostActions.ts
+++ apps/expo/src/components/NewPostTesting/usePostActions.ts
@@ -1,9 +1,10 @@
-import type { PostData as OtherPostProps } from "./ui/PostCard";
-import { useRouter } from "expo-router";
-import { useSaveMedia } from "./useSaveMedia";
-import { useToastController } from "@tamagui/toast";
 import { Position } from "react-native-image-marker";
+import { useRouter } from "expo-router";
 import watermark from "@assets/watermark.png";
+import { useToastController } from "@tamagui/toast";
+
+import type { PostData as OtherPostProps } from "./ui/PostCard";
+import { useSaveMedia } from "./useSaveMedia";
 
 export const usePostActions = (postProps: OtherPostProps) => {
   const router = useRouter();
@@ -11,12 +12,20 @@ export const usePostActions = (postProps: OtherPostProps) => {
   const { saveMedia, isSaving } = useSaveMedia();
 
   const handleSavePost = async () => {
-    await saveMedia(postProps.media.url, {
-      image: watermark,
-      position: Position.bottomRight,
-      scale: 0.7,
-    });
-    toast.show("Post Saved");
+    try {
+      await saveMedia(postProps.media.url, {
+        image: watermark,
+        position: Position.bottomRight,
+        scale: 0.7,
+      });
+      toast.show("Post Saved");
+    } catch (error) {
+      toast.show("Enable media library permission in settings", {
+        burntOptions: {
+          preset: "error",
+        },
+      });
+    }
   };
 
   const handleShare = () => {
