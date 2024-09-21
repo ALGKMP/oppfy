167f6cc3128bc515bb3f156c52a0bb9322583fc8
Einsight <ghaithkhalil2004@gmail.com>
2024-09-21 13:31:41 -0400

    updates

--- apps/expo/src/app/(app)/(bottom-tabs)/(home)/index.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(home)/index.tsx
@@ -138,8 +138,10 @@ const HomeScreen = () => {
                 profilePicture: item.recipientProfilePicture,
               }}
               media={{
+                isViewable: viewableItems.includes(item.postId),
                 type: item.mediaType,
                 url: item.imageUrl,
+
                 dimensions: {
                   width: item.width,
                   height: item.height,
