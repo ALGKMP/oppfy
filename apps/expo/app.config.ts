26e115fa20bb084efe6d9546d2c04189d99acdec
tonyy.lb <antonylbrussolo@gmail.com>
2024-09-26 21:32:17 -0400

    version + ipa

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "oppfy",
   slug: "oppfy",
   scheme: "oppfy",
-  version: "1.0.0",
+  version: "1.0.1",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "dark",
