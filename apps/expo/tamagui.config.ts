2849165bca3d9555218151281a614d3d0d37d436
Einsight <ghaithkhalil2004@gmail.com>
2024-09-29 18:50:37 -0400

    revert

--- apps/expo/tamagui.config.ts
+++ apps/expo/tamagui.config.ts
@@ -26,77 +26,6 @@ const modakFont = createFont({
   },
 });
 
-const poppinsFace = {
-  normal: { normal: "Poppins-Regular", italic: "Poppins-Italic" },
-  500: { normal: "Poppins-Medium", italic: "Poppins-MediumItalic" },
-  600: { normal: "Poppins-SemiBold", italic: "Poppins-SemiBoldItalic" },
-  700: { normal: "Poppins-Bold", italic: "Poppins-BoldItalic" },
-  800: { normal: "Poppins-ExtraBold", italic: "Poppins-ExtraBoldItalic" },
-  900: { normal: "Poppins-Black", italic: "Poppins-BlackItalic" },
-};
-
-const bodyFont = createFont({
-  family: "Poppins",
-  size: config.fonts.body.size,
-  lineHeight: config.fonts.body.lineHeight,
-  weight: config.fonts.body.weight,
-  letterSpacing: config.fonts.body.letterSpacing,
-  face: poppinsFace,
-});
-
-const headingFont = createFont({
-  family: "Poppins",
-  size: {
-    1: 12,
-    2: 14,
-    3: 16,
-    4: 18,
-    5: 22,
-    6: 28,
-    7: 36,
-    8: 48,
-    9: 64,
-    10: 72,
-  },
-  lineHeight: {
-    1: 16,
-    2: 20,
-    3: 24,
-    4: 28,
-    5: 32,
-    6: 40,
-    7: 48,
-    8: 60,
-    9: 76,
-    10: 84,
-  },
-  weight: {
-    1: "400",
-    2: "400",
-    3: "500",
-    4: "500",
-    5: "600",
-    6: "600",
-    7: "700",
-    8: "700",
-    9: "800",
-    10: "900",
-  },
-  letterSpacing: {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: -0.5,
-    5: -0.5,
-    6: -1,
-    7: -1,
-    8: -1.5,
-    9: -1.5,
-    10: -2,
-  },
-  face: poppinsFace,
-});
-
 const tamaguiConfig = createTamagui({
   ...config,
   tokens: {
@@ -108,8 +37,6 @@ const tamaguiConfig = createTamagui({
   },
   fonts: {
     ...config.fonts,
-    body: bodyFont,
-    heading: headingFont,
     modak: modakFont,
   },
 });
