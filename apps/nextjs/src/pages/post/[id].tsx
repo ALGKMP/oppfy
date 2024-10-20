58918646e3ce84451e77f2f132591761bcdfb511
tonyy.lb <antonylbrussolo@gmail.com>
2024-10-20 17:34:05 -0400

    trying this out

--- apps/nextjs/src/pages/post/[id].tsx
+++ apps/nextjs/src/pages/post/[id].tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head';
 import { api } from '~/utils/api';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 
 interface OpenGraphProps {
   title: string;
@@ -10,6 +8,31 @@ interface OpenGraphProps {
   url: string;
   type?: string;
 }
+
+import { useRouter } from 'next/router';
+
+const PostPage: React.FC = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  console.log(id);
+  const { data: post } = api.post.getPostForNextJs.useQuery({ postId: id as string });
+
+  // Fetch post data here
+
+  return (
+    <>
+      <OpenGraph
+        title={`${post?.authorUsername} opped ${post?.recipientUsername}`}
+        description={post?.caption ?? ""}
+        image={`https://d23ceri9cmy0px.cloudfront.net/posts/${id}`} // TODO: use env
+        // image={post?.imageUrl ?? ""}
+        url={`https://opp.oppfy.app/post/${id}`} // TODO: use env
+        type="article"
+      />
+      {/* Rest of your component */}
+    </>
+  );
+};
 
 const OpenGraph: React.FC<OpenGraphProps> = ({ title, description, image, url, type = 'website' }) => {
   return (
@@ -24,46 +47,5 @@ const OpenGraph: React.FC<OpenGraphProps> = ({ title, description, image, url, t
   );
 };
 
-const PostPage: React.FC = () => {
-  const router = useRouter();
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    if (router.isReady) {
-      setIsReady(true);
-    }
-  }, [router.isReady]);
-
-  const { id } = router.query;
-  const { data: post, isLoading, isError } = api.post.getPostForNextJs.useQuery(
-    { postId: id as string },
-    { enabled: isReady && !!id }
-  );
-
-  if (!isReady || isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError || !post) {
-    return <div>Error loading post</div>;
-  }
-
-  const baseUrl = 'https://opp.oppfy.app';
-
-  return (
-    <>
-      <OpenGraph
-        title={`${post.authorUsername} opped ${post.recipientUsername}`}
-        description={post.caption || "No caption provided"}
-        image={post.imageUrl || `${baseUrl}/default-image.jpg`}
-        url={`${baseUrl}/post/${id}`}
-        type="article"
-      />
-      <h1>{post.authorUsername} opped {post.recipientUsername}</h1>
-      <p>{post.caption || "No caption provided"}</p>
-      {post.imageUrl && <img src={post.imageUrl} alt="Post image" />}
-    </>
-  );
-};
-
 export default PostPage;
+
