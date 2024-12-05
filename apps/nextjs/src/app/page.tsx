f53ab8e1326e3f35cc8eacec49b694a90c897a91
tonyy.lb <antonylbrussolo@gmail.com>
2024-12-05 17:04:34 -0500

    page

---
+++ apps/nextjs/src/app/page.tsx
@@ -0,0 +1,17 @@
+
+export default async function HomePage() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            Welcome to <span className="text-[#F214FF]">Oppfy</span>
+          </h1>
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-2xl text-white">
+                hello
+            </p>
+          </div>
+        </div>
+      </main>
+    );
+}
