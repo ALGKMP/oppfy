b7983852a133d9afbe54485b960ffe197bf05e74
tonyy.lb <antonylbrussolo@gmail.com>
2024-12-07 10:21:17 -0500

    cleanup

--- apps/nextjs/src/app/api/trpc/[trpc]/route.ts
+++ apps/nextjs/src/app/api/trpc/[trpc]/route.ts
@@ -1,11 +1,9 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import cors from "nextjs-cors";
-
 import { appRouter, createTRPCContext } from "@oppfy/api";
 
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
-// export const runtime = "edge";
+// The edge runtime is isn't compatible with something
+// export const runtime = "edge"; 
 
 /**
  * Configure basic CORS headers
@@ -30,7 +28,7 @@ export const OPTIONS = () => {
 
 const handler = async (req: Request, res: Response) => {
   console.log("TRPC Route Hit!"); // Add this to verify route is being accessed
-  const response = await fetchRequestHandler({ // TODO: This is not working
+  const response = await fetchRequestHandler({
     endpoint: "/api/trpc",
     router: appRouter,
     req,
@@ -45,49 +43,3 @@ const handler = async (req: Request, res: Response) => {
 };
 
 export { handler as GET, handler as POST };
-// export default handler;
-
-// const setCorsHeaders = (res: Response) => {
-//   res.headers.set("Access-Control-Allow-Origin", "*");
-//   res.headers.set("Access-Control-Request-Method", "*");
-//   res.headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
-//   res.headers.set("Access-Control-Allow-Headers", "*");
-
-//   // Add these additional headers
-//   res.headers.set("Access-Control-Allow-Credentials", "true");
-//   res.headers.set("Access-Control-Expose-Headers", "*");
-// };
-
-// export const OPTIONS = () => {
-//   const response = new Response(null, {
-//     status: 204,
-//   });
-//   setCorsHeaders(response);
-//   return response;
-// };
-
-// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-//   console.log("TRPC Route Hit!"); // Add this to verify route is being accessed
-//   // Setup CORS
-//   // await cors(res, req);
-
-//   // Handle incoming OpenAPI requests
-//   return fetchRequestHandler({
-//     endpoint: "/api/trpc",
-//     router: appRouter,
-//     req,
-//     createContext: createTRPCContext,
-//     onError() {
-//       console.error("Error in TRPC handler");
-//     },
-//     responseMeta() {
-//       return {
-//         headers: {
-//           "x-powered-by": "trpc",
-//         },
-//       };
-//     },
-//   })(req, res);
-// };
-
-// export { handler as GET, handler as POST };
