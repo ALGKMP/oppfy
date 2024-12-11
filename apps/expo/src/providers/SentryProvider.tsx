e6284be98780b6a734d0037eb3f9a440fc259f48
tonyy.lb <antonylbrussolo@gmail.com>
2024-12-11 15:21:31 -0500

    Sentry Provider

--- apps/expo/src/providers/SentryProvider.tsx
+++ apps/expo/src/providers/SentryProvider.tsx
@@ -1,21 +1,17 @@
 import type { ReactNode } from "react";
 import React, { useEffect } from "react";
-import { isRunningInExpoGo } from "expo";
 import { useNavigationContainerRef } from "expo-router";
 import * as Sentry from "@sentry/react-native";
 
-const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
+const navigationIntegration = Sentry.reactNavigationIntegration({
+  enableTimeToInitialDisplay: true,
+});
 
 Sentry.init({
   debug: false,
   enabled: !__DEV__,
   dsn: "https://55ede8542c3606c6e90656eec2d9c6c8@o4507697000611840.ingest.us.sentry.io/4507697356603392",
-  integrations: [
-    new Sentry.ReactNativeTracing({
-      routingInstrumentation,
-      enableNativeFramesTracking: !isRunningInExpoGo(),
-    }),
-  ],
+  integrations: [navigationIntegration],
 });
 
 interface SentryProviderProps {
@@ -25,10 +21,7 @@ interface SentryProviderProps {
 const SentryProvider = ({ children }: SentryProviderProps) => {
   const ref = useNavigationContainerRef();
 
-  useEffect(
-    () => routingInstrumentation.registerNavigationContainer(ref),
-    [ref],
-  );
+  useEffect(() => navigationIntegration.registerNavigationContainer(ref), [ref]);
 
   const SentryWrappedComponent = Sentry.wrap(() => <>{children}</>);
   return <SentryWrappedComponent />;
