42597d5bb1ee540fceba22fc62a4ae21b9f03363
einsight <ghaithkale@gmail.com>
2024-12-13 00:00:23 -0500

    screenview

--- apps/expo/src/components/ui/Views.tsx
+++ apps/expo/src/components/ui/Views.tsx
@@ -6,3 +6,8 @@ import {
 
 export const View = styled(TamaguiView, {});
 export const ScrollView = styled(TamaguiScrollView, {});
+
+export const ScreenView = styled(TamaguiView, {
+  flex: 1,
+  paddingHorizontal: "$2",
+});
