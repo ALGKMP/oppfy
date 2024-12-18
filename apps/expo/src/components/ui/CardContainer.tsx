010a6f00a0c17a3c0e1ffe1265c0e5a4bf8aef02
einsight <ghaithkale@gmail.com>
2024-12-18 00:32:55 -0500

    takes in title

--- apps/expo/src/components/ui/CardContainer.tsx
+++ apps/expo/src/components/ui/CardContainer.tsx
@@ -1,15 +1,23 @@
 import React from "react";
-import type { ViewProps } from "tamagui";
-import { View } from "tamagui";
+import type { YStackProps } from "tamagui";
 
-interface CardContainerProps extends ViewProps {
+import { H5 } from "./Headings";
+import { YStack } from "./Stacks";
+
+interface CardContainerProps extends YStackProps {
   children: React.ReactNode;
+  title?: string;
 }
 
-export const CardContainer = ({ children, ...props }: CardContainerProps) => {
+export const CardContainer = ({
+  children,
+  title,
+  ...props
+}: CardContainerProps) => {
   return (
-    <View padding="$3" borderRadius="$6" backgroundColor="$gray2" {...props}>
+    <YStack padding="$3" borderRadius="$6" backgroundColor="$gray2" {...props}>
+      {title && <H5 fontSize="$3">{title}</H5>}
       {children}
-    </View>
+    </YStack>
   );
 };
