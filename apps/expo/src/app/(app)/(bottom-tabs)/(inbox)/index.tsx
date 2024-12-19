d644f03cd07c0e97b7f3a8a460f464545189a57e
einsight <ghaithkale@gmail.com>
2024-12-19 02:41:31 -0500

    rewrite

--- apps/expo/src/app/(app)/(bottom-tabs)/(inbox)/index.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(inbox)/index.tsx
@@ -1,42 +1,39 @@
 import React, { useCallback, useMemo, useState } from "react";
-import { RefreshControl, StyleSheet, TouchableOpacity } from "react-native";
+import { RefreshControl } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import * as Haptics from "expo-haptics";
 import { useRouter } from "expo-router";
 import DefaultProfilePicture from "@assets/default-profile-picture.jpg";
 import { FlashList } from "@shopify/flash-list";
 import { UserRoundCheck, UserRoundPlus } from "@tamagui/lucide-icons";
 import {
-  Circle,
+  getToken,
   H5,
+  H6,
   Paragraph,
   SizableText,
   View,
-  XStack,
   YStack,
 } from "tamagui";
 
-import CardContainer from "~/components/Containers/CardContainer";
-import { VirtualizedListItem } from "~/components/ListItems";
-import { Skeleton } from "~/components/Skeletons";
-import RecommendationList from "~/components/SpecialLists/RecommendationList";
 import { TimeAgo } from "~/components/Texts";
-import { BaseScreenView } from "~/components/Views";
+import { MediaListItem, MediaListItemSkeleton } from "~/components/ui";
+import { Spacer } from "~/components/ui/Spacer";
+import { EmptyPlaceholder } from "~/components/UIPlaceholders";
 import useRouteProfile from "~/hooks/useRouteProfile";
-import type { RouterOutputs } from "~/utils/api";
-import { api } from "~/utils/api";
-import { PLACEHOLDER_DATA } from "~/utils/placeholder-data";
+import { api, RouterOutputs } from "~/utils/api";
 
 type NotificationItem =
   RouterOutputs["notifications"]["paginateNotifications"]["items"][0];
 
 const REFETCH_INTERVAL = 1000 * 30;
+const PAGE_SIZE = 20;
 
 const Inbox = () => {
   const router = useRouter();
-  const insets = useSafeAreaInsets();
-  const { routeProfile } = useRouteProfile();
   const utils = api.useUtils();
+  const insets = useSafeAreaInsets();
+
+  const { routeProfile } = useRouteProfile();
 
   const [refreshing, setRefreshing] = useState(false);
 
@@ -50,14 +47,6 @@ const Inbox = () => {
   });
 
   const {
-    data: recommendationsData,
-    isLoading: isRecommendationsLoading,
-    refetch: refetchRecommendations,
-  } = api.contacts.getRecommendationProfilesSelf.useQuery(undefined, {
-    refetchInterval: REFETCH_INTERVAL,
-  });
-
-  const {
     data: notificationsData,
     isLoading: isNotificationsLoading,
     isFetchingNextPage,
@@ -65,43 +54,29 @@ const Inbox = () => {
     hasNextPage,
     refetch: refetchNotifications,
   } = api.notifications.paginateNotifications.useInfiniteQuery(
-    { pageSize: 20 },
+    { pageSize: PAGE_SIZE },
     {
       getNextPageParam: (lastPage) => lastPage.nextCursor,
       refetchInterval: REFETCH_INTERVAL,
     },
   );
 
-  const totalRequestCount =
-    (requestsCount?.followRequestCount ?? 0) +
-    (requestsCount?.friendRequestCount ?? 0);
-
   const notificationItems = useMemo(
     () => notificationsData?.pages.flatMap((page) => page.items) ?? [],
     [notificationsData],
   );
 
-  const handleOnEndReached = () => {
-    if (!isFetchingNextPage && hasNextPage) {
-      void fetchNextPage();
-    }
-  };
-
   const followUser = api.follow.followUser.useMutation({
     onMutate: async (newData) => {
-      // Cancel outgoing fetches (so they don't overwrite our optimistic update)
       await utils.notifications.paginateNotifications.cancel();
-
-      // Get the data from the queryCache
       const prevData =
         utils.notifications.paginateNotifications.getInfiniteData({
-          pageSize: 20,
+          pageSize: PAGE_SIZE,
         });
       if (prevData === undefined) return;
 
-      // Optimistically update the data
       utils.notifications.paginateNotifications.setInfiniteData(
-        { pageSize: 20 },
+        { pageSize: PAGE_SIZE },
         {
           ...prevData,
           pages: prevData.pages.map((page) => ({
@@ -121,222 +96,153 @@ const Inbox = () => {
         },
       );
 
-      // Return the previous data so we can revert if something goes wrong
       return { prevData };
     },
     onError: (_err, _newData, ctx) => {
       if (ctx === undefined) return;
-
-      // If the mutation fails, use the context-value from onMutate
       utils.notifications.paginateNotifications.setInfiniteData(
-        { pageSize: 20 },
+        { pageSize: PAGE_SIZE },
         ctx.prevData,
       );
     },
     onSettled: async () => {
-      // Sync with server once mutation has settled
       await utils.notifications.paginateNotifications.invalidate();
     },
   });
 
-  const onUserSelected = ({ userId, username }: NotificationItem) => {
-    routeProfile({ userId, username });
-  };
+  const handleOnEndReached = useCallback(async () => {
+    if (!isFetchingNextPage && hasNextPage) {
+      await fetchNextPage();
+    }
+  }, [isFetchingNextPage, hasNextPage, fetchNextPage]);
 
-  const onFollowUser = async (userId: string) => {
-    await followUser.mutateAsync({ userId });
-  };
-
-  const onRefresh = useCallback(async () => {
+  const handleRefresh = useCallback(async () => {
     setRefreshing(true);
-    await Promise.all([
-      refetchNotifications(),
-      refetchRequestCount(),
-      refetchRecommendations(),
-    ]);
+    await Promise.all([refetchNotifications(), refetchRequestCount()]);
     setRefreshing(false);
-  }, [refetchNotifications, refetchRequestCount, refetchRecommendations]);
+  }, [refetchNotifications, refetchRequestCount]);
 
-  const renderRequestCount = () =>
-    totalRequestCount > 99 ? (
-      <XStack>
-        <SizableText size="$4" color="white" fontWeight="bold">
-          99
-        </SizableText>
-        <SizableText size="$2">+</SizableText>
-      </XStack>
-    ) : (
-      <SizableText size="$4" color="white" fontWeight="bold">
-        {totalRequestCount}
-      </SizableText>
-    );
+  const renderListItem = useCallback(
+    (item: NotificationItem) => {
+      const buttonProps = (() => {
+        switch (item.relationshipState) {
+          case "notFollowing":
+            return {
+              label: "Follow",
+              icon: UserRoundPlus,
+              onPress: () =>
+                void followUser.mutateAsync({ userId: item.userId }),
+            };
+          case "following":
+            return {
+              label: "Followed",
+              icon: UserRoundCheck,
+              disabled: true,
+            };
+          case "followRequestSent":
+            return {
+              label: "Sent",
+              icon: UserRoundCheck,
+              disabled: true,
+            };
+        }
+      })();
 
-  const renderLoadingSkeletons = () => (
-    <YStack gap="$4">
-      <CardContainer padding="$4">
-        <YStack gap="$1">
-          <Skeleton width={150} height={30} />
-          <Skeleton width="100%" height={20} />
-        </YStack>
-      </CardContainer>
-
-      <CardContainer>
-        {PLACEHOLDER_DATA.map((_, index) => (
-          <VirtualizedListItem
-            key={index}
-            loading
-            showSkeletons={{
-              imageUrl: true,
-              title: true,
-              subtitle: true,
-              subtitle2: true,
-              button: true,
-            }}
-          />
-        ))}
-      </CardContainer>
-    </YStack>
+      return (
+        <MediaListItem
+          title={item.username}
+          subtitle={item.message}
+          caption={
+            <TimeAgo
+              size="$2"
+              date={item.createdAt}
+              format={({ value, unit }) => `${value}${unit.charAt(0)} ago`}
+            />
+          }
+          imageUrl={item.profilePictureUrl ?? DefaultProfilePicture}
+          primaryAction={buttonProps}
+          onPress={() =>
+            routeProfile({ userId: item.userId, username: item.username })
+          }
+        />
+      );
+    },
+    [followUser, routeProfile],
   );
 
-  const renderFollowRequests = () =>
-    totalRequestCount > 0 && (
-      <TouchableOpacity onPress={() => router.navigate("/requests")}>
-        <CardContainer padding="$4">
-          <YStack>
-            <SizableText size="$6" fontWeight="bold">
-              Follow and Friend Requests
-            </SizableText>
-            <Paragraph theme="alt1">Approve or ignore these requests</Paragraph>
-          </YStack>
-          <Circle
-            size={totalRequestCount > 99 ? "$2.5" : "$2"}
-            backgroundColor="$red9"
-            style={styles.countContainer}
-          >
-            {renderRequestCount()}
-          </Circle>
-        </CardContainer>
-      </TouchableOpacity>
-    );
+  const ListHeaderComponent = useMemo(
+    () => (
+      <YStack gap="$4">
+        {requestsCount &&
+          (requestsCount.followRequestCount > 0 ||
+            requestsCount.friendRequestCount > 0) && (
+            <YStack
+              backgroundColor="$background"
+              padding="$4"
+              borderRadius="$4"
+              pressStyle={{ opacity: 0.7 }}
+              onPress={() => router.navigate("/requests")}
+            >
+              <H5>Follow and Friend Requests</H5>
+              <Paragraph theme="alt1">
+                {requestsCount.followRequestCount +
+                  requestsCount.friendRequestCount}{" "}
+                pending requests
+              </Paragraph>
+            </YStack>
+          )}
+      </YStack>
+    ),
+    [requestsCount, router],
+  );
 
-  const renderNotifications = () =>
-    notificationItems.length > 0 && (
-      <CardContainer>
-        <H5 theme="alt1">Notifications</H5>
+  const ListEmptyComponent = useCallback(() => {
+    if (isNotificationsLoading) {
+      return (
+        <YStack gap="$4">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <MediaListItemSkeleton key={index} />
+          ))}
+        </YStack>
+      );
+    }
 
-        <FlashList
-          data={notificationItems}
-          estimatedItemSize={75}
-          onEndReached={handleOnEndReached}
-          showsVerticalScrollIndicator={false}
-          renderItem={({ item }) => {
-            const buttonProps = (() => {
-              switch (item.relationshipState) {
-                case "notFollowing":
-                  return {
-                    text: "Follow",
-                    icon: <UserRoundPlus size="$1" />,
-                    backgroundColor: "#F214FF",
-                  };
-                case "following":
-                  return {
-                    text: "Followed",
-                    disabled: true,
-                    disabledStyle: {
-                      opacity: 0.5,
-                    },
-                    icon: <UserRoundCheck size="$1" />,
-                  };
-                case "followRequestSent":
-                  return {
-                    text: "Sent",
-                    disabled: true,
-                    disabledStyle: {
-                      opacity: 0.5,
-                    },
-                    icon: <UserRoundCheck size="$1" />,
-                  };
-              }
-            })();
+    if (notificationItems.length === 0) {
+      return (
+        <YStack flex={1} justifyContent="center">
+          <EmptyPlaceholder
+            title="No notifications"
+            subtitle="You don't have any notifications yet."
+          />
+        </YStack>
+      );
+    }
 
-            const buttonDisabled =
-              item.relationshipState === "following" ||
-              item.relationshipState === "followRequestSent";
-
-            return (
-              <VirtualizedListItem
-                loading={false}
-                title={item.username}
-                subtitle={item.message}
-                subtitle2={
-                  <TimeAgo
-                    size="$2"
-                    date={item.createdAt}
-                    format={({ value, unit }) =>
-                      `${value}${unit.charAt(0)} ago`
-                    }
-                  />
-                }
-                button={{
-                  ...buttonProps,
-                  disabled: buttonDisabled,
-                  disabledStyle: { opacity: 0.5 },
-                  onPress: () => void onFollowUser(item.userId),
-                }}
-                imageUrl={item.profilePictureUrl ?? DefaultProfilePicture}
-                onPress={() => onUserSelected(item)}
-              />
-            );
-          }}
-        />
-      </CardContainer>
-    );
-
-  if (isCountRequestsLoading || isNotificationsLoading) {
-    return (
-      <BaseScreenView scrollable>
-        <View paddingBottom={insets.bottom}>{renderLoadingSkeletons()}</View>
-      </BaseScreenView>
-    );
-  }
+    return null;
+  }, [isNotificationsLoading, notificationItems.length]);
 
   return (
-    <BaseScreenView
-      scrollable
-      refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-      }
-    >
-      <YStack flex={1} gap="$4" paddingBottom={insets.bottom}>
-        {renderFollowRequests()}
-        {renderNotifications()}
-        {recommendationsData && (
-          <RecommendationList
-            handleProfileClicked={(userId, username) => {
-              void Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
-              router.navigate({
-                // pathname: "/(inbox)/profile/[userId]",
-                pathname: "/(app)/(bottom-tabs)/( inbox)/profile/[userId]", // TODO: Typescript keeps yelling about this
-                params: { userId, username },
-              });
-            }}
-            loading={isRecommendationsLoading}
-            recommendationsData={recommendationsData}
-          />
-        )}
-      </YStack>
-    </BaseScreenView>
+    <View flex={1}>
+      <FlashList
+        data={notificationItems}
+        renderItem={({ item }) => renderListItem(item)}
+        estimatedItemSize={75}
+        ListHeaderComponent={ListHeaderComponent}
+        ListEmptyComponent={ListEmptyComponent}
+        ItemSeparatorComponent={Spacer}
+        contentContainerStyle={{
+          padding: getToken("$4", "space"),
+        }}
+        showsVerticalScrollIndicator={false}
+        onEndReached={handleOnEndReached}
+        onEndReachedThreshold={0.5}
+        keyboardShouldPersistTaps="always"
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
+        }
+      />
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  countContainer: {
-    position: "absolute",
-    top: -8,
-    right: -8,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default Inbox;
