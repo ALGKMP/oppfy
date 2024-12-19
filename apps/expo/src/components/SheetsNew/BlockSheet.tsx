b4cd7f4c4a6c202eeae9230c9c19590c3cdacfe4
tonyy.lb <antonylbrussolo@gmail.com>
2024-12-19 18:38:44 -0500

    useBlockUserHook

--- apps/expo/src/components/SheetsNew/BlockSheet.tsx
+++ apps/expo/src/components/SheetsNew/BlockSheet.tsx
@@ -2,22 +2,99 @@ import { ActionSheet, ButtonOption } from "~/components/ui/ActionSheet";
 import { api } from "~/utils/api";
 
 const BlockSheet = ({ userId }: { userId: string }) => {
+  const { isBlocking, isUnblocking, handleBlockUser, handleUnblockUser } =
+    useBlockUser(userId);
+
+  const { data: networkRelationships } =
+    api.profile.getNetworkRelationships.useQuery({ userId });
 
   return (
     <ActionSheet
-      isVisible={true}
       title="Block"
-      buttonOptions={[
-        {
-          text: "Block",
-          onPress: () => {
-            // blockUser.mutate({ userId });
-          },
-          textProps: { color: "$red9" },
-        },
-      ]}
+      isVisible={true}
+    //   buttonOptions={sheetButtonOptions}
     />
   );
+};
+
+/*
+ * ==========================================
+ * ============== hooks =====================
+ * ==========================================
+ */
+
+const useBlockUser = (userId: string) => {
+  const utils = api.useUtils();
+
+  const { isPending: isUnblocking, mutateAsync: unblockUser } =
+    api.block.unblockUser.useMutation({
+      onMutate: async () => {
+        await utils.profile.getNetworkRelationships.cancel({ userId });
+        const prevData = utils.profile.getNetworkRelationships.getData({
+          userId,
+        });
+        if (prevData === undefined) return;
+        utils.profile.getNetworkRelationships.setData(
+          { userId },
+          {
+            ...prevData,
+            blocked: false,
+          },
+        );
+        return { prevData };
+      },
+      onError: (error, _newUnblockedUser, context) => {
+        console.error(error);
+        if (context === undefined) return;
+        utils.profile.getNetworkRelationships.setData(
+          { userId },
+          context.prevData,
+        );
+      },
+      onSettled: async () => {
+        await utils.profile.getNetworkRelationships.invalidate({ userId });
+      },
+    });
+
+  const { isPending: isBlocking, mutateAsync: blockUser } =
+    api.block.blockUser.useMutation({
+      onMutate: async () => {
+        await utils.profile.getNetworkRelationships.cancel({ userId });
+        const prevData = utils.profile.getNetworkRelationships.getData({
+          userId,
+        });
+        if (prevData === undefined) return;
+        utils.profile.getNetworkRelationships.setData(
+          { userId },
+          {
+            ...prevData,
+            blocked: true,
+          },
+        );
+        return { prevData };
+      },
+      onError: (error, _newBlockedUser, context) => {
+        console.error(error);
+        if (context === undefined) return;
+        utils.profile.getNetworkRelationships.setData(
+          { userId },
+          context.prevData,
+        );
+      },
+      onSettled: async () => {
+        await utils.profile.getNetworkRelationships.invalidate({ userId });
+      },
+    });
+
+  const handleBlockUser = async () => {
+    await blockUser({ userId });
+  };
+
+  const handleUnblockUser = async () => {
+    await unblockUser({ userId });
+  };
+
+  return { isBlocking, isUnblocking, handleBlockUser, handleUnblockUser };
 };
 
 export default BlockSheet;
