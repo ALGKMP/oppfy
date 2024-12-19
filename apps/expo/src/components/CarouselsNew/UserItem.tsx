72cc8647a4d6630026127d4839db7d21512e870e
tonyy.lb <antonylbrussolo@gmail.com>
2024-12-19 10:08:15 -0500

    UserItem (profile pic + name & interactions)

---
+++ apps/expo/src/components/CarouselsNew/UserItem.tsx
@@ -0,0 +1,38 @@
+import { TouchableOpacity } from "react-native";
+
+import { Text, XStack, YStack } from "~/components/ui";
+import useRouteProfile from "~/hooks/useRouteProfile";
+import Avatar from "../Avatar";
+
+interface UserItem {
+  userId: string;
+  username: string;
+  profilePictureUrl: string | null;
+}
+
+const UserItem = ({ item }: { item: UserItem }) => {
+  const { routeProfile } = useRouteProfile();
+
+  return (
+    <TouchableOpacity onPress={() => routeProfile({ userId: item.userId, username: item.username })}>
+      <YStack width={70} gap="$2" alignItems="center">
+        <Avatar
+          source={item.profilePictureUrl}
+          size={70}
+          recyclingKey={item.userId}
+        />
+        <Text
+          fontSize="$2"
+          fontWeight="600"
+          textAlign="center"
+          numberOfLines={1}
+          ellipsizeMode="tail"
+        >
+          {item.username}
+        </Text>
+      </YStack>
+    </TouchableOpacity>
+  );
+};
+
+export default UserItem;
