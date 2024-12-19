89e96fb3da24ab74ae5a974ac4056c2d4baa5ddc
tonyy.lb <antonylbrussolo@gmail.com>
2024-12-19 10:42:21 -0500

    FriendsCarousel

---
+++ apps/expo/src/components/CarouselsNew/FriendCarousel.tsx
@@ -0,0 +1,187 @@
+import React, { useCallback, useEffect, useRef } from "react";
+import { TouchableOpacity } from "react-native";
+import type { NativeScrollEvent, NativeSyntheticEvent } from "react-native";
+import * as Haptics from "expo-haptics";
+import { FlashList } from "@shopify/flash-list";
+import { throttle } from "lodash";
+import { getToken, H5, Spacer, Text, View, XStack, YStack } from "tamagui";
+import { api } from "~/utils/api";
+
+import CardContainer from "~/components/Containers/CardContainer";
+import { Skeleton } from "~/components/Skeletons";
+import { PLACEHOLDER_DATA } from "~/utils/placeholder-data";
+import UserItem from "./UserItem";
+
+/*
+* TODO: Can make this a compound component later if we want to add more styles
+* or if we want to add more functionality to the carousel.
+* For example, the Instagram carousel, and how some have big cards and some have small cards.
+*/
+
+interface PersonItem {
+  userId: string;
+  username: string;
+  profilePictureUrl: string | null;
+}
+
+interface LoadedProps<T extends PersonItem> {
+  userId: string;
+  data: T[];
+  title?: string;
+  emoji?: string;
+  showMore?: boolean;
+  onTitlePress?: () => void;
+  onItemPress: (item: T) => void;
+  onShowMore?: () => void;
+  renderExtraItem?: () => React.ReactElement;
+}
+
+type PeopleCarouselProps<T extends PersonItem> = LoadedProps<T>;
+
+function PeopleCarousel<T extends PersonItem>(props: PeopleCarouselProps<T>) {
+
+  const {
+    data: friendsData,
+    isLoading: isLoadingFriendsData,
+    refetch: refetchFriendsData,
+  } = api.friend.paginateFriendsOthers.useInfiniteQuery(
+    { userId: props.userId, pageSize: 10 },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+      enabled: !!props.userId,
+      refetchOnMount: true,
+    },
+  );
+
+
+  const throttledHandleShowMore = useRef(
+    throttle(
+      () => {
+        if ("onShowMore" in props && props.onShowMore) {
+          void Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+          props.onShowMore();
+        }
+      },
+      300,
+      { leading: true, trailing: false },
+    ),
+  ).current;
+
+  useEffect(() => {
+    return () => {
+      throttledHandleShowMore.cancel();
+    };
+  }, [throttledHandleShowMore]);
+
+  const handleScroll = useCallback(
+    (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+      if (isLoadingFriendsData) return;
+      if (!props.showMore) return;
+
+      const { contentSize, contentOffset, layoutMeasurement } =
+        event.nativeEvent;
+      const contentWidth = contentSize.width;
+      const offsetX = contentOffset.x;
+      const layoutWidth = layoutMeasurement.width;
+
+      if (offsetX + layoutWidth >= contentWidth + 80) {
+        throttledHandleShowMore();
+      }
+    },
+    [isLoadingFriendsData, throttledHandleShowMore],
+  );
+
+  if (isLoadingFriendsData) {
+    return (
+      <CardContainer paddingLeft={0} paddingRight={0}>
+        <FlashList
+          data={PLACEHOLDER_DATA}
+          horizontal
+          ListEmptyComponent={null}
+          estimatedItemSize={70}
+          showsHorizontalScrollIndicator={false}
+          renderItem={() => <Skeleton circular size={70} />}
+          ItemSeparatorComponent={() => <Spacer size="$2.5" />}
+          contentContainerStyle={{
+            paddingHorizontal: getToken("$2", "space") as number,
+          }}
+        />
+      </CardContainer>
+    );
+  }
+
+  const {
+    data,
+    title,
+    emoji,
+    showMore = false,
+    onTitlePress,
+    onItemPress,
+    renderExtraItem,
+  } = props;
+
+  return (
+    <CardContainer paddingHorizontal={0}>
+      <YStack gap="$3">
+        {title && (
+          <TouchableOpacity onPress={onTitlePress} disabled={!onTitlePress}>
+            <XStack>
+              <H5 paddingLeft="$3">{title}</H5>
+              {emoji && <Text fontSize="$3"> {emoji}</Text>}
+            </XStack>
+          </TouchableOpacity>
+        )}
+
+        <FlashList
+          data={data}
+          horizontal
+          estimatedItemSize={70}
+          showsHorizontalScrollIndicator={false}
+          onScroll={handleScroll}
+          renderItem={({ item }) => <UserItem item={item} />}
+          ListFooterComponent={
+            <>
+              {renderExtraItem?.()}
+              {showMore && (
+                <TouchableOpacity onPress={throttledHandleShowMore}>
+                  <YStack width={70} gap="$2" alignItems="center">
+                    <View
+                      width={70}
+                      height={70}
+                      justifyContent="center"
+                      alignItems="center"
+                      borderRadius={35}
+                      backgroundColor="$backgroundStrong"
+                    >
+                      <Text fontSize={24}>👀</Text>
+                    </View>
+                    <Text
+                      theme="alt1"
+                      fontSize="$2"
+                      fontWeight="600"
+                      textAlign="center"
+                      numberOfLines={1}
+                      ellipsizeMode="tail"
+                    >
+                      See more
+                    </Text>
+                  </YStack>
+                </TouchableOpacity>
+              )}
+            </>
+          }
+          ItemSeparatorComponent={() => <Spacer size="$2.5" />}
+          contentContainerStyle={{
+            paddingHorizontal: getToken("$2.5", "space") as number,
+          }}
+          ListFooterComponentStyle={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        />
+      </YStack>
+    </CardContainer>
+  );
+}
+
+export default PeopleCarousel;
