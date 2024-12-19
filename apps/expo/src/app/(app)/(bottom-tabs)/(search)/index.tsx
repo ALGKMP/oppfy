9e4b3754724fcf241f9921b14f4cd314743cc7e2
einsight <ghaithkale@gmail.com>
2024-12-19 02:16:05 -0500

    rewrite

--- apps/expo/src/app/(app)/(bottom-tabs)/(search)/index.tsx
+++ apps/expo/src/app/(app)/(bottom-tabs)/(search)/index.tsx
@@ -1,20 +1,18 @@
-import React, { useState } from "react";
-import { Keyboard } from "react-native";
+import React, { useCallback, useMemo, useState } from "react";
+import { Keyboard, RefreshControl } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import * as Haptics from "expo-haptics";
-import { router } from "expo-router";
 import DefaultProfilePicture from "@assets/default-profile-picture.jpg";
 import { FlashList } from "@shopify/flash-list";
-import { H5, H6, View, YStack } from "tamagui";
+import { getToken, H5, H6, View, YStack } from "tamagui";
 
 import CardContainer from "~/components/Containers/CardContainer";
 import { SearchInput } from "~/components/Inputs";
-import { VirtualizedListItem } from "~/components/ListItems";
 import RecommendationList from "~/components/SpecialLists/RecommendationList";
-import { BaseScreenView } from "~/components/Views";
+import { MediaListItem, MediaListItemSkeleton } from "~/components/ui";
+import { Spacer } from "~/components/ui/Spacer";
+import useRouteProfile from "~/hooks/useRouteProfile";
 import { api } from "~/utils/api";
 import type { RouterOutputs } from "~/utils/api";
-import { PLACEHOLDER_DATA } from "~/utils/placeholder-data";
 
 type SearchResultsData = RouterOutputs["search"]["profilesByUsername"];
 type RecommendationsData =
@@ -22,117 +20,129 @@ type RecommendationsData =
 
 const Search = () => {
   const insets = useSafeAreaInsets();
+  const { routeProfile } = useRouteProfile();
 
   const [searchTerm, setSearchTerm] = useState("");
+  const [refreshing, setRefreshing] = useState(false);
   const [searchResults, setSearchResults] = useState<SearchResultsData>([]);
 
   const { data: recommendationsData, isLoading: isLoadingRecommendationsData } =
     api.contacts.getRecommendationProfilesSelf.useQuery();
 
-  const { isLoading, mutateAsync: searchProfilesByUsername } =
+  const { mutateAsync: searchProfilesByUsername, isPending: isSearching } =
     api.search.profilesByUsername.useMutation();
 
-  const performSearch = async (partialUsername: string) => {
-    setSearchTerm(partialUsername);
+  const performSearch = useCallback(
+    async (username: string) => {
+      setSearchTerm(username);
 
-    if (!partialUsername) {
-      setSearchResults([]);
-      return;
+      if (!username) {
+        setSearchResults([]);
+        return;
+      }
+
+      const results = await searchProfilesByUsername({ username });
+      setSearchResults(results);
+    },
+    [searchProfilesByUsername],
+  );
+
+  const handleRefresh = useCallback(async () => {
+    setRefreshing(true);
+    if (searchTerm) {
+      await performSearch(searchTerm);
     }
+    setRefreshing(false);
+  }, [searchTerm, performSearch]);
 
-    const data = await searchProfilesByUsername({
-      username: partialUsername,
-    });
-    setSearchResults(data);
-  };
-
-  const renderLoadingSkeletons = () => (
-    <CardContainer>
-      {PLACEHOLDER_DATA.map((_, index) => (
-        <VirtualizedListItem
-          key={index}
-          loading
-          showSkeletons={{
-            imageUrl: true,
-            title: true,
-            subtitle: true,
-            button: true,
-          }}
-        />
-      ))}
-    </CardContainer>
-  );
-
-  const renderSearchResults = () => (
-    <CardContainer>
-      <FlashList
-        data={searchResults}
-        estimatedItemSize={75}
-        showsVerticalScrollIndicator={false}
-        onScrollBeginDrag={Keyboard.dismiss}
-        keyExtractor={(item) => "search_results_" + item.userId}
-        keyboardShouldPersistTaps="handled"
-        ListHeaderComponent={<H5 theme="alt1">Search Results</H5>}
-        renderItem={({ item }) => (
-          <VirtualizedListItem
-            loading={false}
-            title={item.username}
-            subtitle={item.name}
-            imageUrl={item.profilePictureUrl ?? DefaultProfilePicture}
-            onPress={() => {
-              router.navigate({
-                pathname: "/(search)/profile/[userId]",
-                params: { userId: item.userId, username: item.username },
-              });
-            }}
-          />
-        )}
+  const renderListItem = useCallback(
+    (item: SearchResultsData[0]) => (
+      <MediaListItem
+        title={item.username}
+        subtitle={item.name}
+        imageUrl={item.profilePictureUrl ?? DefaultProfilePicture}
+        onPress={() =>
+          routeProfile({ userId: item.userId, username: item.username })
+        }
       />
-    </CardContainer>
+    ),
+    [routeProfile],
   );
 
-  const renderRecommendations = (recommendationsData: RecommendationsData) => (
-    <RecommendationList
-      loading={isLoadingRecommendationsData}
-      recommendationsData={recommendationsData}
-      handleProfileClicked={(userId, username) => {
-        void Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
-        router.push({
-          pathname: "/(search)/profile/[userId]",
-          params: { userId, username },
-        });
-      }}
-    />
-  );
-
-  return (
-    <BaseScreenView scrollable keyboardDismissMode="interactive">
-      <YStack flex={1} gap="$4" paddingBottom={insets.bottom}>
+  const ListHeaderComponent = useMemo(
+    () => (
+      <YStack gap="$4" pb="$4">
         <SearchInput
           value={searchTerm}
           placeholder="Search by username"
           onChangeText={performSearch}
-          onClear={() => setSearchTerm("")}
+          onClear={() => {
+            setSearchTerm("");
+            setSearchResults([]);
+          }}
         />
-        <View>
-          {!searchTerm ? (
-            isLoadingRecommendationsData ? (
-              renderLoadingSkeletons()
-            ) : recommendationsData?.length ? (
-              renderRecommendations(recommendationsData)
-            ) : null
-          ) : isLoading ? (
-            renderLoadingSkeletons()
-          ) : searchResults.length ? (
-            renderSearchResults()
-          ) : (
-            <H6 theme="alt1" lineHeight={0}>
-              No Users Found
-            </H6>
-          )}
-        </View>
+
+        {!searchTerm && (
+          <RecommendationList
+            loading={isLoadingRecommendationsData}
+            recommendationsData={recommendationsData}
+            handleProfileClicked={(userId, username) => {
+              routeProfile({ userId, username });
+            }}
+          />
+        )}
       </YStack>
-    </BaseScreenView>
+    ),
+    [
+      searchTerm,
+      isLoadingRecommendationsData,
+      recommendationsData,
+      routeProfile,
+      performSearch,
+    ],
+  );
+
+  const ListEmptyComponent = useCallback(() => {
+    if (isSearching) {
+      return (
+        <YStack gap="$4">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <MediaListItemSkeleton key={index} />
+          ))}
+        </YStack>
+      );
+    }
+
+    if (searchTerm && !searchResults.length) {
+      return (
+        <YStack flex={1}>
+          <H6 theme="alt1">No Users Found</H6>
+        </YStack>
+      );
+    }
+
+    return null;
+  }, [isSearching, searchTerm, searchResults.length]);
+
+  return (
+    <FlashList
+      data={searchTerm ? searchResults : []}
+      renderItem={({ item }) => renderListItem(item)}
+      estimatedItemSize={75}
+      ListHeaderComponent={ListHeaderComponent}
+      ListEmptyComponent={ListEmptyComponent}
+      ItemSeparatorComponent={Spacer}
+      contentContainerStyle={{
+        padding: getToken("$4", "space"),
+        paddingBottom: insets.bottom,
+      }}
+      showsVerticalScrollIndicator={false}
+      onScrollBeginDrag={Keyboard.dismiss}
+      keyboardShouldPersistTaps="handled"
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
+      }
+    />
   );
 };
 
