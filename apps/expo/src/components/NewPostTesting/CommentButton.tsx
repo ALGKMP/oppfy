38522854f25373eccb88c8edb38c6204963316d5
tonyy.lb <antonylbrussolo@gmail.com>
2024-12-21 10:47:22 -0500

    wip: CommentButton

---
+++ apps/expo/src/components/NewPostTesting/CommentButton.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { MessageCircle } from "@tamagui/lucide-icons";
+import { SizableText, XStack } from "tamagui";
+
+import { useBottomSheetController } from "~/components/ui/NewBottomSheet";
+import { Text } from "../ui";
+// import CommentsBottomSheet from "./ui/CommentsBottomSheet";
+
+interface CommentButtonProps {
+  postId: string;
+  commentCount: number;
+  selfProfilePicture?: string;
+}
+
+const CommentButton = ({
+  postId,
+  commentCount,
+  selfProfilePicture,
+}: CommentButtonProps) => {
+  const { show } = useBottomSheetController();
+
+  const handlePress = () => {
+    show({
+      snapPoints: ["90%"],
+      children: (
+        // <CommentsBottomSheet
+        //   postId={postId}
+        //   selfProfilePicture={selfProfilePicture}
+        // />
+        <Text>Hello</Text>
+      ),
+    });
+  };
+
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <XStack alignItems="center" gap="$2">
+        <MessageCircle size={20} />
+        <SizableText size="$4">{commentCount}</SizableText>
+      </XStack>
+    </TouchableOpacity>
+  );
+};
+
+export default CommentButton;
