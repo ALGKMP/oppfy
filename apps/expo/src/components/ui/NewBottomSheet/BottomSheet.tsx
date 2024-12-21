d3a4461d4ff92d357eff9cef032ef784fcb7dba3
tonyy.lb <antonylbrussolo@gmail.com>
2024-12-21 11:28:27 -0500

    cleanup

--- apps/expo/src/components/ui/NewBottomSheet/BottomSheet.tsx
+++ apps/expo/src/components/ui/NewBottomSheet/BottomSheet.tsx
@@ -1,14 +1,12 @@
 import React, { useCallback, useEffect, type ReactNode } from "react";
 import {
-  // BottomSheetBackdrop,
   BottomSheetModal,
   type BottomSheetBackdropProps,
   type BottomSheetModalProps,
 } from "@gorhom/bottom-sheet";
 import BottomSheetBackdrop from "./BottomSheetBackdrop";
 import BottomSheetHeader from "./BottomSheetHeader";
-import { Minus } from "@tamagui/lucide-icons";
-import { SizableText, useTheme, View, YStack } from "tamagui";
+import { useTheme } from "tamagui";
 
 export interface BottomSheetProps
   extends Partial<Omit<BottomSheetModalProps, "ref">> {
@@ -32,45 +30,6 @@ export const BottomSheet = React.forwardRef<BottomSheetModal, BottomSheetProps>(
         bottomSheetRef.current?.dismiss();
       }
     }, [isVisible]);
-
-
-    // const renderBackdrop = useCallback(
-    //   (backdropProps: BottomSheetBackdropProps) => (
-    //     <BottomSheetBackdrop
-    //       appearsOnIndex={0}
-    //       disappearsOnIndex={-1}
-    //       opacity={0.5}
-    //       {...backdropProps}
-    //     />
-    //   ),
-    //   [],
-    // );
-
-    // const renderHeader = useCallback(() => {
-    //   return (
-    //     <YStack
-    //       flex={1}
-    //       justifyContent="center"
-    //       alignItems="center"
-    //       position="relative"
-    //     >
-    //       <Minus size="$4" />
-    //       {title && (
-    //         <View justifyContent="center" alignItems="center">
-    //           <SizableText size="$5" textAlign="center" fontWeight="bold">
-    //             {title}
-    //           </SizableText>
-    //         </View>
-    //       )}
-    //       <View
-    //         width="95%"
-    //         borderColor="$gray8"
-    //         borderWidth="$0.25"
-    //         marginTop="$3"
-    //       />
-    //     </YStack>
-    //   );
-    // }, [title]);
 
     const header = useCallback(
       () => <BottomSheetHeader title={title ?? ""} />,
