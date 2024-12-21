e894742566e974982fb8e43759aecd9450dc92be
tonyy.lb <antonylbrussolo@gmail.com>
2024-12-21 13:03:24 -0500

    CommentsCount

---
+++ apps/expo/src/components/NewPostTesting/CommentsCount.tsx
@@ -0,0 +1,47 @@
+import { TouchableOpacity } from "react-native";
+
+import { SizableText } from "~/components/ui/";
+import { useBottomSheetController } from "~/components/ui/NewBottomSheet";
+
+import CommentsBottomSheet from "./ui/CommentsBottomSheet";
+
+interface CommentsCountProps {
+  commentsCount: number;
+  postId: string;
+  endpoint: "self-profile" | "other-profile" | "single-post" | "home-feed";
+  postRecipientUserId: string;
+}
+
+const CommentsCount = ({
+  commentsCount,
+  postId,
+  endpoint,
+  postRecipientUserId,
+}: CommentsCountProps) => {
+  const { show } = useBottomSheetController();
+
+  const handlePress = () => {
+    show({
+      snapPoints: ["90%"],
+      title: "Comments",
+      children: (
+        <CommentsBottomSheet
+          postId={postId}
+          endpoint={endpoint}
+          postRecipientUserId={postRecipientUserId}
+        />
+      ),
+    });
+  };
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <SizableText size="$3" color="$gray10">
+        {commentsCount > 0
+          ? `View ${commentsCount > 1 ? "all " : ""}${commentsCount} ${commentsCount === 1 ? "comment" : "comments"}`
+          : "Be the first to comment"}
+      </SizableText>
+    </TouchableOpacity>
+  );
+};
+
+export default CommentsCount;
