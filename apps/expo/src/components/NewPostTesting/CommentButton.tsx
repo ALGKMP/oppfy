5648d03c92066e28256475524c48d6ced1f582bc
tonyy.lb <antonylbrussolo@gmail.com>
2024-12-21 11:17:29 -0500

    unused props and implementation of new CommentsBottomSheet

--- apps/expo/src/components/NewPostTesting/CommentButton.tsx
+++ apps/expo/src/components/NewPostTesting/CommentButton.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
 import { MessageCircle } from "@tamagui/lucide-icons";
-import { SizableText, XStack } from "tamagui";
 
 import { useBottomSheetController } from "~/components/ui/NewBottomSheet";
-import { Text } from "../ui";
 
-// import CommentsBottomSheet from "./ui/CommentsBottomSheet";
+import CommentsBottomSheet from "./ui/CommentsBottomSheet";
 
 interface CommentButtonProps {
   postId: string;
-  commentCount: number;
-  selfProfilePicture?: string;
+  postRecipientId: string;
+  endpoint: "self-profile" | "other-profile" | "single-post" | "home-feed";
 }
 
 const CommentButton = ({
   postId,
-  commentCount,
-  selfProfilePicture,
+  postRecipientId,
+  endpoint,
 }: CommentButtonProps) => {
   const { show } = useBottomSheetController();
 
@@ -25,11 +23,11 @@ const CommentButton = ({
     show({
       snapPoints: ["90%"],
       children: (
-        // <CommentsBottomSheet
-        //   postId={postId}
-        //   selfProfilePicture={selfProfilePicture}
-        // />
-        <Text>Hello</Text>
+        <CommentsBottomSheet
+          postId={postId}
+          endpoint={endpoint}
+          postRecipientUserId={postRecipientId}
+        />
       ),
     });
   };
