6c507b8cceee79dc11f2a67ac89e5d6958f0c15a
tonyy.lb <antonylbrussolo@gmail.com>
2024-12-25 15:03:00 -0500

    moreOptions button on posts handles self and other

--- apps/expo/src/components/NewPostTesting/MorePostOptionsButton.tsx
+++ apps/expo/src/components/NewPostTesting/MorePostOptionsButton.tsx
@@ -3,11 +3,14 @@ import Haptics from "expo-haptics";
 import { MoreHorizontal } from "@tamagui/lucide-icons";
 
 import { useActionSheetController } from "~/components/ui";
+import { useSession } from "~/contexts/SessionContext";
+import { useDeletePost } from "./hooks/useDeletePost";
 import { useReportPost } from "./hooks/useReportPost";
 import { useSaveMedia } from "./hooks/useSaveMedia";
 
 interface MorePostOptionsButtonProps {
   postId: string;
+  recipientUserId: string;
   mediaUrl: string;
   style?: StyleProp<ViewStyle>;
 }
@@ -16,11 +19,87 @@ const MorePostOptionsButton = ({
   mediaUrl,
   style = { position: "absolute", bottom: 15, right: 15 },
   postId,
+  recipientUserId,
 }: MorePostOptionsButtonProps) => {
   const { handleSavePost, isSaving } = useSaveMedia();
+  const { handleDeletePost, isDeleting } = useDeletePost();
   const { handleReportPost } = useReportPost(postId);
 
-  const { show, hide } = useActionSheetController();
+  const { user } = useSession();
+
+  const { show } = useActionSheetController();
+
+  const buttonOptionsOther = [
+    {
+      text: isSaving ? "Saving..." : "Save Post",
+      onPress: () => void handleSavePost(mediaUrl),
+    },
+    {
+      text: "Report Post",
+      onPress: () => {
+        // hide();
+        setTimeout(() => {
+          show({
+            title: "Report Post",
+            buttonOptions: [
+              {
+                text: "Violent or abusive",
+                textProps: { color: "$blue9" },
+                onPress: () => void handleReportPost("Violent or abusive"),
+              },
+              {
+                text: "Sexually explicit or predatory",
+                textProps: { color: "$blue9" },
+                onPress: () =>
+                  void handleReportPost("Sexually explicit or predatory"),
+              },
+              {
+                text: "Hate, harassment, or bullying",
+                textProps: { color: "$blue9" },
+                onPress: () =>
+                  void handleReportPost("Hate, harassment or bullying"),
+              },
+              {
+                text: "Suicide and self-harm",
+                textProps: { color: "$blue9" },
+                onPress: () => void handleReportPost("Suicide and self-harm"),
+              },
+              {
+                text: "Scam or spam",
+                textProps: { color: "$blue9" },
+                onPress: () => void handleReportPost("Spam or scam"),
+              },
+              {
+                text: "Other",
+                textProps: { color: "$blue9" },
+                onPress: () => void handleReportPost("Other"),
+              },
+            ],
+          });
+        }, 4000);
+      },
+    },
+  ];
+
+  const buttonOptionsSelf = [
+    {
+      text: isSaving ? "Saving" : "Save Post",
+      textProps: {
+        color: isSaving ? "$gray9" : undefined,
+      },
+      autoClose: false,
+      disabled: isSaving,
+      onPress: () => void handleSavePost(mediaUrl),
+    },
+    {
+      text: "Delete Post",
+      textProps: {
+        color: "$red9",
+      },
+      disabled: isDeleting,
+      onPress: () => void handleDeletePost(postId),
+    },
+  ];
 
   return (
     <TouchableOpacity
@@ -30,59 +109,10 @@ const MorePostOptionsButton = ({
         // void Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
         show({
           title: "More Options",
-          buttonOptions: [
-            {
-              text: isSaving ? "Saving..." : "Save Post",
-              onPress: () => void handleSavePost(mediaUrl),
-            },
-            {
-              text: "Report Post",
-              onPress: () => {
-                // hide();
-                setTimeout(() => {
-                  show({
-                    title: "Report Post",
-                    buttonOptions: [
-                    {
-                      text: "Violent or abusive",
-                      textProps: { color: "$blue9" },
-                      onPress: () =>
-                        void handleReportPost("Violent or abusive"),
-                    },
-                    {
-                      text: "Sexually explicit or predatory",
-                      textProps: { color: "$blue9" },
-                      onPress: () =>
-                        void handleReportPost("Sexually explicit or predatory"),
-                    },
-                    {
-                      text: "Hate, harassment, or bullying",
-                      textProps: { color: "$blue9" },
-                      onPress: () =>
-                        void handleReportPost("Hate, harassment or bullying"),
-                    },
-                    {
-                      text: "Suicide and self-harm",
-                      textProps: { color: "$blue9" },
-                      onPress: () =>
-                        void handleReportPost("Suicide and self-harm"),
-                    },
-                    {
-                      text: "Scam or spam",
-                      textProps: { color: "$blue9" },
-                      onPress: () => void handleReportPost("Spam or scam"),
-                    },
-                    {
-                      text: "Other",
-                      textProps: { color: "$blue9" },
-                      onPress: () => void handleReportPost("Other"),
-                    },
-                  ],
-                  });
-                }, 4000);
-              },
-            },
-          ],
+          buttonOptions:
+            user?.uid === recipientUserId
+              ? buttonOptionsSelf
+              : buttonOptionsOther,
         });
       }}
     >
