c118886dd7f1e554febddb90dc7ecd0d47fc78bd
tonyy.lb <antonylbrussolo@gmail.com>
2024-12-25 11:43:00 -0500

    implement the handleSavePost function from useSaveMediaHook on useSaveMedia

--- apps/expo/src/components/NewPostTesting/MorePostOptionsButton.tsx
+++ apps/expo/src/components/NewPostTesting/MorePostOptionsButton.tsx
@@ -1,9 +1,6 @@
 import { StyleProp, TouchableOpacity, ViewStyle } from "react-native";
-import { Position } from "react-native-image-marker";
 import Haptics from "expo-haptics";
-import watermark from "@assets/watermark.png";
 import { MoreHorizontal } from "@tamagui/lucide-icons";
-import { useToastController } from "@tamagui/toast";
 
 import { useActionSheetController } from "~/components/ui";
 import { useSaveMedia } from "./hooks/useSaveMedia";
@@ -17,27 +14,9 @@ const MorePostOptionsButton = ({
   mediaUrl,
   style = { position: "absolute", bottom: 15, right: 15 },
 }: MorePostOptionsButtonProps) => {
-  const { saveMedia, isSaving } = useSaveMedia();
+  const { handleSavePost, isSaving } = useSaveMedia();
 
   const { show } = useActionSheetController();
-  const toast = useToastController();
-
-  const handleSavePost = async () => {
-    try {
-      await saveMedia(mediaUrl, {
-        image: watermark,
-        position: Position.bottomRight,
-        scale: 0.7,
-      });
-      toast.show("Post Saved");
-    } catch (error) {
-      toast.show("Enable media library permission in settings", {
-        burntOptions: {
-          preset: "error",
-        },
-      });
-    }
-  };
   return (
     <TouchableOpacity
       hitSlop={20}
@@ -49,7 +28,7 @@ const MorePostOptionsButton = ({
           buttonOptions: [
             {
               text: isSaving ? "Saving..." : "Save Post",
-              onPress: handleSavePost,
+              onPress: () => void handleSavePost(mediaUrl),
             },
             { text: "Report Post", onPress: () => {} },
           ],
