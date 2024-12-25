0d95234bb9cb76f600070adcd10104c147ec7540
einsight <ghaithkale@gmail.com>
2024-12-24 21:50:26 -0500

    refactor

--- apps/expo/src/features/camera/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CaptureButton } from "./CaptureButton";
-export { default as FocusIcon } from "../../../components/Icons/FocusIcon";
-export { useFocusAnimations } from "../../../components/Icons/FocusIcon";
