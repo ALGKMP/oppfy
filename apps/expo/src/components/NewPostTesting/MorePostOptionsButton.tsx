54d48e763759819546795ca1113963fdf53ca94e
tonyy.lb <antonylbrussolo@gmail.com>
2024-12-25 12:04:25 -0500

    wip: report options

--- apps/expo/src/components/NewPostTesting/MorePostOptionsButton.tsx
+++ apps/expo/src/components/NewPostTesting/MorePostOptionsButton.tsx
@@ -3,9 +3,11 @@ import Haptics from "expo-haptics";
 import { MoreHorizontal } from "@tamagui/lucide-icons";
 
 import { useActionSheetController } from "~/components/ui";
+import { useReportPost } from "./hooks/useReportPost";
 import { useSaveMedia } from "./hooks/useSaveMedia";
 
 interface MorePostOptionsButtonProps {
+  postId: string;
   mediaUrl: string;
   style?: StyleProp<ViewStyle>;
 }
@@ -13,10 +15,13 @@ interface MorePostOptionsButtonProps {
 const MorePostOptionsButton = ({
   mediaUrl,
   style = { position: "absolute", bottom: 15, right: 15 },
+  postId,
 }: MorePostOptionsButtonProps) => {
   const { handleSavePost, isSaving } = useSaveMedia();
+  const { handleReportPost } = useReportPost(postId);
 
-  const { show } = useActionSheetController();
+  const { show, hide } = useActionSheetController();
+
   return (
     <TouchableOpacity
       hitSlop={20}
@@ -30,7 +35,53 @@ const MorePostOptionsButton = ({
               text: isSaving ? "Saving..." : "Save Post",
               onPress: () => void handleSavePost(mediaUrl),
             },
-            { text: "Report Post", onPress: () => {} },
+            {
+              text: "Report Post",
+              onPress: () => {
+                hide();
+                setTimeout(() => {
+                  show({
+                    title: "Report Post",
+                    buttonOptions: [
+                    {
+                      text: "Violent or abusive",
+                      textProps: { color: "$blue9" },
+                      onPress: () =>
+                        void handleReportPost("Violent or abusive"),
+                    },
+                    {
+                      text: "Sexually explicit or predatory",
+                      textProps: { color: "$blue9" },
+                      onPress: () =>
+                        void handleReportPost("Sexually explicit or predatory"),
+                    },
+                    {
+                      text: "Hate, harassment, or bullying",
+                      textProps: { color: "$blue9" },
+                      onPress: () =>
+                        void handleReportPost("Hate, harassment or bullying"),
+                    },
+                    {
+                      text: "Suicide and self-harm",
+                      textProps: { color: "$blue9" },
+                      onPress: () =>
+                        void handleReportPost("Suicide and self-harm"),
+                    },
+                    {
+                      text: "Scam or spam",
+                      textProps: { color: "$blue9" },
+                      onPress: () => void handleReportPost("Spam or scam"),
+                    },
+                    {
+                      text: "Other",
+                      textProps: { color: "$blue9" },
+                      onPress: () => void handleReportPost("Other"),
+                    },
+                  ],
+                  });
+                }, 400);
+              },
+            },
           ],
         });
       }}
