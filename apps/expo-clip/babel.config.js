a4c54a56876ceb1f0cd8c946c4a72d3a241dee63
tonyy.lb <antonylbrussolo@gmail.com>
2025-01-16 23:21:42 -0500

    feat: update dependencies and configuration for expo-clip app
    - Added new dependencies for Tamagui and Babel in pnpm-lock.yaml to support the expo-clip app.
    - Introduced a new Babel configuration file to optimize the build process for the expo-clip app.
    - Enhanced TypeScript configuration by adding asset paths and type definitions for improved development experience.
    - Updated the include paths in tsconfig.json to ensure proper type checking and module resolution.

---
+++ apps/expo-clip/babel.config.js
@@ -0,0 +1,20 @@
+/** @type {import("@babel/core").ConfigFunction} */
+module.exports = function (api) {
+  api.cache.forever();
+
+  return {
+    presets: [["babel-preset-expo"]],
+    plugins: [
+      // "react-native-reanimated/plugin",
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
+          // config: "./tamagui.config.ts",
+          logTimings: true,
+          disableExtraction: process.env.NODE_ENV === "development",
+        },
+      ],
+    ],
+  };
+};
