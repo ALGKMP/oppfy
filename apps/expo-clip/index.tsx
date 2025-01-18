0f76c82bb0af1e9b516c21c907d6397c713a7c12
tonyy.lb <antonylbrussolo@gmail.com>
2025-01-18 18:11:05 -0500

    type

--- apps/expo-clip/index.tsx
+++ apps/expo-clip/index.tsx
@@ -2,8 +2,6 @@ import { Text, View } from "react-native";
 import { registerRootComponent } from "expo";
 import { ExpoRoot, SplashScreen } from "expo-router";
 
-// import "expo-router/entry";
-
 void SplashScreen.preventAutoHideAsync();
 export const App = () => {
   console.log("Root AppClip");
@@ -13,9 +11,18 @@ export const App = () => {
     return <ExpoRoot context={ctx} />;
   } catch (error) {
     console.error("App Error:", error);
+    if (error instanceof Error) {
+      return (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Text>Failed to start app: {error.message}</Text>
+        </View>
+      );
+    }
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Failed to start app: {error.message}</Text>
+        <Text>Failed to start app: No Error Message</Text>
       </View>
     );
   }
