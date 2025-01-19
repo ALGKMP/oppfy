b8ff2e4511dfc7cb717861edaa11c8e7f6423209
tonyy.lb <antonylbrussolo@gmail.com>
2025-01-19 14:01:28 -0500

    ShareButton

--- apps/expo/src/components/Post/ShareButton.tsx
+++ apps/expo/src/components/Post/ShareButton.tsx
@@ -5,17 +5,24 @@ import useShare from "~/hooks/useShare";
 
 interface ShareButtonProps {
   postId: string;
+  mediaUrl: string;
+  recipientName: string;
 }
 
-const ShareButton = ({ postId }: ShareButtonProps) => {
+const ShareButton = ({ postId, mediaUrl, recipientName }: ShareButtonProps) => {
   const { share } = useShare();
 
   return (
     <TouchableOpacity
       onPress={async () => {
+        const appClipUrl = new URL("/post", "https://oppfy.app");
+        appClipUrl.searchParams.set("id", postId);
+        appClipUrl.searchParams.set("media", encodeURIComponent(mediaUrl));
+        appClipUrl.searchParams.set("name", encodeURIComponent(recipientName));
+
         await share({
-          url: `https://www.oppfy.app/post/${postId}`,
-          title: "Share post",
+          url: appClipUrl.toString(),
+          title: "Check out what was posted for you!",
         });
       }}
     >
