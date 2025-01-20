332ae842fd125c6f6a3a5838e8b430807a4116d4
Ghaith Khalil <ghaithkale@gmail.com>
2025-01-19 23:34:53 -0500

    include posts ive uploaded

--- packages/api/src/repositories/media/post.ts
+++ packages/api/src/repositories/media/post.ts
@@ -178,20 +178,34 @@ export class PostRepository {
             "has_liked",
           ),
       })
-      .from(schema.follower)
-      .innerJoin(
-        schema.post,
-        eq(schema.post.recipientId, schema.follower.recipientId),
-      )
+      .from(schema.post)
       .innerJoin(schema.postStats, eq(schema.postStats.postId, schema.post.id))
-      .innerJoin(schema.like, eq(schema.like.postId, schema.post.id))
       .innerJoin(author, eq(schema.post.authorId, author.id))
       .innerJoin(authorProfile, eq(author.profileId, authorProfile.id))
       .innerJoin(recipient, eq(schema.post.recipientId, recipient.id))
       .innerJoin(recipientProfile, eq(recipient.profileId, recipientProfile.id))
-      .where(
+      .leftJoin(
+        schema.like,
+        and(
+          eq(schema.like.postId, schema.post.id),
+          eq(schema.like.userId, userId),
+        ),
+      )
+      .leftJoin(
+        schema.follower,
         and(
           eq(schema.follower.senderId, userId),
+          eq(schema.follower.recipientId, schema.post.recipientId),
+        ),
+      )
+      .where(
+        and(
+          or(
+            // Posts where user follows the recipient
+            eq(schema.follower.senderId, userId),
+            // Posts authored by the user
+            eq(schema.post.authorId, userId),
+          ),
           cursor
             ? or(
                 lt(schema.post.createdAt, cursor.createdAt),
