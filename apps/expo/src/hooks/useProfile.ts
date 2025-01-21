48a4e0fea34b745d2da70f0b81cac90c2001f995
Ghaith Khalil <ghaithkale@gmail.com>
2025-01-21 00:12:32 -0500

    no stale time

--- apps/expo/src/hooks/useProfile.ts
+++ apps/expo/src/hooks/useProfile.ts
@@ -3,8 +3,6 @@ import type { UseQueryResult } from "@tanstack/react-query";
 import type { RouterOutputs } from "~/utils/api";
 import { api } from "~/utils/api";
 
-const STALE_TIME = 5 * 60 * 1000; // 5 minutes
-
 export type SelfProfile = RouterOutputs["profile"]["getFullProfileSelf"];
 export type OtherProfile = RouterOutputs["profile"]["getFullProfileOther"];
 
@@ -23,13 +21,11 @@ const useProfile = (
   const query = api.profile.getFullProfileOther.useQuery(
     { userId: userId! },
     {
-      staleTime: STALE_TIME,
       enabled: !!userId,
     },
   );
 
   const selfQuery = api.profile.getFullProfileSelf.useQuery(undefined, {
-    staleTime: STALE_TIME,
     enabled: !userId,
   });
 
