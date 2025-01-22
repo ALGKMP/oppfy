670d331b18dd5c9cbddd7b825e1b0d3b26c35ce0
tonyy.lb <antonylbrussolo@gmail.com>
2025-01-21 21:20:47 -0500

    tamagui

--- apps/expo-clip/tamagui.config.ts
+++ apps/expo-clip/tamagui.config.ts
@@ -26,6 +26,28 @@ const modakFont = createFont({
   },
 });
 
+const interFont = createFont({
+  family: "InterBlack",
+  size: {
+    1: 11,
+    2: 12,
+    3: 13,
+    4: 14,
+    5: 16,
+    6: 18,
+    7: 20,
+    8: 22,
+    9: 30,
+    10: 42,
+    11: 52,
+    12: 62,
+    13: 72,
+    14: 92,
+    15: 114,
+    16: 124,
+  },
+});
+
 const tamaguiConfig = createTamagui({
   ...config,
   tokens: {
@@ -38,6 +60,7 @@ const tamaguiConfig = createTamagui({
   fonts: {
     ...config.fonts,
     modak: modakFont,
+    heading: interFont,
   },
 });
 
