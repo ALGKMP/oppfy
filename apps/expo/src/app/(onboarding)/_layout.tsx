262fd9707d4a01a151f55bdecaa69305b65ec7c4
Ghaith Khalil <ghaithkale@gmail.com>
2025-02-08 03:42:16 -0500

    cleanup

--- apps/expo/src/app/(onboarding)/_layout.tsx
+++ apps/expo/src/app/(onboarding)/_layout.tsx
@@ -1,61 +1,50 @@
 import React from "react";
-import { Info } from "@tamagui/lucide-icons";
 
 import { OnboardingStack } from "~/components/Layouts/Navigation/OnboardingStack";
-import { Button } from "~/components/ui";
 
-export const unstable_settings = {
-  // Ensure any route can link back to `/`
-  initialRouteName: "index",
-};
-
-export default function OnboardingLayout() {
-  const handleInfo = () => {
-    // TODO: Add info action
-  };
-
-  return (
-    <OnboardingStack
-      screenOptions={{
-        gestureEnabled: false,
+const OnboardingLayout = () => (
+  <OnboardingStack
+    screenOptions={{
+      gestureEnabled: false,
+    }}
+  >
+    <OnboardingStack.Screen
+      name="index"
+      options={{
+        headerShown: false,
       }}
-    >
-      <OnboardingStack.Screen
-        name="index"
-        options={{
-          headerShown: false,
-        }}
-      />
+    />
 
-      {/* Main route groups */}
-      <OnboardingStack.Screen
-        name="auth"
-        options={{
-          headerShown: false,
-        }}
-      />
+    {/* Main route groups */}
+    <OnboardingStack.Screen
+      name="auth"
+      options={{
+        headerShown: false,
+      }}
+    />
 
-      <OnboardingStack.Screen
-        name="user-info"
-        options={{
-          headerShown: false,
-        }}
-      />
+    <OnboardingStack.Screen
+      name="user-info"
+      options={{
+        headerShown: false,
+      }}
+    />
 
-      <OnboardingStack.Screen
-        name="tutorial"
-        options={{
-          headerShown: false,
-        }}
-      />
+    <OnboardingStack.Screen
+      name="tutorial"
+      options={{
+        headerShown: false,
+      }}
+    />
 
-      {/* Misc screens like permissions that can be accessed from anywhere */}
-      <OnboardingStack.Screen
-        name="misc/permissions"
-        options={{
-          title: "",
-        }}
-      />
-    </OnboardingStack>
-  );
-}
+    {/* Misc screens like permissions that can be accessed from anywhere */}
+    <OnboardingStack.Screen
+      name="misc/permissions"
+      options={{
+        title: "",
+      }}
+    />
+  </OnboardingStack>
+);
+
+export default OnboardingLayout;
