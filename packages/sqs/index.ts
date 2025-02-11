c5e8b50032567bb8e53b38943bc45c8883adb0d8
tonyy.lb <antonylbrussolo@gmail.com>
2025-02-11 09:35:32 -0500

    Add SQS utility methods for sending, receiving, and deleting messages

--- packages/sqs/index.ts
+++ packages/sqs/index.ts
@@ -1,11 +1,40 @@
-import { SQSClient } from "@aws-sdk/client-sqs";
+import {
+  DeleteMessageCommand,
+  ReceiveMessageCommand,
+  SendMessageCommand,
+  SQSClient,
+} from "@aws-sdk/client-sqs";
+import type {
+  DeleteMessageCommandInput,
+  ReceiveMessageCommandInput,
+  SendMessageCommandInput,
+} from "@aws-sdk/client-sqs";
 
 import { env } from "@oppfy/env";
 
-export const sqs = new SQSClient({
+const client = new SQSClient({
   region: env.AWS_REGION,
   credentials: {
     accessKeyId: env.AWS_ACCESS_KEY_ID,
     secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
   },
 });
+
+export const sqs = {
+  client,
+
+  async sendMessage(sendMessageCommandInput: SendMessageCommandInput) {
+    const command = new SendMessageCommand(sendMessageCommandInput);
+    return await client.send(command);
+  },
+
+  async receiveMessage(receiveMessageCommandInput: ReceiveMessageCommandInput) {
+    const command = new ReceiveMessageCommand(receiveMessageCommandInput);
+    return await client.send(command);
+  },
+
+  async deleteMessage(deleteMessageCommandInput: DeleteMessageCommandInput) {
+    const command = new DeleteMessageCommand(deleteMessageCommandInput);
+    return await client.send(command);
+  },
+};
