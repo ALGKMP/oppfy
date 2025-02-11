e6e6f36e4ee0ac7dfa861e38eadcc98ea51719e2
tonyy.lb <antonylbrussolo@gmail.com>
2025-02-11 09:35:04 -0500

    Add CloudFront utility methods for signed URLs and invalidations

--- packages/cloudfront/index.ts
+++ packages/cloudfront/index.ts
@@ -1,11 +1,45 @@
-import { CloudFrontClient } from "@aws-sdk/client-cloudfront";
+import {
+  CloudFrontClient,
+  CreateInvalidationCommand,
+} from "@aws-sdk/client-cloudfront";
+import { getSignedUrl } from "@aws-sdk/cloudfront-signer";
 
 import { env } from "@oppfy/env";
 
-export const cloudfront = new CloudFrontClient({
+const ONE_HOUR = 60 * 60 * 1000;
+
+const client = new CloudFrontClient({
   region: env.AWS_REGION,
   credentials: {
     accessKeyId: env.AWS_ACCESS_KEY_ID,
     secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
   },
 });
+
+export const cloudfront = {
+  client,
+
+  async getSignedUrl({ url }: { url: string }): Promise<string> {
+    return getSignedUrl({
+      url,
+      keyPairId: env.CLOUDFRONT_PUBLIC_KEY_ID,
+      privateKey: env.CLOUDFRONT_PRIVATE_KEY,
+      dateLessThan: new Date(Date.now() + ONE_HOUR).toISOString(),
+    }) as unknown as Promise<string>;
+  },
+
+  async createInvalidation(distributionId: string, objectPattern: string) {
+    const command = new CreateInvalidationCommand({
+      DistributionId: distributionId,
+      InvalidationBatch: {
+        CallerReference: Date.now().toString(),
+        Paths: {
+          Quantity: 1,
+          Items: [objectPattern],
+        },
+      },
+    });
+
+    await client.send(command);
+  },
+};
