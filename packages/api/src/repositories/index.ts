d8082bcba60ec56be7dec341173bd90b5c0d3b2e
tonyy.lb <antonylbrussolo@gmail.com>
2025-02-18 15:59:51 -0500

    Remove ViewRepository and related view tracking functionality

--- packages/api/src/repositories/index.ts
+++ packages/api/src/repositories/index.ts
@@ -7,7 +7,6 @@ export { ProfileRepository } from "./user/profile";
 export { PostRepository } from "./media/post";
 export { ContactsRepository } from "./user/contacts";
 export { ReportRepository } from "./report/report";
-export { ViewRepository } from "./network/view";
 export { ProfileStatsRepository } from "./user/profileStats";
 export { PostStatsRepository } from "./media/post-stats";
 export { NotificationsRepository } from "./user/notifications";
