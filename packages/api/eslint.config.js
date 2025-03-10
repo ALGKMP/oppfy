61a9758c1e9760b2789d4e47d951335ef5120f5b
matt <30198937+6mp@users.noreply.github.com>
2025-03-10 18:05:08 -0400

    demo working

--- packages/api/eslint.config.js
+++ packages/api/eslint.config.js
@@ -1,9 +1,14 @@
 import baseConfig from "@oppfy/eslint-config/base";
+import { propagateThrowsRule } from "./eslint-rules/propagate-throws.js";
 
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
     ignores: [],
+    rules: {
+      // Register the custom rule under a local key:
+      "propagate-throws": "error",
+    },
   },
   ...baseConfig,
 ];
