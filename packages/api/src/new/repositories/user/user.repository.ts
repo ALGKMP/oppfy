5669a418cf3be34ff289ab85eaf38d0779d9ad4d
Ghaith Khalil <ghaithkale@gmail.com>
2025-03-25 17:56:55 -0400

    Refactor UserRepository and related interfaces for improved clarity and functionality
    - Renamed getRandomActiveProfilesForRecs to getRandomActiveUserIds for better semantic clarity.
    - Updated user creation methods to createUserOnApp and createUserNotOnApp for improved structure.
    - Enhanced method signatures and parameters in IUserRepository for consistency.
    - Streamlined user status updates and added new methods for marking user states.
    - Removed obsolete parameters and interfaces to enhance overall code maintainability.

--- packages/api/src/new/repositories/user/user.repository.ts
+++ packages/api/src/new/repositories/user/user.repository.ts
@@ -1,4 +1,4 @@
-import { and, eq, inArray, or, sql } from "drizzle-orm";
+import { and, eq, inArray, sql } from "drizzle-orm";
 import { inject, injectable } from "inversify";
 
 import type {
@@ -9,30 +9,16 @@ import type {
 } from "@oppfy/db";
 
 import { TYPES } from "../../container";
-import type {
-  CreateUserParams,
-  DeleteUserParams,
+import {
+  CreateUserNotOnAppParams,
+  CreateUserOnAppParams,
   ExistingPhoneNumbersParams,
-  GetRandomActiveProfilesForRecsParams,
+  GetRandomActiveUserIdsParams,
   GetUserByPhoneNumberParams,
-  GetUserParams,
-  GetUserStatusParams,
-  GetUserWithNotificationSettingsParams,
-  GetUserWithProfileParams,
   IUserRepository,
-  PrivacySettings,
-  UpdatePrivacyParams,
-  UpdateStatsOnUserDeleteParams,
-  UpdateUserOnAppStatusParams,
-  UpdateUserOnboardingCompleteParams,
-  UpdateUserTutorialCompleteParams,
+  UserIdParams,
 } from "../../interfaces/repositories/user/userRepository.interface";
-import type {
-  User,
-  UserStatus,
-  UserWithNotificationSettings,
-  UserWithProfile,
-} from "../../models";
+import type { User, UserStatus } from "../../models";
 
 @injectable()
 export class UserRepository implements IUserRepository {
@@ -47,28 +33,64 @@ export class UserRepository implements IUserRepository {
     this.schema = schema;
   }
 
-  async createUser(params: CreateUserParams, tx: Transaction): Promise<void> {
-    const { userId, phoneNumber, username, isOnApp, name } = params;
+  /** Creates a user who is on the app, inserting records into multiple tables within a transaction. */
+  async createUserOnApp(
+    params: CreateUserOnAppParams,
+    tx: Transaction,
+  ): Promise<void> {
+    const { userId, phoneNumber, username } = params;
 
     await tx.insert(this.schema.user).values({
       id: userId,
       phoneNumber,
     });
 
-    await tx
-      .insert(this.schema.profile)
-      .values({ userId, username, name })
-      .returning({ id: this.schema.profile.id });
+    await tx.insert(this.schema.profile).values({
+      userId,
+      username,
+    });
 
     await tx.insert(this.schema.userStats).values({ userId });
 
     await tx.insert(this.schema.notificationSettings).values({ userId });
 
-    await tx.insert(this.schema.userStatus).values({ userId, isOnApp });
+    await tx.insert(this.schema.userStatus).values({
+      userId,
+      isOnApp: true,
+    });
   }
 
+  /** Creates a user who is not on the app, including a name, within a transaction. */
+  async createUserNotOnApp(
+    params: CreateUserNotOnAppParams,
+    tx: Transaction,
+  ): Promise<void> {
+    const { userId, phoneNumber, username, name } = params;
+
+    await tx.insert(this.schema.user).values({
+      id: userId,
+      phoneNumber,
+    });
+
+    await tx.insert(this.schema.profile).values({
+      userId,
+      username,
+      name,
+    });
+
+    await tx.insert(this.schema.userStats).values({ userId });
+
+    await tx.insert(this.schema.notificationSettings).values({ userId });
+
+    await tx.insert(this.schema.userStatus).values({
+      userId,
+      isOnApp: false,
+    });
+  }
+
+  /** Retrieves a user by their ID. */
   async getUser(
-    { userId }: GetUserParams,
+    { userId }: UserIdParams,
     db: DatabaseOrTransaction = this.db,
   ): Promise<User | undefined> {
     return await db.query.user.findFirst({
@@ -76,35 +98,7 @@ export class UserRepository implements IUserRepository {
     });
   }
 
-  async getUserWithNotificationSettings(
-    { userId }: GetUserWithNotificationSettingsParams,
-    db: DatabaseOrTransaction = this.db,
-  ): Promise<UserWithNotificationSettings | undefined> {
-    return await db.query.user.findFirst({
-      where: eq(this.schema.user.id, userId),
-      with: { notificationSettings: true },
-    });
-  }
-
-  async getUserWithProfile(
-    { userId }: GetUserWithProfileParams,
-    db: DatabaseOrTransaction = this.db,
-  ): Promise<UserWithProfile | undefined> {
-    return await db.query.user.findFirst({
-      where: eq(this.schema.user.id, userId),
-      with: { profile: true },
-    });
-  }
-
-  async getUserStatus(
-    { userId }: GetUserStatusParams,
-    db: DatabaseOrTransaction = this.db,
-  ): Promise<UserStatus | undefined> {
-    return await db.query.userStatus.findFirst({
-      where: eq(this.schema.userStatus.userId, userId),
-    });
-  }
-
+  /** Retrieves a user by their phone number. */
   async getUserByPhoneNumber(
     { phoneNumber }: GetUserByPhoneNumberParams,
     db: DatabaseOrTransaction = this.db,
@@ -114,24 +108,42 @@ export class UserRepository implements IUserRepository {
     });
   }
 
-  async deleteUser(
-    { userId }: DeleteUserParams,
-    db: DatabaseOrTransaction = this.db,
-  ): Promise<void> {
-    await db.delete(this.schema.user).where(eq(this.schema.user.id, userId));
-  }
-
+  /** Fetches a list of random active user IDs, limited by the specified number. */
   async getRandomActiveUserIds(
-    { limit }: GetRandomActiveProfilesForRecsParams,
+    { limit }: GetRandomActiveUserIdsParams,
     db: DatabaseOrTransaction = this.db,
   ): Promise<{ userId: string }[]> {
     return await db
       .select({ userId: this.schema.user.id })
       .from(this.schema.user)
+      .innerJoin(
+        this.schema.userStatus,
+        eq(this.schema.user.id, this.schema.userStatus.userId),
+      )
+      .where(eq(this.schema.userStatus.isOnApp, true))
       .orderBy(sql`RANDOM()`)
       .limit(limit);
   }
 
+  /** Deletes a user by their ID. */
+  async deleteUser(
+    { userId }: UserIdParams,
+    db: DatabaseOrTransaction = this.db,
+  ): Promise<void> {
+    await db.delete(this.schema.user).where(eq(this.schema.user.id, userId));
+  }
+
+  /** Retrieves the status of a user by their ID. */
+  async getUserStatus(
+    { userId }: UserIdParams,
+    db: DatabaseOrTransaction = this.db,
+  ): Promise<UserStatus | undefined> {
+    return await db.query.userStatus.findFirst({
+      where: eq(this.schema.userStatus.userId, userId),
+    });
+  }
+
+  /** Checks for existing phone numbers that are on the app. */
   async existingPhoneNumbers(
     { phoneNumbers }: ExistingPhoneNumbersParams,
     db: DatabaseOrTransaction = this.db,
@@ -153,130 +165,36 @@ export class UserRepository implements IUserRepository {
     return existingNumbers.map((user) => user.phoneNumber);
   }
 
-  async updateStatsOnUserDelete(
-    { userId }: UpdateStatsOnUserDeleteParams,
-    tx: Transaction,
-  ): Promise<void> {
-    // Update post stats
-    // Decrement likes count
-    await tx
-      .update(this.schema.postStats)
-      .set({ likes: sql`${this.schema.postStats.likes} - 1` })
-      .where(
-        inArray(
-          this.schema.postStats.postId,
-          tx
-            .select({ postId: this.schema.like.postId })
-            .from(this.schema.like)
-            .where(eq(this.schema.like.userId, userId)),
-        ),
-      );
-
-    // Decrement comments count
-    await tx
-      .update(this.schema.postStats)
-      .set({ comments: sql`${this.schema.postStats.comments} - 1` })
-      .where(
-        inArray(
-          this.schema.postStats.postId,
-          tx
-            .select({ postId: this.schema.comment.postId })
-            .from(this.schema.comment)
-            .where(eq(this.schema.comment.userId, userId)),
-        ),
-      );
-
-    // Update profile stats
-    // Decrement followers count for users that the deleted user was following
-    await tx
-      .update(this.schema.userStats)
-      .set({ followers: sql`${this.schema.userStats.followers} - 1` })
-      .where(
-        inArray(
-          this.schema.userStats.userId,
-          tx
-            .select({ userId: this.schema.user.id })
-            .from(this.schema.follow)
-            .innerJoin(
-              this.schema.user,
-              eq(this.schema.follow.recipientId, this.schema.user.id),
-            )
-            .where(eq(this.schema.follow.senderId, userId)),
-        ),
-      );
-
-    // Decrement following count for users that were following the deleted user
-    await tx
-      .update(this.schema.userStats)
-      .set({ following: sql`${this.schema.userStats.following} - 1` })
-      .where(
-        inArray(
-          this.schema.userStats.userId,
-          tx
-            .select({ userId: this.schema.user.id })
-            .from(this.schema.follow)
-            .innerJoin(
-              this.schema.user,
-              eq(this.schema.follow.senderId, this.schema.user.id),
-            )
-            .where(eq(this.schema.follow.recipientId, userId)),
-        ),
-      );
-
-    // Decrement friends count for users that were friends with the deleted user
-    await tx
-      .update(this.schema.userStats)
-      .set({ friends: sql`${this.schema.userStats.friends} - 1` })
-      .where(
-        inArray(
-          this.schema.userStats.userId,
-          tx
-            .select({ userId: this.schema.user.id })
-            .from(this.schema.friend)
-            .innerJoin(
-              this.schema.user,
-              or(
-                and(
-                  eq(this.schema.friend.userIdA, userId),
-                  eq(this.schema.friend.userIdB, this.schema.user.id),
-                ),
-                and(
-                  eq(this.schema.friend.userIdB, userId),
-                  eq(this.schema.friend.userIdA, this.schema.user.id),
-                ),
-              ),
-            ),
-        ),
-      );
-  }
-
-  async updateUserOnAppStatus(
-    { userId, isOnApp }: UpdateUserOnAppStatusParams,
+  /** Marks a user as being on the app. */
+  async markUserAsOnApp(
+    { userId }: UserIdParams,
     db: DatabaseOrTransaction = this.db,
   ): Promise<void> {
     await db
       .update(this.schema.userStatus)
-      .set({ isOnApp })
+      .set({ isOnApp: true })
       .where(eq(this.schema.userStatus.userId, userId));
   }
 
-  async updateUserTutorialComplete(
-    { userId, hasCompletedTutorial }: UpdateUserTutorialCompleteParams,
+  /** Marks a user as having completed the tutorial. */
+  async markUserAsTutorialComplete(
+    { userId }: UserIdParams,
     db: DatabaseOrTransaction = this.db,
   ): Promise<void> {
     await db
       .update(this.schema.userStatus)
-      .set({ hasCompletedTutorial })
+      .set({ hasCompletedTutorial: true })
       .where(eq(this.schema.userStatus.userId, userId));
   }
 
-  async updateUserOnboardingComplete(
-    { userId, hasCompletedOnboarding }: UpdateUserOnboardingCompleteParams,
+  /** Marks a user as having completed onboarding. */
+  async markUserAsOnboardingComplete(
+    { userId }: UserIdParams,
     db: DatabaseOrTransaction = this.db,
   ): Promise<void> {
     await db
       .update(this.schema.userStatus)
-      .set({ hasCompletedOnboarding })
+      .set({ hasCompletedOnboarding: true })
       .where(eq(this.schema.userStatus.userId, userId));
   }
 }
