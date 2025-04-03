681f1e3ecb394f8507aa8fd7e01fd8352b3fbaeb
Ghaith Khalil <ghaithkale@gmail.com>
2025-04-02 23:02:18 -0400

    Refactor validators to use a unified object structure
    - Consolidated individual validators into a single `validators` object for improved organization and maintainability.
    - Updated the name, username, bio, dateOfBirth, phoneNumber, and phoneNumberOTP validators to align with the new structure while preserving existing validation logic.
    - Enhanced readability and accessibility of validation rules across the application.

--- packages/validators/index.ts
+++ packages/validators/index.ts
@@ -4,39 +4,44 @@ import { z } from "zod";
 
 import { AgeChecker } from "./utils";
 
-export const name = z
-  .string()
-  .min(1, { message: "Name is too short" })
-  .max(50, { message: "Name is too long" })
-  .regex(/^[a-zA-Z]+(?:\s[a-zA-Z]+)?$/, {
-    message: "Name can only contain letters and a single space between names",
-  });
+export const validators = {
+  name: z
+    .string()
+    .min(1, { message: "Name is too short" })
+    .max(50, { message: "Name is too long" })
+    .regex(/^[a-zA-Z]+(?:\s[a-zA-Z]+)?$/, {
+      message: "Name can only contain letters and a single space between names",
+    }),
 
-export const username = z
-  .string()
-  .min(1, "Too short")
-  .max(30, "Too long")
-  .regex(/^[a-z0-9_]/, "Must start with letter, number, or underscore")
-  .regex(/^[a-z0-9_.]*$/, "Only lowercase, numbers, underscores, dots allowed")
-  .regex(/[a-z0-9]$/, "Must end with letter or number");
+  username: z
+    .string()
+    .min(1, "Too short")
+    .max(30, "Too long")
+    .regex(/^[a-z0-9_]/, "Must start with letter, number, or underscore")
+    .regex(
+      /^[a-z0-9_.]*$/,
+      "Only lowercase, numbers, underscores, dots allowed",
+    )
+    .regex(/[a-z0-9]$/, "Must end with letter or number"),
 
-export const bio = z
-  .string()
-  .max(100, { message: "Bio must be at most 100 characters long" });
+  bio: z
+    .string()
+    .max(100, { message: "Bio must be at most 100 characters long" }),
 
-export const dateOfBirth = z
-  .date()
-  .refine((date) =>
-    new AgeChecker(date).isAtLeast(13).isAtMost(100).checkValid(),
-  );
+  dateOfBirth: z
+    .date()
+    .refine((date) =>
+      new AgeChecker(date).isAtLeast(13).isAtMost(100).checkValid(),
+    ),
 
-export const phoneNumber = z
-  .object({
-    phoneNumber: z.string(),
-    countryCode: z.string(),
-  })
-  .refine((data) =>
-    isValidPhoneNumber(data.phoneNumber, data.countryCode as CountryCode),
-  );
+  phoneNumber: z
+    .object({
+      phoneNumber: z.string(),
+      countryCode: z.string(),
+    })
+    .refine((data) =>
+      isValidPhoneNumber(data.phoneNumber, data.countryCode as CountryCode),
+    ),
 
-export const phoneNumberOTP = z.string().length(6);
+  phoneNumberOTP: z.string().length(6),
+};
