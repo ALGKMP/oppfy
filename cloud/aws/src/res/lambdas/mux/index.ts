aeb106d2765d0803d19fede3765639824e51ffa2
Antony <antonylbrussolo@gmail.com>
2025-04-11 00:02:54 -0400

    Refactor mux and posts lambdas to improve type handling and remove unused code; implement asset deletion and webhook signature verification in Mux class.

--- cloud/aws/src/res/lambdas/mux/index.ts
+++ cloud/aws/src/res/lambdas/mux/index.ts
@@ -1,34 +1,38 @@
 import { parser } from "@aws-lambda-powertools/parser/middleware";
 import { APIGatewayProxyEventV2Schema } from "@aws-lambda-powertools/parser/schemas";
-import { PublishCommand, SNSClient } from "@aws-sdk/client-sns";
+// import { PublishCommand, SNSClient } from "@aws-sdk/client-sns";
 import middy from "@middy/core";
-import { createEnv } from "@t3-oss/env-core";
+// import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
-import { db, eq, schema } from "@oppfy/db";
-import { mux } from "@oppfy/mux";
-import { validators } from "@oppfy/validators";
+import { db, eq, schema, sql } from "@oppfy/db";
+import { Mux } from "@oppfy/mux";
 
-type SnsNotificationData = z.infer<typeof validators.snsNotificationData>;
+// type SnsNotificationData = z.infer<typeof validators.snsNotificationData>;
 
-type StoreNotificationData = z.infer<typeof validators.notificationData>;
+// type StoreNotificationData = z.infer<typeof validators.notificationData>;
 
-type SendNotificationData = z.infer<typeof validators.sendNotificationData>;
+// type SendNotificationData = z.infer<typeof validators.sendNotificationData>;
 
 type APIGatewayProxyEvent = z.infer<typeof APIGatewayProxyEventV2Schema>;
 
 const MUX_READY_EVENT = "video.asset.ready";
 
-const env = createEnv({
-  server: {
-    SNS_PUSH_NOTIFICATION_TOPIC_ARN: z.string().min(1),
-  },
-  runtimeEnv: process.env,
-});
+const mux = new Mux();
 
-const sns = new SNSClient({
-  region: "us-east-1",
+const muxPassthroughSchema = z.object({
+  postid: z.string(),
 });
+// const env = createEnv({
+//   server: {
+//     SNS_PUSH_NOTIFICATION_TOPIC_ARN: z.string().min(1),
+//   },
+//   runtimeEnv: process.env,
+// });
+
+// const sns = new SNSClient({
+//   region: "us-east-1",
+// });
 
 const muxBodySchema = z
   .object({
@@ -43,13 +47,13 @@ const muxBodySchema = z
       playback_ids: z.array(z.object({ id: z.string() })).nonempty(),
       passthrough: z
         .string()
-        .transform((str) => validators.metadataSchema.parse(JSON.parse(str))),
+        .transform((str) => muxPassthroughSchema.parse(JSON.parse(str))),
     }),
   })
   .passthrough();
 
 const deleteAsset = async (assetId: string) => {
-  await mux.video.assets.delete(assetId);
+  await mux.deleteAsset(assetId);
 };
 
 const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<void> => {
@@ -61,7 +65,7 @@ const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<void> => {
   }
 
   try {
-    mux.webhooks.verifySignature(rawBody, headers);
+    mux.verifyWebhookSignature(rawBody, headers);
   } catch {
     throw new Error("Invalid Mux Webhook Signature");
   }
@@ -73,146 +77,131 @@ const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<void> => {
     console.log(`Ignoring Mux event of type: ${body.type}`);
     return;
   }
-
-  const key = body.data.playback_ids[0].id;
-  const metadata = body.data.passthrough;
-  const assetId = body.object.id;
-
+  const postId = body.data.passthrough.postid;
   try {
-    try {
-      const { insertId: postId } = await db.transaction(async (tx) => {
-        const [post] = await tx
-          .insert(schema.post)
-          .values({
-            id: metadata.postid,
-            key,
-            mediaType: "video" as const,
-            authorId: metadata.author,
-            height: parseInt(metadata.height),
-            width: parseInt(metadata.width),
-            caption: metadata.caption,
-            recipientId: metadata.recipient,
-          })
-          .returning({ insertId: schema.post.id });
+    // transaction to update post status and user stats
+    await db.transaction(async (tx) => {
+      await tx
+        .update(schema.post)
+        .set({
+          status: "processed",
+        })
+        .where(eq(schema.post.id, postId));
 
-        if (post === undefined) {
-          throw new Error("Failed to insert post");
-        }
+      // Update user stats to increment post count
+      await tx
+        .update(schema.userStats)
+        .set({ posts: sql`${schema.userStats.posts} + 1` })
+        .where(
+          eq(
+            schema.userStats.userId,
+            sql`(SELECT recipient_user_id FROM post WHERE id = ${postId})`,
+          ),
+        );
+    });
 
-        await tx.insert(schema.postStats).values({ postId: post.insertId });
-        return post;
-      });
+    // await storeNotification(metadata.author, metadata.recipient, {
+    //   eventType: "post",
+    //   entityType: "post",
+    //   entityId: postId.toString(),
+    // });
 
-      await storeNotification(metadata.author, metadata.recipient, {
-        eventType: "post",
-        entityType: "post",
-        entityId: postId.toString(),
-      });
-
-      const { posts } = await getNotificationSettings(metadata.recipient);
-      if (posts) {
-        const pushTokens = await getPushTokens(metadata.recipient);
-        if (pushTokens.length > 0) {
-          const senderProfile = await getProfile(metadata.author);
-          await sendNotification(
-            pushTokens,
-            metadata.author,
-            metadata.recipient,
-            {
-              title: "You've been opped",
-              body: `${senderProfile.username} posted a video of you`,
-              entityId: postId.toString(),
-              entityType: "post",
-            },
-          );
-        }
-      }
-    } catch (error) {
-      console.error("Error processing video:", error);
-      // If the transaction fails, delete the Mux asset
-      await deleteAsset(assetId);
-      throw error;
-    }
+    // const { posts } = await getNotificationSettings(metadata.recipient);
+    // if (posts) {
+    //   const pushTokens = await getPushTokens(metadata.recipient);
+    //   if (pushTokens.length > 0) {
+    //     const senderProfile = await getProfile(metadata.author);
+    //     await sendNotification(
+    //       pushTokens,
+    //       metadata.author,
+    //       metadata.recipient,
+    //       {
+    //         title: "You've been opped",
+    //         body: `${senderProfile.username} posted a video of you`,
+    //         entityId: postId.toString(),
+    //         entityType: "post",
+    //       },
+    //     );
+    //   }
+    // }
   } catch (error) {
     console.error("Error processing video:", error);
-    // Ensure Mux asset is deleted in case of any error
-    await deleteAsset(assetId);
     throw error;
   }
 };
 
-const sendNotification = async (
-  pushTokens: string[],
-  senderId: string,
-  recipientId: string,
-  notificationData: SendNotificationData,
-) => {
-  const message = {
-    senderId,
-    recipientId,
-    pushTokens,
-    ...notificationData,
-  } satisfies SnsNotificationData;
-  const params = {
-    Subject: "New notification",
-    TopicArn: env.SNS_PUSH_NOTIFICATION_TOPIC_ARN,
-    Message: JSON.stringify(message),
-  };
-  await sns.send(new PublishCommand(params));
-};
+// const sendNotification = async (
+//   pushTokens: string[],
+//   senderId: string,
+//   recipientId: string,
+//   notificationData: SendNotificationData,
+// ) => {
+//   const message = {
+//     senderId,
+//     recipientId,
+//     pushTokens,
+//     ...notificationData,
+//   } satisfies SnsNotificationData;
+//   const params = {
+//     Subject: "New notification",
+//     TopicArn: env.SNS_PUSH_NOTIFICATION_TOPIC_ARN,
+//     Message: JSON.stringify(message),
+//   };
+//   await sns.send(new PublishCommand(params));
+// };
 
-const storeNotification = async (
-  senderId: string,
-  recipientId: string,
-  notificationData: StoreNotificationData,
-) => {
-  await db.insert(schema.notifications).values({
-    senderId,
-    recipientId,
-    ...notificationData,
-  });
-};
+// const storeNotification = async (
+//   senderId: string,
+//   recipientId: string,
+//   notificationData: StoreNotificationData,
+// ) => {
+//   await db.insert(schema.notifications).values({
+//     senderId,
+//     recipientId,
+//     ...notificationData,
+//   });
+// };
 
-const getProfile = async (userId: string) => {
-  const user = await db.query.user.findFirst({
-    where: eq(schema.user.id, userId),
-    with: {
-      profile: true,
-    },
-  });
+// const getProfile = async (userId: string) => {
+//   const user = await db.query.user.findFirst({
+//     where: eq(schema.user.id, userId),
+//     with: {
+//       profile: true,
+//     },
+//   });
 
-  if (user === undefined) {
-    throw new Error("User not found");
-  }
+//   if (user === undefined) {
+//     throw new Error("User not found");
+//   }
 
-  return user.profile;
-};
+//   return user.profile;
+// };
 
-const getNotificationSettings = async (userId: string) => {
-  const user = await db.query.user.findFirst({
-    where: eq(schema.user.id, userId),
-    with: {
-      notificationSettings: true,
-    },
-  });
+// const getNotificationSettings = async (userId: string) => {
+//   const user = await db.query.user.findFirst({
+//     where: eq(schema.user.id, userId),
+//     with: {
+//       notificationSettings: true,
+//     },
+//   });
 
-  if (user === undefined) {
-    throw new Error("User not found");
-  }
+//   if (user === undefined) {
+//     throw new Error("User not found");
+//   }
 
-  return user.notificationSettings;
-};
+//   return user.notificationSettings;
+// };
 
-const getPushTokens = async (userId: string) => {
-  const possiblePushTokens = await db.query.pushToken.findMany({
-    where: eq(schema.pushToken.userId, userId),
-    columns: {
-      token: true,
-    },
-  });
+// const getPushTokens = async (userId: string) => {
+//   const possiblePushTokens = await db.query.pushToken.findMany({
+//     where: eq(schema.pushToken.userId, userId),
+//     columns: {
+//       token: true,
+//     },
+//   });
 
-  return possiblePushTokens.map((token) => token.token);
-};
+//   return possiblePushTokens.map((token) => token.token);
+// };
 
 export const handler = middy(lambdaHandler).use(
   parser({ schema: APIGatewayProxyEventV2Schema }),
