e778e0f33489d21743dacdc0eb44cef1f0467759
tonyy.lb <antonylbrussolo@gmail.com>
2025-05-17 19:52:02 -0400

    feat(onboarding): add tutorial screen and update onboarding layout
    Introduced a new tutorial screen in the onboarding flow to enhance user guidance. Updated the onboarding layout to include the tutorial screen, ensuring a seamless transition and improved navigation experience for users.

---
+++ apps/expo/src/app/(onboarding)/tutorial/_layout.tsx
@@ -0,0 +1,13 @@
+
+import { OnboardingStack } from "~/components/Layouts/Navigation/OnboardingStack";
+
+const AuthLayout = () => (
+  <OnboardingStack
+    screenOptions={{
+      title: "",
+      headerShown: false,
+    }}
+  />
+);
+
+export default AuthLayout;
