30e3a4e4c96250667b4ea3e2afbce306e9241de2
tonyy.lb <antonylbrussolo@gmail.com>
2025-05-18 12:35:52 -0400

    feat(onboarding): update navigation and add new tutorial screen
    Modified the onboarding flow to navigate to the new tutorial screen "screen1" instead of the previous route. Enhanced the tutorial layout to include both "screen1" and "screen2" with improved animation and progress tracking. Introduced a new "screen1" component featuring a background image and interactive button for seamless user engagement.

--- apps/expo/src/app/(onboarding)/tutorial/_layout.tsx
+++ apps/expo/src/app/(onboarding)/tutorial/_layout.tsx
@@ -1,22 +1,50 @@
+import { usePathname } from "expo-router";
+
+import type { OnboardingStackOptions } from "~/components/Layouts/Navigation/OnboardingStack";
 import { OnboardingStack } from "~/components/Layouts/Navigation/OnboardingStack";
 
-const AuthLayout = () => (
-  <OnboardingStack
-    screenOptions={{
-      title: "",
-      headerShown: false,
-    }}
-  >
-    <OnboardingStack.Screen
-      name="screen2"
-      options={{
-        headerShown: false,
-        contentStyle: {
-          backgroundColor: "#C7F458",
-        },
-      }}
-    />
-  </OnboardingStack>
-);
+const ROUTES = ["screen1", "screen2"];
+
+const AuthLayout = () => {
+  const pathName = usePathname();
+  const currentRoute = pathName.split("/").pop();
+  const currentIndex = ROUTES.indexOf(currentRoute ?? "");
+
+  return (
+    <OnboardingStack
+      screenOptions={
+        {
+          title: "",
+          animation: "fade",
+          // headerShown: false,
+          progress: {
+            currentStep: Math.max(0, currentIndex),
+            totalSteps: ROUTES.length,
+          },
+        } as OnboardingStackOptions
+      }
+    >
+      <OnboardingStack.Screen
+        name="screen1"
+        options={{
+        //   headerShown: false,
+          contentStyle: {
+            backgroundColor: "$primary",
+          },
+        }}
+      />
+
+      <OnboardingStack.Screen
+        name="screen2"
+        options={{
+        //   headerShown: false,
+          contentStyle: {
+            backgroundColor: "#C7F458",
+          },
+        }}
+      />
+    </OnboardingStack>
+  );
+};
 
 export default AuthLayout;
